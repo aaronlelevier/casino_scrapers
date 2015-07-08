e20f33dd2fa78fbb714a94bedb5adf0d503c93e8
toran billups <toranb@gmail.com>
2015-07-07 17:23:35 -0700

    [BUILD]: added ci.sh to run a build

---
+++ ci.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+cd bsrs-ember
+npm install
+if [ "$?" == 1 ]; then
+  echo "npm install failed"
+  exit $?
+fi
+./node_modules/ember-cli/bin/ember test
+if [ "$?" == 1 ]; then
+  echo "ember test failed"
+  exit $?
+fi
+
+cd ../bsrs-django
+virtualenv venv
+source venv/bin/activate
+pip install -r requirements.txt
+if [ "$?" == 1 ]; then
+  echo "pip install failed"
+  exit $?
+fi
+
+cd bigsky
+python manage.py test --settings=bigsky.settings.ci
+if [ "$?" == 1 ]; then
+  echo "django test failed"
+  exit $?
+fi
+
+echo "BUILD SUCCESSFUL!"
+exit 0
