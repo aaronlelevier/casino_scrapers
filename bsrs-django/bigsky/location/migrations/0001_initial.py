5fd53ff8adf284d0340cdd9f75859788492fe5c1
Aaron Lelevier <pyaaron@gmail.com>
2015-07-10 15:10:38 -0700

    remove fixtures by app, and make just one _data.json_ fixtures to load

--- bsrs-django/bigsky/location/migrations/0001_initial.py
+++ bsrs-django/bigsky/location/migrations/0001_initial.py
@@ -25,7 +25,7 @@ class Migration(migrations.Migration):
             name='LocationLevel',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('name', models.CharField(max_length=100)),
+                ('name', models.CharField(unique=True, max_length=100)),
                 ('children', models.ManyToManyField(related_name='parents', to='location.LocationLevel', blank=True)),
             ],
             options={
@@ -36,7 +36,7 @@ class Migration(migrations.Migration):
             name='LocationStatus',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('name', models.CharField(max_length=100)),
+                ('name', models.CharField(unique=True, max_length=100)),
             ],
             options={
                 'abstract': False,
@@ -46,7 +46,7 @@ class Migration(migrations.Migration):
             name='LocationType',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('name', models.CharField(max_length=100)),
+                ('name', models.CharField(unique=True, max_length=100)),
             ],
             options={
                 'abstract': False,
