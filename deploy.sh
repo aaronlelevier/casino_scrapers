5e2249e8b624131c4bd76d333d1b01b214ef4dd9
toran billups <toranb@gmail.com>
2015-07-15 17:51:25 -0500

    [DEPLOY]: now running migrate w/out activated venv

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ venv/bin/pip install -r bsrs-django/requirements.txt
 
 cd bsrs-django/bigsky
 
-python manage.py migrate --settings=bigsky.settings.ci
+../../venv/bin/python manage.py migrate --settings=bigsky.settings.ci
 uwsgi --http :8000 --wsgi-file bigsky.wsgi --virtualenv /www/django/releases/$NEW_UUID/venv
 
 echo "DEPLOY SUCCESSFUL!"
