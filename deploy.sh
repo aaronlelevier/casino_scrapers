5c44f66d48db4f2f28a523cc64005f8f8d47c397
toran billups <toranb@gmail.com>
2015-07-17 16:58:46 -0500

    [DEPLOY]: removed half/baked error handling

--- deploy.sh
+++ deploy.sh
@@ -25,22 +25,10 @@ cd bsrs-django/bigsky
 ../../venv/bin/python manage.py migrate --settings=bigsky.settings.ci
 ../../venv/bin/python manage.py loaddata fixtures/jenkins.json --settings=bigsky.settings.ci
 
-MIGRATE_EXIT=$?
-if [ "$MIGRATE_EXIT" == 1 ]; then
-  echo "DEPLOY MIGRATION FAILED!"
-  exit $MIGRATE_EXIT
-fi
-
 cp -r ../../bsrs-ember/dist/assets .
 cp -r ../../bsrs-ember/dist/index.html templates
 
 uwsgi --http :8000 --wsgi-file bigsky.wsgi --virtualenv /www/django/releases/$NEW_UUID/venv --daemonize /tmp/bigsky.log --static-map /assets=/www/django/releases/$NEW_UUID/bsrs-django/bigsky --check-static /www/django/releases/$NEW_UUID/bsrs-django/bigsky
 
-WSGI_EXIT=$?
-if [ "$WSGI_EXIT" == 1 ]; then
-  echo "WSGI FAILED!"
-  exit $WSGI_EXIT
-fi
-
-echo "DEPLOY SUCCESSFUL!"
+echo "DEPLOY FINISHED!"
 exit 0
