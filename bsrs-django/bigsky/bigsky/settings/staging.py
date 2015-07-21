f351774d01a96b896b80aa1fd9e6f176f15d8918
Aaron Lelevier <pyaaron@gmail.com>
2015-07-21 13:12:58 -0700

    [POSTGRES] staging.py config

--- bsrs-django/bigsky/bigsky/settings/staging.py
+++ bsrs-django/bigsky/bigsky/settings/staging.py
@@ -20,9 +20,9 @@ DEBUG = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', 
-        'NAME': '', # os.environ['DB_NAME']
-        'USER': '', # os.environ['DB_USER']
-        'PASSWORD': '', # os.environ['DB_PASSWORD']
+        'NAME': 'staging',  # os.environ['DB_NAME']
+        'USER': '',         # os.environ['DB_USER']
+        'PASSWORD': '',     # os.environ['DB_PASSWORD']
         'HOST': 'localhost',
         'PORT': '5432',
     }
