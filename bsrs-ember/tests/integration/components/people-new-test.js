55d724bfea8b181a4e8819c011d0d1bdf55b356d
snewcomer <snewcomer24@gmail.com>
2015-07-24 16:06:10 -0700

    [PERSON NEW]: fixed the person new integration tests

--- bsrs-ember/tests/integration/components/people-new-test.js
+++ bsrs-ember/tests/integration/components/people-new-test.js
@@ -2,17 +2,20 @@ import Ember from 'ember';
 import hbs from 'htmlbars-inline-precompile';
 import { moduleForComponent, test } from 'ember-qunit';
 import translation from "bsrs-ember/instance-initializers/ember-i18n";
-import Person from 'bsrs-ember/models/person';
+import module_registry from 'bsrs-ember/tests/helpers/module_registry';
+
+var store;
 
 moduleForComponent('person-new', 'integration: person-new test', {
     integration: true,
     setup() {
+        store = module_registry(this.container, this.registry, ['model:person', 'model:phonenumber']);
         translation.initialize(this);
     }
 });
 
 test('filling in invalid username reveal validation messages', function(assert) {
-    this.set('model', Person.create());
+    this.set('model', store.push('person', {}));
     this.render(hbs`{{person-new model=model}}`);
     var $component = this.$('.t-username-validation-error');
     assert.ok(this.$('.t-username-validation-error').is(':hidden'));
@@ -24,7 +27,7 @@ test('filling in invalid username reveal validation messages', function(assert) 
 });
 
 test('filling in invalid password reveal validation messages', function(assert) {
-    this.set('model', Person.create());
+    this.set('model', store.push('person', {}));
     this.render(hbs`{{person-new model=model}}`);
     var $component = this.$('.t-password-validation-error');
     assert.ok(this.$('.t-password-validation-error').is(':hidden'));
@@ -36,7 +39,7 @@ test('filling in invalid password reveal validation messages', function(assert) 
 });
 
 test('filling in invalid first name reveal validation messages', function(assert) {
-    this.set('model', Person.create());
+    this.set('model', store.push('person', {}));
     this.render(hbs`{{person-new model=model}}`);
     var $component = this.$('.t-first-name-validation-error');
     assert.ok(this.$('.t-first-name-validation-error').is(':hidden'));
@@ -48,7 +51,7 @@ test('filling in invalid first name reveal validation messages', function(assert
 });
 
 test('filling in invalid last name reveal validation messages', function(assert) {
-    this.set('model', Person.create());
+    this.set('model', store.push('person', {}));
     this.render(hbs`{{person-new model=model}}`);
     var $component = this.$('.t-last-name-validation-error');
     assert.ok(this.$('.t-last-name-validation-error').is(':hidden'));
@@ -60,7 +63,7 @@ test('filling in invalid last name reveal validation messages', function(assert)
 });
 
 test('filling in invalid email reveal validation messages', function(assert) {
-    this.set('model', Person.create());
+    this.set('model', store.push('person', {}));
     this.render(hbs`{{person-new model=model}}`);
     var $component = this.$('.t-email-validation-error');
     assert.ok(this.$('.t-email-validation-error').is(':hidden'));
