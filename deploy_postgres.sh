1662ae74a14f604eeb1ea27962970ffb2f4fd17e
Aaron Lelevier <pyaaron@gmail.com>
2015-07-27 10:38:02 -0700

    [POSTGRES BUILD] add exit blocks for quick exits on erros

--- deploy_postgres.sh
+++ deploy_postgres.sh
@@ -22,53 +22,89 @@ function npmInstall {
 }
 
 function stopUwsgi {
-UWSGI_PORT=$((8002))
-echo "KILL UWSGI PROCESSES ON PORT $UWSGI_PORT"
-fuser -k -n tcp $UWSGI_PORT
-RESULT=$?
+	UWSGI_PORT=$((8002))
+	echo "KILL UWSGI PROCESSES ON PORT $UWSGI_PORT"
+	fuser -k -n tcp $UWSGI_PORT
+	RESULT=$?
     if [ "$RESULT" == 1 ]; then
-      echo "git clone failed"
+      echo "uWSGI failed"
       exit $RESULT
     fi
 }
 
+function buildEmber {
 	./node_modules/ember-cli/bin/ember build --env=production
-	cd ../
+	RESULT=$?
+    if [ "$RESULT" == 1 ]; then
+      echo "uWSGI failed"
+      exit $RESULT
+    fi
 
+}
+
+function buildVirtualenv {
 	echo "CREATE VIRTUALENV AND PIP DEPENDENCIES"
+	easy_install -U pip
 	rm -rf venv
 	virtualenv venv
 	venv/bin/pip install -r bsrs-django/requirements.txt
+	RESULT=$?
+    if [ "$RESULT" == 1 ]; then
+      echo "uWSGI failed"
+      exit $RESULT
+    fi
+}
 
-	cd bsrs-django/bigsky
-
-
+function dropCreateDB {
 	DB_NAME="staging"
 	echo "DB NAME TO DROP: $DB_NAME"
 	export PGPASSWORD=tango
-
 	dropdb $DB_NAME -U bsdev
 	echo "$DB_NAME dropped"
-
 	createdb $DB_NAME -U bsdev -O bsdev
 	echo "$DB_NAME created"
+	RESULT=$?
+    if [ "$RESULT" == 1 ]; then
+      echo "uWSGI failed"
+      exit $RESULT
+    fi
+}
 
+function runMigrations {
 	echo "RUN DATABASE MIGRATIONS"
 	export DJANGO_SETTINGS_MODULE='bigsky.settings.staging'
 	../../venv/bin/python manage.py collectstatic --noinput
 	../../venv/bin/python manage.py makemigrations
 	../../venv/bin/python manage.py migrate
 	../../venv/bin/python manage.py loaddata fixtures/postgres.json
+	RESULT=$?
+    if [ "$RESULT" == 1 ]; then
+      echo "uWSGI failed"
+      exit $RESULT
+    fi
+}
 
+function copyStatic {
 	echo "COPY OVER EMBER STATIC ASSETS"
 	cp -r ../../bsrs-ember/dist/assets .
 	cp -r ../../bsrs-ember/dist/fonts .
 	cp -r ../../bsrs-ember/dist/index.html templates
+	RESULT=$?
+    if [ "$RESULT" == 1 ]; then
+      echo "uWSGI failed"
+      exit $RESULT
+    fi
+}
 
+function startUwsgi {
 	uwsgi --http :$UWSGI_PORT --wsgi-file bigsky_postgres.wsgi --virtualenv /www/django/releases/$NEW_UUID/venv --daemonize /tmp/bigsky.log --static-map /assets=/www/django/releases/$NEW_UUID/bsrs-django/bigsky --static-map /fonts=/www/django/releases/$NEW_UUID/bsrs-django/bigsky --check-static /www/django/releases/$NEW_UUID/bsrs-django/bigsky
-
 	UWSGI=$(ps aux | grep $UWSGI_PORT)
 	echo $UWSGI
+	RESULT=$?
+    if [ "$RESULT" == 1 ]; then
+      echo "uWSGI failed"
+      exit $RESULT
+    fi
 }
 
 cd /www/django/releases
@@ -77,7 +113,21 @@ gitClone
 cd $NEW_UUID/bsrs-ember/
 npmInstall
 
-theRest
+stopUwsgi
+
+buildEmber
+cd ../
+
+buildVirtualenv
+cd bsrs-django/bigsky
+
+dropCreateDB
+
+runMigrations
+
+copyStatic
+
+startUwsgi
 
 echo "DEPLOY FINISHED!"
 exit 0
