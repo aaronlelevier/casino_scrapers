6dfd1e9456af87c05988243b763c0868d023894f
Aaron Lelevier <pyaaron@gmail.com>
2015-07-28 09:39:40 -0700

    [UUID] passing in a uuid works

--- deploy_postgres.sh
+++ deploy_postgres.sh
@@ -27,7 +27,7 @@ function npmInstall {
 }
 
 function stopUwsgi {
-    echo "KILL UWSGI PROCESSES ON PORT $UWSGI_PORT"
+    echo "KILL UWSGI PROCESSES"
     killall -s INT uwsgi
 }
 
@@ -103,9 +103,8 @@ function copyStatic {
 
 function startUwsgi {
     uwsgi --http :$UWSGI_PORT --wsgi-file bigsky_postgres.wsgi --virtualenv /www/django/releases/$NEW_UUID/bsrs-django/venv --daemonize /tmp/bigsky.log --static-map /assets=/www/django/releases/$NEW_UUID/bsrs-django/bigsky --static-map /fonts=/www/django/releases/$NEW_UUID/bsrs-django/bigsky --check-static /www/django/releases/$NEW_UUID/bsrs-django/bigsky
-    UWSGI=$(ps aux | grep $UWSGI_PORT)
+    UWSGI=$(ps aux | grep uwsgi)
     echo $UWSGI
-    lsof -i :8000
     RESULT=$?
     if [ "$RESULT" == 1 ]; then
       echo "uWSGI failed"
