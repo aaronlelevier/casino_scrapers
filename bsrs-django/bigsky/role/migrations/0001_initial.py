1cfe8070e34a23a300df68e41031d9b1ce330543
Aaron Lelevier <pyaaron@gmail.com>
2015-07-28 17:49:02 -0700

    [FIXTURES] Update to work w/ Person standard fixtures, and PhoneNumberType custom fixtures

--- bsrs-django/bigsky/role/migrations/0001_initial.py
+++ bsrs-django/bigsky/role/migrations/0001_initial.py
@@ -7,7 +7,7 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('person', '0002_auto_20150724_1314'),
+        ('person', '0001_initial'),
     ]
 
     operations = [
