3022b2b80d9c901e3a7d18441f3463ea808a0e7b
aronysidoro <pyaaron@gmail.com>
2015-08-12 13:13:50 -0700

    [SELENIUM] refactor first 2 Selenium tests.

--- bsrs-django/bigsky/selenium/fill_in_helper.py
+++ bsrs-django/bigsky/selenium/fill_in_helper.py
@@ -10,3 +10,15 @@ class FillInHelper(object):
             inputrr = getattr(self, k + '_input')
             inputrr.send_keys(v)
 
+
+class FillInDictHelper(object):
+    '''
+    This Helper will loop through model's attributes and find the element on the page
+    and fill in input with the attributes' values
+    Ensure model has attrs (k) equal to id on template 
+    '''
+    def _fill_in_dict(self, dict_):
+        for k, v in dict_.iteritems():
+            setattr(self, k + '_input', self.wait_for_xhr_request_id(k))
+            inputrr = getattr(self, k + '_input')
+            inputrr.send_keys(v)
--- bsrs-django/bigsky/selenium/javascript.py
+++ bsrs-django/bigsky/selenium/javascript.py
@@ -4,7 +4,7 @@ import time
 class JavascriptMixin(object):
 
     def wait_for_xhr_request(self, selector, plural=False):
-        for w in range(1, 10):
+        for w in range(20):
             print "waiting for xhr callback..."
             if(self.driver.execute_script("return $.active") == 0):
                 if(plural):
@@ -17,7 +17,7 @@ class JavascriptMixin(object):
             time.sleep(1)
 
     def wait_for_xhr_request_id(self, selector):
-        for w in range(1, 10):
+        for w in range(20):
             print "waiting for xhr callback..."
             element = self.driver.find_element_by_id(selector)
             if element.is_displayed():
