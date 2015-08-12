e15086ee75952da362f3be1718dcd88348af735a
toran billups <toranb@gmail.com>
2015-08-12 09:23:22 -0500

    [SELENIUM]: fixed a python syntax error

--- bsrs-django/bigsky/selenium/person_helper.py
+++ bsrs-django/bigsky/selenium/person_helper.py
@@ -3,7 +3,6 @@ class PersonHelper(object):
     Loops through kwargs passed in as kwargs
     Ensure model has attrs (k) equal to id on template 
     '''
-    def _fill_in(self, model):
     def __init__(self, **kwargs):
         for k, v in kwargs.iteritems():
             setattr(self, k, v)
