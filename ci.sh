ffd50969b8eb06cce527e122e85a6ad716ff53d2
toran billups <toranb@gmail.com>
2015-08-12 15:41:17 -0500

    [BUILD]: removed the --no-use-wheel from the pip install (mistake)

--- ci.sh
+++ ci.sh
@@ -30,7 +30,7 @@ function pipInstall {
     rm -rf venv
     virtualenv venv
     source venv/bin/activate
-    pip install -r requirements.txt --no-use-wheel
+    pip install -r requirements.txt
     PIP_INSTALL=$?
     if [ "$PIP_INSTALL" == 1 ]; then
       echo "pip install failed"
