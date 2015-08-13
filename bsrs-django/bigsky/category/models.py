b3b22ce46e817128b041275f513889367e6e26d2
aronysidoro <pyaaron@gmail.com>
2015-08-13 16:49:57 -0700

    [CATEGORY] First commit

---
+++ bsrs-django/bigsky/category/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+from util.models import AbstractName, BaseModel
+
+
+class CategoryType(AbstractName):
+	"""Single Parent / Child Hierarchical Structure."""
+	child = models.OneToOneField("self", related_name="parent")
