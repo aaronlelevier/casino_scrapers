e493b671ec3692b06420c07f5e6cdb060e6f0152
aronysidoro <pyaaron@gmail.com>
2015-08-18 12:42:02 -0700

    wip

--- bsrs-django/bigsky/accounting/tests/test_views.py
+++ bsrs-django/bigsky/accounting/tests/test_views.py
@@ -1,7 +1,10 @@
 import json
+import uuid
 
 from rest_framework.test import APITestCase
 
+from accounting.models import Currency
+from accounting.serializers import CurrencySerializer
 from person.tests.factory import create_person, PASSWORD
 
 
@@ -21,3 +24,22 @@ class CurrencyTests(APITestCase):
         # Content
         data = json.loads(response.content)
         self.assertTrue(data['results'] > 0)
+
+    def test_create(self):
+        # TODO
+        pass
+
+    def test_create_duplicate(self):
+        # Setup
+        self.currency = Currency.objects.first()
+        self.assertIsInstance(self.currency, Currency)
+
+        serializer = CurrencySerializer(self.currency)
+        self.data = serializer.data
+        self.data.update({
+            'id': str(uuid.uuid4())
+            })
+        # Test
+        response = self.client.post('/api/admin/currencies/', self.data, format='json')
+        # print response
+        self.assertEqual(response.status_code, 201)
