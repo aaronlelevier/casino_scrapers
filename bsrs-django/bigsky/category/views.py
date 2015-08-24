4f9b3262461024b632fc712e846f2b5b0a7a672d
aronysidoro <pyaaron@gmail.com>
2015-08-24 14:17:02 -0700

    [CATEGORY] filter API Endpoints added

--- bsrs-django/bigsky/category/views.py
+++ bsrs-django/bigsky/category/views.py
@@ -1,8 +1,17 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+import rest_framework_filters as filters
 
 from category.models import Category
 from category import serializers as cs
+
+
+class CategoryFilterSet(filters.FilterSet):
+    parent = filters.AllLookupsFilter(name='parent')
+
+    class Meta:
+        model = Category
+        fields = ['parent']
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
@@ -14,10 +23,20 @@ class CategoryViewSet(viewsets.ModelViewSet):
     Can add and/or remove a single Parent and multiple Children Categories.
 
     Must send the Parent/Children on every Update.
-    '''
 
+    ### Filters
+    1. Get top level Categories
+
+        `/api/admin/categories/?parent__isnull=True`
+
+    2. Get Children for a specified Parent
+
+        `/api/admin/categories/?parent=id`
+
+    '''
     permission_classes = (IsAuthenticated,)
     queryset = Category.objects.all()
+    filter_class = CategoryFilterSet
 
     def get_serializer_class(self):
         """
