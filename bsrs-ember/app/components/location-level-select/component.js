4fca82dc701bfb37388706bece324ec8b1a3ba02
snewcomer <snewcomer24@gmail.com>
2015-08-24 21:46:53 -0700

    [LOCATION LEVEL SELECT]: changed variable names of related models to ensure manyRelationship is clear and thus easier to use the component

--- bsrs-ember/app/components/location-level-select/component.js
+++ bsrs-ember/app/components/location-level-select/component.js
@@ -5,20 +5,18 @@ let LocationLevelSelect = Ember.Component.extend({
     store: inject('main'),
     actions: {
         changed(model, val) {
-            var modelName = this.get('modelName');
+            var manyRelationshipModel = this.get('manyRelationshipTo');
             var location_id = model.get('id');
             var new_location_level = this.get('store').find('location-level', val);
             var old_location_level = this.get('model');
-
-            var new_location_level_locations = new_location_level.get(modelName) || [];
-
+            var new_location_level_locations = new_location_level.get(manyRelationshipModel) || [];
             Ember.run(() => {
                 if(new_location_level.get('content')) {
-                    new_location_level.set(modelName, new_location_level_locations.concat([location_id]));
+                    new_location_level.set(manyRelationshipModel, new_location_level_locations.concat([location_id]));
                 }
                 if(old_location_level) {
-                    var old_location_level_locations = old_location_level.get(modelName) || [];
-                    old_location_level.set(modelName, old_location_level_locations.filter((old_location_level_location_pk) => {
+                    var old_location_level_locations = old_location_level.get(manyRelationshipModel) || [];
+                    old_location_level.set(manyRelationshipModel, old_location_level_locations.filter((old_location_level_location_pk) => {
                         return old_location_level_location_pk !== location_id;
                     }));
                     old_location_level.save();
