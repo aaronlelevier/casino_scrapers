7d786793ce3634654dde3c91635ebbab36417690
aronysidoro <pyaaron@gmail.com>
2015-08-26 11:25:51 -0700

    [PERSON FACTORY] refactor factory for creating initial Person set of 23 people with different Roles and Locations

--- bsrs-django/bigsky/person/tests/factory.py
+++ bsrs-django/bigsky/person/tests/factory.py
@@ -6,18 +6,41 @@ from django.db import IntegrityError
 from model_mommy import mommy
 
 from accounting.models import Currency
-from location.models import LocationLevel
+from location.models import LocationLevel, Location
+from location.tests.factory import create_locations
 from person.models import Person, PersonStatus, Role
 from util import create
 
 
 PASSWORD = '1234'
-
+LOCATION_LEVEL = 'region'
 
 def create_role():
+    "Single Role needed to create Person with Login privileges."
+
     currency = Currency.objects.default()
-    location_level = mommy.make(LocationLevel)
+
+    try:
+        location_level = LocationLevel.objects.get(name=LOCATION_LEVEL)
+    except LocationLevel.DoesNotExist:
+        location_level = mommy.make(LocationLevel, name=LOCATION_LEVEL)
+
     return mommy.make(Role, name=create._generate_chars(), location_level=location_level)
+
+
+def create_roles():
+    "Create a Role for each LocationLevel"
+    # initial Locations
+    try:
+        create_locations()
+    except IntegrityError:
+        pass
+
+    for location_level in LocationLevel.objects.all():
+        mommy.make(Role, name='{}-role'.format(location_level.name),
+            location_level=location_level)
+
+    return Role.objects.all()
 
 
 def create_single_person(username, role):
@@ -35,8 +58,9 @@ def create_person(username=None, _many=1):
 
     # Single User Create
     if username and _many != 1:
-        raise Exception("Can't specify more than 1 user with a specific username. \
-You specified {} user(s) with username: {}".format(_many, username))
+        raise Exception(
+            "Can't specify more than 1 user with a specific username. "
+            "You specified {} user(s) with username: {}".format(_many, username))
     elif username:
         return create_single_person(username, role)
         
@@ -49,16 +73,29 @@ You specified {} user(s) with username: {}".format(_many, username))
 
 
 def create_23_people():
-    role = create_role()
-    aaron = mommy.make(Person, username='aaron', role=role)
+    # initial Locations
+    try:
+        create_locations()
+    except IntegrityError:
+        pass
+
+    # initial Roles
+    roles = create_roles()
+
+    # Person used to Login (so needs a 'password' set here)
+    aaron = mommy.make(Person, username='aaron', role=roles[0])
     aaron.set_password('1234')
     aaron.save()
 
+    # other Persons for Grid View
     count = Person.objects.count()
     while count < 23:
         try:
             username = create.random_lorem(words=1)
-            mommy.make(Person, username=username, first_name=username, role=role)
+            role = random.choice(roles)
+            locations = Location.objects.filter(location_level=role.location_level)
+            mommy.make(Person, username=username, first_name=username, role=role,
+                locations=locations)
         except IntegrityError:
             pass
         count = Person.objects.count()
