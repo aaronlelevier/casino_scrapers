36b3d76804a84b2080d4236e48f5ce8bd5093759
toran billups <toranb@gmail.com>
2015-08-28 12:25:13 -0700

    [PERSON]: locations are now filtered by person.role.location_level

--- bsrs-ember/app/components/person-single/component.js
+++ bsrs-ember/app/components/person-single/component.js
@@ -1,24 +1,15 @@
 import Ember from 'ember';
 import inject from 'bsrs-ember/utilities/inject';
 import injectStore from 'bsrs-ember/utilities/store';
+import PersonLocationsMixin from 'bsrs-ember/mixins/person/locations';
 import {ValidationMixin, validate} from 'ember-cli-simple-validation/mixins/validate';
 
-export default Ember.Component.extend(ValidationMixin, {
+export default Ember.Component.extend(ValidationMixin, PersonLocationsMixin, {
     repository: inject('person'),
     location_repo: inject('location'),
     classNames: ['wrapper', 'form'],
     attemptedTransition: '',
     usernameValidation: validate('model.username'),
-    locations: Ember.computed('model.role', function() {
-        let role = this.get('model.role');
-        if(role && role.get('id')) {
-            let location_level = role.get('location_level');
-            let location_level_pk = location_level.get('id');
-            let location_repo = this.get('location_repo');
-            return location_repo.find({location_level: location_level_pk});
-        }
-        return [];
-    }),
     actions: {
         savePerson() {
             this.get('model').set('dirtyModel', false);
