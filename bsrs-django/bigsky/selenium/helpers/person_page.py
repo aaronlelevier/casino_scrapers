c513a8f5a01abe949d3e0276fd6f596154127293
aronysidoro <pyaaron@gmail.com>
2015-09-02 17:46:50 -0700

    [SELENIUM] person_page - extract list checker as method for class

--- bsrs-django/bigsky/selenium/helpers/person_page.py
+++ bsrs-django/bigsky/selenium/helpers/person_page.py
@@ -15,23 +15,31 @@ class PersonPage(ModelPage):
         self.list_name = list_name
         self.list_data = list_data
 
+    def _loop_over_names(self, name, new_person, count):
+        """Loop over names in the list.
+
+        :Return: new_person; incremented count for the page.
+        """
+        try:
+            all_people = self.find_list_data()
+        except AssertionError:
+            pass
+        list_view_elements = self.find_list_name()
+        for row in list_view_elements:
+            if row.text and row.text == name:
+                new_person = row
+        count += 1
+        return (new_person, count)
+
     def click_name_in_list(self, name, new_person):
         pagination = self.wait_for_xhr_request("t-pages")
         element_list = pagination.find_elements_by_tag_name("li")
         element_list_len = len(element_list)
         count = 0
         while count < element_list_len:
-            try:
-                all_people = self.find_list_data()
-            except AssertionError:
-                pass
-            list_view_elements = self.find_list_name()
-            for row in list_view_elements:
-                if row.text and row.text == name:
-                    new_person = row
+            new_person, count = self._loop_over_names(name, new_person, count)
             if new_person:
                 break
-            count += 1
             pagination = self.driver.find_element_by_class_name("t-pages")
             element_list = pagination.find_elements_by_tag_name("a")
             next_elem = element_list[count]
@@ -56,19 +64,12 @@ class PersonPage(ModelPage):
         element_list_len = len(element_list)
         count = 0
         while count < element_list_len:
-            try:
-                all_people = self.find_list_data()
-            except AssertionError:
-                pass
-            list_view_elements = self.find_list_name()
-            for row in list_view_elements:
-                if row.text and row.text == name:
-                    new_person = row
-            count += 1
+            new_person, count = self._loop_over_names(name, new_person, count)
+
             pagination = self.driver.find_element_by_class_name("t-pages")
             try:
                 element_list = pagination.find_elements_by_tag_name("a")
                 next_elem = element_list[count]
                 next_elem.click()
-            except:
+            except IndexError:
                 assert new_person == None
