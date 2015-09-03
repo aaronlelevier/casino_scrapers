0b22f1bb2cbcfebff6b82ae130fc766366510d96
aronysidoro <pyaaron@gmail.com>
2015-09-03 09:37:21 -0700

    [CATEGORY] refactor label field, and use '_update_defaults' model method pattern like other models

--- bsrs-django/bigsky/category/models.py
+++ bsrs-django/bigsky/category/models.py
@@ -11,9 +11,14 @@ class Category(AbstractName):
     """
     Category tree. Categories are self referencing OneToMany.  A Parent has 
     many Children.
+
+    - Parent or Label is required to create a Category.
+    - If the ``parent`` FK is null, then it is a Top Level Category.
     """
     description = models.CharField(max_length=100, blank=True, null=True)
-    label = models.CharField(max_length=100, blank=True, null=True)
+    label = models.CharField(max_length=100, editable=False, blank=True, null=True,
+        help_text="This field cannot be set directly.  It is either set from "
+                  "a system setting, or defaulted from the Parent Category.")
     subcategory_label = models.CharField(max_length=100)
     cost_amount = models.DecimalField(max_digits=15, decimal_places=4, blank=True, default=0)
     cost_currency = models.ForeignKey(Currency, blank=True, null=True)
@@ -21,15 +26,14 @@ class Category(AbstractName):
     parent = models.ForeignKey("self", related_name='children', blank=True, null=True)
 
     def save(self, *args, **kwargs):
-        '''
-        Parent or Label is required to create a Category.
-        '''
+        self._update_defalts()
+        return super(Category, self).save(*args, **kwargs)
+
+    def _update_defalts(self):
         if not self.parent:
             self.label = settings.TOP_LEVEL_CATEGORY_LABEL
         else:
             self.label = self.parent.subcategory_label
-
+            
         if not self.cost_currency:
             self.cost_currency = Currency.objects.default()
-
-        return super(Category, self).save(*args, **kwargs)
