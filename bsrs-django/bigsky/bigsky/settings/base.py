19a28eb96626ba59743b6646c4f9e57c957d2bbd
aronysidoro <pyaaron@gmail.com>
2015-09-09 15:32:40 -0700

    [PASSWORD] move the bcrypt hashing algorithm to 'settings/prod.py'

--- bsrs-django/bigsky/bigsky/settings/base.py
+++ bsrs-django/bigsky/bigsky/settings/base.py
@@ -110,15 +110,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'source')
 
-PASSWORD_HASHERS = (
-    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
-    'django.contrib.auth.hashers.BCryptPasswordHasher',
-    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
-    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
-    'django.contrib.auth.hashers.SHA1PasswordHasher',
-    'django.contrib.auth.hashers.MD5PasswordHasher',
-    'django.contrib.auth.hashers.CryptPasswordHasher',
-)
 
 ### Native Configurations ###
 
