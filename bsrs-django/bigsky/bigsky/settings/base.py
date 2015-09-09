1cab5ba53994223ee66efc9ca34e79d1773cb8d7
aronysidoro <pyaaron@gmail.com>
2015-09-09 15:18:53 -0700

    [PASSWORD] bcrypt added

--- bsrs-django/bigsky/bigsky/settings/base.py
+++ bsrs-django/bigsky/bigsky/settings/base.py
@@ -110,6 +110,15 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'source')
 
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.SHA1PasswordHasher',
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+    'django.contrib.auth.hashers.CryptPasswordHasher',
+)
 
 ### Native Configurations ###
 
