5857ab56f1859e17c026b66ac00ea2b5fcb4cc4d
toran billups <toranb@gmail.com>
2015-09-10 14:53:12 -0500

    [SELENIUM]: added just_refreshed kwarg for pages that are slow to re-render

--- bsrs-django/bigsky/selenium/helpers/javascript.py
+++ bsrs-django/bigsky/selenium/helpers/javascript.py
@@ -9,7 +9,7 @@ class JavascriptMixin(object):
     requires the driver to check for elements.
     """
     
-    def wait_for_xhr_request(self, selector, plural=False):
+    def wait_for_xhr_request(self, selector, plural=False, just_refreshed=False):
         for w in range(10):
             print "waiting for xhr callback...{}".format(selector)
             if(self.driver.execute_script("return $.active") == 0):
@@ -20,4 +20,7 @@ class JavascriptMixin(object):
                     element = self.driver.find_element_by_class_name(selector)
                     assert element.is_displayed()
                 return element
-            time.sleep(1)
+            if(just_refreshed):
+                time.sleep(2)
+            else:
+                time.sleep(1)
