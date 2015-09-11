795e9c87897a48ecb4a7791e6c15b29328acc68f
aronysidoro <pyaaron@gmail.com>
2015-09-11 14:32:54 -0700

    [ATTACHMENT] replace test to not use Tester fake model b/c was conflicting w/ django-nose b/c of util app name.

--- bsrs-django/bigsky/generic/tests/test_models.py
+++ bsrs-django/bigsky/generic/tests/test_models.py
@@ -7,13 +7,17 @@ from django.core.exceptions import ValidationError
 from model_mommy import mommy
 
 from generic.models import Attachment
-from util.models import Tester
+from location.models import LocationLevel
 
 
 class AttachmentModelTests(TestCase):
 
     def setUp(self):
-        self.model = mommy.make(Tester)
+        # this can be any Model here, ``Attachment`` just requires a 
+        # model UUID to reference b/c the attachments are assoc. w/ models.
+        self.model = mommy.make(LocationLevel)
+        
+        # test upload file save in source control
         self.filename = "/Users/alelevier/Documents/bsrs/bsrs-django/\
 bigsky/source/attachments/test_in/test-mountains.jpg"
 
