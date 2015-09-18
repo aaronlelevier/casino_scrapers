2d466ec3e927489e0bf59929c6c8d8165f8c4831
toran billups <toranb@gmail.com>
2015-09-18 10:08:58 -0500

    [JSHINT]: added ember import to the focus tether dialog

--- bsrs-ember/app/components/focus-tether-dialog/component.js
+++ bsrs-ember/app/components/focus-tether-dialog/component.js
@@ -1,8 +1,9 @@
+import Ember from 'ember';
 import TetherDialog from 'ember-modal-dialog/components/tether-dialog';
 
 export default TetherDialog.extend({
     didInsertElement: function() {
         this._super();
-        Ember.$('.ember-modal-dialog .t-new-entry').focus();
+        Ember.$('.ember-modal-dialog input:first').focus();
     }
 });
