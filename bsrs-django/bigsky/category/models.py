a7889f6aa0bb16092eeaae26acaf2e21297c0853
aronysidoro <pyaaron@gmail.com>
2015-09-30 18:04:35 -0700

    [D3 TREE] WIP - need to still iron out child recursive call to populate child tree structure. Only working for 2 levels at this time.

--- bsrs-django/bigsky/category/models.py
+++ bsrs-django/bigsky/category/models.py
@@ -19,6 +19,46 @@ class CategoryManager(BaseManager):
                 models.append({"source": category.parent.name, "target": category.name, "type": "suit"})
         return json.dumps(models)
 
+    @property
+    def d3_json_tree(self):
+        """
+        TODO: 
+
+        - Only works for the first level of Children currently.
+        - To use with this tempalate: `d3_tree.html`
+        """
+        def categories(array=None):
+            if not array:
+                array = []
+                for category in Category.objects.filter(parent__isnull=True):
+                    array.append({
+                            "id": str(category.id),
+                            "name": category.name,
+                            "parent": "null" if not category.parent else category.parent.name,
+                            "children": [],
+                            "checked": False
+                        })
+                categories(array)
+            else:
+                for i, arr in enumerate(array):
+                    if not arr["children"] and not arr["checked"]:
+                        array[i]["checked"] = True
+
+                        category = Category.objects.get(id=arr["id"])
+                        children = category.children.all()
+                        for child in children:
+                            array[i]["children"].append({
+                                    "id": str(child.id),
+                                    "name": child.name,
+                                    "parent": category.name,
+                                    "children": [],
+                                    "checked": False
+                                })
+                            categories(array)
+            return array
+
+        return json.dumps(categories())
+
 
 class Category(BaseModel):
     """
