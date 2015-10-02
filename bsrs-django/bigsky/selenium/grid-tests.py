63d1ac5095c34ab305cd5d2fb355681a52493ea8
aronysidoro <pyaaron@gmail.com>
2015-10-02 10:44:03 -0700

    [SELENIUM] refactor test for 'saved_search' change in input field html name.

--- bsrs-django/bigsky/selenium/grid-tests.py
+++ bsrs-django/bigsky/selenium/grid-tests.py
@@ -1,4 +1,5 @@
 import unittest
+import uuid
 
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
@@ -34,119 +35,119 @@ class SeleniumGridTests(JavascriptMixin, LoginMixin, FillInHelper, unittest.Test
     def tearDown(self):
         self.driver.close()
 
-    # def test_ordering(self):
-    #     # ASC
-    #     self.wait_for_xhr_request("t-sort-username-dir").click()
-    #     usernames = self.wait_for_xhr_request("t-person-username", plural=True)
-    #     self.assertEqual(self.lorem[0], usernames[0].text)
-    #     # DESC
-    #     self.wait_for_xhr_request("t-sort-username-dir").click()
-    #     usernames = self.wait_for_xhr_request("t-person-username", plural=True)
-    #     self.assertEqual(self.lorem[-1], usernames[0].text)
+    def test_ordering(self):
+        # ASC
+        self.wait_for_xhr_request("t-sort-username-dir").click()
+        usernames = self.wait_for_xhr_request("t-person-username", plural=True)
+        self.assertEqual(self.lorem[0], usernames[0].text)
+        # DESC
+        self.wait_for_xhr_request("t-sort-username-dir").click()
+        usernames = self.wait_for_xhr_request("t-person-username", plural=True)
+        self.assertEqual(self.lorem[-1], usernames[0].text)
 
-    # def test_ordering_multiple(self):
-    #     # order: username,title
-    #     self.wait_for_xhr_request("t-sort-title-dir").click()
-    #     self.wait_for_xhr_request("t-sort-username-dir").click()
-    #     usernames = self.wait_for_xhr_request("t-person-username", plural=True)
-    #     self.assertEqual(self.lorem[0], usernames[0].text)
-    #     self.wait_for_xhr_request("t-sort-username-dir").click()
-    #     titles = self.wait_for_xhr_request("t-person-title", plural=True)
-    #     self.assertEqual(self.lorem[-1], titles[0].text)
-    #     # order: -username,title
-    #     self.wait_for_xhr_request("t-sort-username-dir").click()
-    #     self.wait_for_xhr_request("t-sort-username-dir").click()
-    #     usernames = self.wait_for_xhr_request("t-person-username", plural=True)
-    #     self.assertEqual(self.lorem[-1], usernames[0].text)
-    #     self.wait_for_xhr_request("t-sort-username-dir").click()
-    #     titles = self.wait_for_xhr_request("t-person-title", plural=True)
-    #     self.assertEqual(self.lorem[0], titles[0].text)
+    def test_ordering_multiple(self):
+        # order: username,title
+        self.wait_for_xhr_request("t-sort-title-dir").click()
+        self.wait_for_xhr_request("t-sort-username-dir").click()
+        usernames = self.wait_for_xhr_request("t-person-username", plural=True)
+        self.assertEqual(self.lorem[0], usernames[0].text)
+        self.wait_for_xhr_request("t-sort-username-dir").click()
+        titles = self.wait_for_xhr_request("t-person-title", plural=True)
+        self.assertEqual(self.lorem[-1], titles[0].text)
+        # order: -username,title
+        self.wait_for_xhr_request("t-sort-username-dir").click()
+        self.wait_for_xhr_request("t-sort-username-dir").click()
+        usernames = self.wait_for_xhr_request("t-person-username", plural=True)
+        self.assertEqual(self.lorem[-1], usernames[0].text)
+        self.wait_for_xhr_request("t-sort-username-dir").click()
+        titles = self.wait_for_xhr_request("t-person-title", plural=True)
+        self.assertEqual(self.lorem[0], titles[0].text)
 
-    # def test_search_input(self):
-    #     people = self.wait_for_xhr_request("t-grid-data", plural=True)
-    #     self.assertEqual(len(people), 10)
-    #     search = self.wait_for_xhr_request("t-grid-search-input").send_keys('cu')
-    #     people = self.wait_for_xhr_request("t-grid-data", plural=True)
-    #     self.assertEqual(len(people), 10)
-    #     search = self.wait_for_xhr_request("t-grid-search-input").send_keys('aaaaa')
-    #     with self.assertRaises(NoSuchElementException):
-    #         people = self.wait_for_xhr_request("t-grid-data", debounce=True)
-    #         people = self.driver.find_element_by_class_name("t-grid-data")
+    def test_search_input(self):
+        people = self.wait_for_xhr_request("t-grid-data", plural=True)
+        self.assertEqual(len(people), 10)
+        search = self.wait_for_xhr_request("t-grid-search-input").send_keys('cu')
+        people = self.wait_for_xhr_request("t-grid-data", plural=True)
+        self.assertEqual(len(people), 10)
+        search = self.wait_for_xhr_request("t-grid-search-input").send_keys('aaaaa')
+        with self.assertRaises(NoSuchElementException):
+            people = self.wait_for_xhr_request("t-grid-data", debounce=True)
+            people = self.driver.find_element_by_class_name("t-grid-data")
 
-    # def test_search_input_with_ordering(self):
-    #     # Search
-    #     _search_input = "ab"
-    #     _search_input_matches = sorted([x for x in self.lorem if _search_input in x])
-    #     search = self.wait_for_xhr_request("t-grid-search-input").send_keys(_search_input)
-    #     people = self.wait_for_xhr_request("t-grid-data", plural=True, debounce=True)
-    #     self.assertEqual(len(people), len(_search_input_matches))
-    #     # Order
-    #     self.wait_for_xhr_request("t-sort-title-dir").click()
-    #     self.wait_for_xhr_request("t-sort-username-dir").click()
-    #     usernames = self.wait_for_xhr_request("t-person-username", plural=True)
-    #     self.assertEqual(_search_input_matches[0], usernames[0].text)
-    #     # Order: -username,title
-    #     self.wait_for_xhr_request("t-sort-username-dir").click()
-    #     usernames = self.wait_for_xhr_request("t-person-username", plural=True)
-    #     self.assertEqual(_search_input_matches[-1], usernames[0].text)
-    #     # Search maintained
-    #     people = self.wait_for_xhr_request("t-grid-data", plural=True)
-    #     self.assertEqual(len(people), len(_search_input_matches))
+    def test_search_input_with_ordering(self):
+        # Search
+        _search_input = "ab"
+        _search_input_matches = sorted([x for x in self.lorem if _search_input in x])
+        search = self.wait_for_xhr_request("t-grid-search-input").send_keys(_search_input)
+        people = self.wait_for_xhr_request("t-grid-data", plural=True, debounce=True)
+        self.assertEqual(len(people), len(_search_input_matches))
+        # Order
+        self.wait_for_xhr_request("t-sort-title-dir").click()
+        self.wait_for_xhr_request("t-sort-username-dir").click()
+        usernames = self.wait_for_xhr_request("t-person-username", plural=True)
+        self.assertEqual(_search_input_matches[0], usernames[0].text)
+        # Order: -username,title
+        self.wait_for_xhr_request("t-sort-username-dir").click()
+        usernames = self.wait_for_xhr_request("t-person-username", plural=True)
+        self.assertEqual(_search_input_matches[-1], usernames[0].text)
+        # Search maintained
+        people = self.wait_for_xhr_request("t-grid-data", plural=True)
+        self.assertEqual(len(people), len(_search_input_matches))
 
-    # def test_search_field_and_order(self):
-    #     # USERNAME
-    #     # setup
-    #     _username= "tt"
-    #     _username_matches = sorted([x for x in self.lorem if _username in x])
-    #     # test
-    #     self.wait_for_xhr_request("t-filter-username").click()
-    #     username_fulltext_search = self.driver.find_element_by_class_name("t-new-entry")
-    #     username_fulltext_search.send_keys(_username)
-    #     people = self.wait_for_xhr_request("t-grid-data", plural=True, debounce=True)
-    #     self.assertEqual(len(people), len(_username_matches))
-    #     # test - ordered
-    #     self.driver.find_element_by_class_name("t-sort-username-dir").click()
-    #     usernames = self.driver.find_elements_by_class_name("t-person-username")
-    #     self.assertEqual(_username_matches[0], usernames[0].text)
-    #     # TITLE
-    #     # setup
-    #     _title = "p"
-    #     _title_matches = [x for x in _username_matches if _title in x]
-    #     # test
-    #     self.driver.find_element_by_class_name("t-filter-title").click()
-    #     title_fulltext_search = self.driver.find_element_by_class_name("t-new-entry")
-    #     title_fulltext_search.send_keys(_title)
-    #     people = self.wait_for_xhr_request("t-grid-data", plural=True, debounce=True)
-    #     self.assertEqual(len(people), len(_title_matches))
-    #     usernames = self.driver.find_elements_by_class_name("t-person-username")
-    #     self.assertEqual(_title_matches[0], usernames[0].text)
-    #     # test - w/ refresh
-    #     self.driver.refresh()
-    #     people = self.wait_for_xhr_request("t-grid-data", plural=True, just_refreshed=True)
-    #     self.assertEqual(len(people), len(_title_matches))
-    #     usernames = self.driver.find_elements_by_class_name("t-person-username")
-    #     self.assertEqual(_title_matches[0], usernames[0].text)
-    #     # submitted text still present
-    #     self.driver.find_element_by_class_name("t-filter-username").click()
-    #     username_fulltext_search = self.driver.find_element_by_class_name("t-new-entry")
-    #     self.assertEqual(username_fulltext_search.get_attribute("value"), _username)
-    #     self.driver.find_element_by_class_name("t-filter-title").click()
-    #     title_fulltext_search = self.driver.find_element_by_class_name("t-new-entry")
-    #     self.assertEqual(title_fulltext_search.get_attribute("value"), _title)
+    def test_search_field_and_order(self):
+        # USERNAME
+        # setup
+        _username= "tt"
+        _username_matches = sorted([x for x in self.lorem if _username in x])
+        # test
+        self.wait_for_xhr_request("t-filter-username").click()
+        username_fulltext_search = self.driver.find_element_by_class_name("t-new-entry")
+        username_fulltext_search.send_keys(_username)
+        people = self.wait_for_xhr_request("t-grid-data", plural=True, debounce=True)
+        self.assertEqual(len(people), len(_username_matches))
+        # test - ordered
+        self.driver.find_element_by_class_name("t-sort-username-dir").click()
+        usernames = self.driver.find_elements_by_class_name("t-person-username")
+        self.assertEqual(_username_matches[0], usernames[0].text)
+        # TITLE
+        # setup
+        _title = "p"
+        _title_matches = [x for x in _username_matches if _title in x]
+        # test
+        self.driver.find_element_by_class_name("t-filter-title").click()
+        title_fulltext_search = self.driver.find_element_by_class_name("t-new-entry")
+        title_fulltext_search.send_keys(_title)
+        people = self.wait_for_xhr_request("t-grid-data", plural=True, debounce=True)
+        self.assertEqual(len(people), len(_title_matches))
+        usernames = self.driver.find_elements_by_class_name("t-person-username")
+        self.assertEqual(_title_matches[0], usernames[0].text)
+        # test - w/ refresh
+        self.driver.refresh()
+        people = self.wait_for_xhr_request("t-grid-data", plural=True, just_refreshed=True)
+        self.assertEqual(len(people), len(_title_matches))
+        usernames = self.driver.find_elements_by_class_name("t-person-username")
+        self.assertEqual(_title_matches[0], usernames[0].text)
+        # submitted text still present
+        self.driver.find_element_by_class_name("t-filter-username").click()
+        username_fulltext_search = self.driver.find_element_by_class_name("t-new-entry")
+        self.assertEqual(username_fulltext_search.get_attribute("value"), _username)
+        self.driver.find_element_by_class_name("t-filter-title").click()
+        title_fulltext_search = self.driver.find_element_by_class_name("t-new-entry")
+        self.assertEqual(title_fulltext_search.get_attribute("value"), _title)
 
-    # def test_full_text_search_hidden_on_enter_and_escape(self):
-    #     self.wait_for_xhr_request("t-filter-username").click()
-    #     username_fulltext_search = self.driver.find_element_by_class_name("t-new-entry")
-    #     username_fulltext_search.send_keys("at")
-    #     username_fulltext_search.send_keys(Keys.RETURN)
-    #     fulltext_modal_present = is_present(self.driver, "ember-modal-dialog")
-    #     self.assertEqual(fulltext_modal_present, False)
-    #     self.wait_for_xhr_request("t-filter-title").click()
-    #     title_fulltext_search = self.driver.find_element_by_class_name("t-new-entry")
-    #     title_fulltext_search.send_keys("de")
-    #     title_fulltext_search.send_keys(Keys.ESCAPE)
-    #     title_modal_present = is_present(self.driver, "ember-modal-dialog")
-    #     self.assertEqual(title_modal_present, False)
+    def test_full_text_search_hidden_on_enter_and_escape(self):
+        self.wait_for_xhr_request("t-filter-username").click()
+        username_fulltext_search = self.driver.find_element_by_class_name("t-new-entry")
+        username_fulltext_search.send_keys("at")
+        username_fulltext_search.send_keys(Keys.RETURN)
+        fulltext_modal_present = is_present(self.driver, "ember-modal-dialog")
+        self.assertEqual(fulltext_modal_present, False)
+        self.wait_for_xhr_request("t-filter-title").click()
+        title_fulltext_search = self.driver.find_element_by_class_name("t-new-entry")
+        title_fulltext_search.send_keys("de")
+        title_fulltext_search.send_keys(Keys.ESCAPE)
+        title_modal_present = is_present(self.driver, "ember-modal-dialog")
+        self.assertEqual(title_modal_present, False)
 
     def test_reset_grid(self):
         people = self.wait_for_xhr_request("t-grid-data", plural=True)
@@ -171,28 +172,19 @@ class SeleniumGridTests(JavascriptMixin, LoginMixin, FillInHelper, unittest.Test
         usernames = self.wait_for_xhr_request("t-person-username", plural=True)
         self.assertEqual(self.lorem[-1], usernames[0].text)
         # Save FilterSet
-        search_name = "my search"
+        search_name = str(uuid.uuid4())[:5]
         modal = self.wait_for_xhr_request("t-show-save-filterset-modal")
-        modal.send_keys(search_name)
+        modal.click()
+        modal_input = self.wait_for_xhr_request("t-filterset-name-input")
+        modal_input.send_keys(search_name)
         self.wait_for_xhr_request("t-filterset-save-btn").click()
         self.driver.find_element_by_link_text(search_name)
-        # hit 'reset' then 'saved_search' link, and should get back to same results
-        self.wait_for_xhr_request("t-reset-grid").click()
-        self.driver.find_element_by_link_text(search_name).click()
-        usernames = self.wait_for_xhr_request("t-person-username", plural=True)
-        self.assertEqual(self.lorem[-1], usernames[0].text)
         # Hard refresh OK b/c saved in the DB
         self.driver.refresh()
         self.wait_for_xhr_request("t-reset-grid").click()
         self.driver.find_element_by_link_text(search_name).click()
         usernames = self.wait_for_xhr_request("t-person-username", plural=True)
         self.assertEqual(self.lorem[-1], usernames[0].text)
-
-    def test_records_page(self):
-        people = self.wait_for_xhr_request("t-grid-data", plural=True)
-        self.assertEqual(len(people), 10)
-        self.driver.find_element_by_link_text("10 per page").click()
-
 
 
 if __name__ == "__main__":
--- ember_build.sh
+++ ember_build.sh
@@ -2,9 +2,14 @@ cd bsrs-ember
 ./node_modules/ember-cli/bin/ember build --env=production
 
 wait
-cp -r dist/assets ../bsrs-django/bigsky/ember/assets
-cp -r dist/fonts ../bsrs-django/bigsky/ember/fonts
-cp -r dist/index.html ../bsrs-django/bigsky/templates
-
 cd ../bsrs-django/bigsky/
+
+rm -rf ember/*
+rm -rf static/*
+rm -rf templates/index.html
+
+cp -r ../../bsrs-ember/dist/assets ember/assets
+cp -r ../../bsrs-ember/dist/fonts ember/fonts
+cp -r ../../bsrs-ember/dist/index.html templates
+
 ./manage.py collectstatic --noinput
