4647808790d30a4d6b9c83c0c5001d15fe6a4471
aronysidoro <pyaaron@gmail.com>
2015-10-02 09:36:17 -0700

    [PYTHON 3] README.md section added.

--- clear_django_static.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -lx
-
-echo "MUST RUN FROM '../bsrs/' DIR LEVEL!"
-
-cd bsrs-django/bigsky
-
-rm -rf ember/*
-rm -rf static/*
--- ember_build.sh
+++ ember_build.sh
@@ -1,10 +1,5 @@
 cd bsrs-ember
 ./node_modules/ember-cli/bin/ember build --env=production
-wait
-rm -rf -rf assets
-rm -rf -rf templates/index.html
-wait
-rm -rf ../bsrs-django/bigsky/ember/*
 
 wait
 cp -r dist/assets ../bsrs-django/bigsky/ember/assets
@@ -12,5 +7,4 @@ cp -r dist/fonts ../bsrs-django/bigsky/ember/fonts
 cp -r dist/index.html ../bsrs-django/bigsky/templates
 
 cd ../bsrs-django/bigsky/
-rm -rf static/*
 ./manage.py collectstatic --noinput
