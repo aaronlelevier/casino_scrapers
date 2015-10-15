78c754808ce6c6143a98ffe24fc8211e0210c3f8
toran billups <toranb@gmail.com>
2015-10-15 09:01:08 -0500

    [TICKET]: moved the equal helper out of the model

--- bsrs-ember/app/models/ticket.js
+++ bsrs-ember/app/models/ticket.js
@@ -2,14 +2,7 @@ import Ember from 'ember';
 import { attr, Model } from 'ember-cli-simple-store/model';
 import inject from 'bsrs-ember/utilities/store';
 import injectUUID from 'bsrs-ember/utilities/uuid';
-
-//kill this later?
-function equal(first, second) {
-    if (first instanceof Array && second instanceof Array) {
-        return Ember.$(first).not(second).get().length === 0 && Ember.$(second).not(first).get().length === 0;
-    }
-    return first === second;
-}
+import equal from 'bsrs-ember/utilities/equal';
 
 var TicketModel = Model.extend({
     store: inject('main'),
