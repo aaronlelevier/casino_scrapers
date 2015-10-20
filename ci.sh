e6ea58a92a7f8cad27c88ce0c36ce34ce674413e
aronysidoro <pyaaron@gmail.com>
2015-10-19 17:17:46 -0700

    /mnt/tmpfs/ for 'build' ci.sh

--- ci.sh
+++ ci.sh
@@ -153,6 +153,12 @@ function runSeleniumTests {
     fi
 }
 
+if ! [ "$(uname)" == "Darwin" ]; then
+  mkdir build
+  cd build/
+  echo `pwd`
+fi
+
 echo $(date -u) "NPM INSTALL"
 cd bsrs-ember
 npmInstall
