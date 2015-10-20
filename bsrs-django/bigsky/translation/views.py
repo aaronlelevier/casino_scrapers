32da85769fadc4a24c61292b3911d31c6699429c
aronysidoro <pyaaron@gmail.com>
2015-10-19 18:30:34 -0700

    translations

--- bsrs-django/bigsky/translation/views.py
+++ bsrs-django/bigsky/translation/views.py
@@ -1,5 +1,4 @@
 from rest_framework import viewsets
-from rest_framework.decorators import list_route
 from rest_framework.exceptions import NotFound
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -21,20 +20,11 @@ class LocaleViewSet(viewsets.ModelViewSet):
 
 class TranslationViewSet(viewsets.ModelViewSet):
     '''
-    The `list` endpoint, which displays all available Translation keys, 
-    uses the `en` locale for all available Keys. *(Main English Language 
-    Locale)*.  This is for efficiency, because otherwise we would need 
-    to compare all available Translations for all Keys each time.
-
     ## Filters
 
        **1. Filter by Locale.name:**
 
-       URL: `/api/translations/bootstrap/{locale}/`
-
-       **2. All Translations for a single Translation Key:**
-
-       URL: `/api/translations/{translation-key}/`
+       URL: `/api/translations/?locale=en`
     '''
     permission_classes = (IsAuthenticated,)
     serializer_class = TranslationSerializer
@@ -42,7 +32,7 @@ class TranslationViewSet(viewsets.ModelViewSet):
 
     def get_paginated_response(self, data):
         """
-        The response should always be an object.  The key each item in the 
+        The response should alwasy be an object.  The key each item in the 
         object is the Locale, and the value is the Translation ``key:value`` 
         object for that Translation.
         """
@@ -51,35 +41,13 @@ class TranslationViewSet(viewsets.ModelViewSet):
         else:
             raise NotFound
 
-    def list(self, request):
-        try:
-            translation = Translation.objects.get(locale__locale='en')
-        except Translation.DoesNotExist:
-            return Response([])
-        return Response(sorted(translation.values.keys()))
-
-    @list_route(methods=['GET'], url_path=r"bootstrap/(?P<locale>[\w\-\_]+)")
-    def bootstrap(self, request, locale=None):
+    def get_queryset(self):
         queryset = Translation.objects.all()
+        locale = self.request.query_params.get('locale', None)
         if locale is not None:
             queryset = queryset.filter(
                 locale__locale__istartswith=locale[:2]
                 ).extra(
                 where=["CHAR_LENGTH(locale) <= {}".format(len(locale))]
                 )
-        serializer = TranslationSerializer(queryset, many=True)
-        return Response(serializer.data)
-
-    @list_route(methods=['GET'], url_path=r"(?P<key>[\w\.]+)")
-    def translation_key(self, request, key=None):
-        translations = Translation.objects.filter(values__has_key=key)
-        return Response([{
-            'id': str(t.id),
-            'translations': {
-                'locale': str(t.locale.id),
-                'text': t.values.get(key, ""),
-                'helper': t.context.get(key, "") if t.context else ""
-            }
-        } for t in translations])
-
-
+        return queryset
