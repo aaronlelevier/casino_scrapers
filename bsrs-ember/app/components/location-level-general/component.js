ba5ea7404360e0b040f9f3041bc352051a1824bb
toran billups <toranb@gmail.com>
2015-10-27 11:27:14 -0500

    [LOCATION-LEVEL]: consolidate new and detail components

---
+++ bsrs-ember/app/components/location-level-general/component.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+import inject from 'bsrs-ember/utilities/inject';
+import EditMixin from 'bsrs-ember/mixins/components/tab/edit';
+import TabMixin from 'bsrs-ember/mixins/components/tab/base';
+import {ValidationMixin, validate} from 'ember-cli-simple-validation/mixins/validate';
+import prevent_duplicate_name from 'bsrs-ember/validation/prevent_duplicate_name';
+
+var LocationLevelGeneral = Ember.Component.extend(TabMixin, EditMixin, ValidationMixin, {
+    repository: inject('location-level'),
+    classNames: ['wrapper', 'form'],
+    nameValidation: validate('model.name', prevent_duplicate_name),
+    actions: {
+        save() {
+            this.set('submitted', true);
+            if (this.get('valid')) {
+                this._super();
+            }
+        },
+    }
+});
+
+export default LocationLevelGeneral;
