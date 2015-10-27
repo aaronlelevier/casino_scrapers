753055936f2699a8f19a0ae6e12c2f42d84d4209
aronysidoro <pyaaron@gmail.com>
2015-10-27 12:23:00 -0700

    [NGINX W/ UNIX SOCK] deploy.sh / persistent.sh

--- deploy.sh
+++ deploy.sh
@@ -118,10 +118,13 @@ cd ../../builds/deploy/
 
 wait
 echo "UWSGI - START/RELOAD"
-sudo fuser -k 8002/tcp
+sudo kill -INT `cat /var/run/nginx-deploy.pid`
+wait
+sudo rm /var/run/nginx-deploy.sock
+wait
+sudo rm /var/run/nginx-deploy.pid
 wait
 sudo /usr/local/lib/deploy/uwsgi/uwsgi --ini uwsgi.ini
-sudo touch /tmp/bigsky-master-deploy.pid
 TEST=$?; if [ "$TEST" == 1 ]; then echo "uwsgi failed"; exit $TEST; fi
 
 
