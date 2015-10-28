291ff4952644ff224e5ab3caf48d3ab452fa84bb
snewcomer <snewcomer24@gmail.com>
2015-10-28 15:25:24 -0700

    [FIX]

--- bsrs-ember/tests/unit/deserializers/role-test.js
+++ bsrs-ember/tests/unit/deserializers/role-test.js
@@ -48,7 +48,7 @@ test('location level and category will correctly be deserialized into its own st
     let category = store.find('category', CATEGORY_DEFAULTS.idOne);
     assert.deepEqual(role.get('role_category_fks'), []);
     let role_two = store.find('role', ROLE_DEFAULTS.unusedId);
-    assert.deepEqual(role_two.get('role_category_fks'), ['abc123']);
+    assert.deepEqual(role_two.get('role_category_fks').length, 1);
     assert.ok(role.get('isNotDirty'));
     assert.ok(role_two.get('isNotDirty'));
     assert.ok(category.get('isNotDirty'));
@@ -153,7 +153,7 @@ test('role category will correctly be deserialized when server returns role with
     subject.deserialize(response, ROLE_DEFAULTS.idOne);
     let original = store.find('category', CATEGORY_DEFAULTS.idOne);
     assert.ok(role.get('isNotDirty'));
-    assert.deepEqual(role.get('role_category_fks'), [ROLE_CATEGORY_DEFAULTS.abc, ROLE_CATEGORY_DEFAULTS.idOne]);
+    assert.deepEqual(role.get('role_category_fks').length, 2);
     assert.equal(role.get('categories').get('length'), 2);
 });
 
