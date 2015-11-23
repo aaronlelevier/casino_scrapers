d13b407afac24bd4914120d4271cb5f9d1c0a9c7
snewcomer <snewcomer24@gmail.com>
2015-11-22 16:23:24 -0800

    [GRID]: switch to v2 for all modules. Only includes closure actions and ES6 updates

--- bsrs-ember/app/components/person-list/component.js
+++ bsrs-ember/app/components/person-list/component.js
@@ -1,7 +1,7 @@
-import GridViewComponent from 'bsrs-ember/components/grid-view/component';
+import GridViewComponent from 'bsrs-ember/components/grid-view-2/component';
 
 export default GridViewComponent.extend({
-    layoutName: 'components/grid-view',
+    layoutName: 'components/grid-view-2',
     searchable: ['fullname', 'username', 'title'],
     nonsearchable: ['role', 'employee_id']
 });
--- bsrs-ember/app/components/tickets/ticket-list/component.js
+++ bsrs-ember/app/components/tickets/ticket-list/component.js
@@ -1,7 +1,7 @@
-import GridViewComponent from 'bsrs-ember/components/grid-view/component';
+import GridViewComponent from 'bsrs-ember/components/grid-view-2/component';
 
 export default GridViewComponent.extend({
-    layoutName: 'components/grid-view',
+    layoutName: 'components/grid-view-2',
     searchable: ['request', 'priority'],
     related_fields: [{'model': 'priority', 'field': 'name'}],
     nonsearchable: ['status']
