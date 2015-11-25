a2f1339eb186ce959da4dbcd63e1c340512eaba3
aronysidoro <pyaaron@gmail.com>
2015-11-25 10:16:41 -0800

    [MAX_UPLOAD_SIZE] add the same max upload size in Django as configured in Nginx.

--- bsrs-django/bigsky/bigsky/settings/base.py
+++ bsrs-django/bigsky/bigsky/settings/base.py
@@ -171,6 +171,15 @@ FILES_SUB_PATH = "attachments/files"
 IMAGE_FULL_SUB_PATH = "attachments/images/full"
 IMAGE_MEDIUM_SUB_PATH = "attachments/images/medium"
 IMAGE_THUMBNAIL_SUB_PATH = "attachments/images/thumbnail"
+# 2.5MB - 2621440
+# 5MB   - 5242880
+# 10MB  - 10485760
+# 20MB  - 20971520
+# 50MB  - 52428800
+# 100MB - 104857600
+# 250MB - 214958080
+# 500MB - 429916160
+MAX_UPLOAD_SIZE = "5242880"
 
 ### LOCATION
 DEFAULT_LOCATION_STATUS = 'Open'
