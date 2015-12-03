80ec384e4e5152018ec2aa21f3a44ac3595482df
aronysidoro <pyaaron@gmail.com>
2015-12-03 14:13:01 -0800

    [LOGGING] initial config. WIP to add features or tweaks for emailing errors to admins, but works to log errors to a single fie.

--- bsrs-django/bigsky/bigsky/settings/base.py
+++ bsrs-django/bigsky/bigsky/settings/base.py
@@ -199,3 +199,50 @@ THIRD_PARTY_STATUS_DEFAULT = "active"
 ### TICKETS
 ACTIVITY_DEFAULT_WEIGHT = 4
 DEFAULTS_TICKET_STATUS = "ticket.status.new" 
+
+
+### EMAIL ###
+# django native settings for ``django.core.mail.mail_admins()``
+# EMAIL_HOST_USER
+# EMAIL_HOST_PASSWORD
+
+
+### LOGGING ###
+LOGGING_DIR = os.path.join(os.path.dirname(BASE_DIR), "log") # ../bsrs-django/log/
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(LOGGING_DIR, 'debug.log'),
+            'formatter': 'verbose'
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+        'bigsky': {
+            'handlers': ['file'],
+            'level': 'DEBUG'
+        }
+    }
+}
