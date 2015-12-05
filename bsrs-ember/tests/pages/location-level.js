f67c773ffc794323b9092d388eb8035e02668f87
snewcomer <snewcomer24@gmail.com>
2015-12-05 10:56:29 -0800

    [LOCATION LEVEL]: children select converted to power select and lots of cleanup

--- bsrs-ember/tests/pages/location-level.js
+++ bsrs-ember/tests/pages/location-level.js
@@ -1,9 +1,26 @@
 import PageObject from '../page-object';
+import LLD from 'bsrs-ember/vendor/defaults/location-level';
 
-let {
-  visitable
-} = PageObject;
+let { text, visitable, fillable, clickable, count } = PageObject;
+const CHILDREN = '.t-location-level-children-select > .ember-basic-dropdown-trigger';
+const CHILDREN_MULTIPLE = `${CHILDREN} > .ember-power-select-multiple-option`;
+const CHILDREN_ONE = `${CHILDREN_MULTIPLE}:eq(0)`;
+const CHILDREN_DROPDOWN = '.t-location-level-children-select-dropdown > .ember-power-select-options';
 
 export default PageObject.create({
-  visit: visitable('/')
+  visit: visitable('/'),
+  childrenClickDropdown: clickable(CHILDREN),
+  childrenSelected: text(CHILDREN_ONE),
+  childrenOneRemove: clickable(`${CHILDREN_ONE} > .ember-power-select-multiple-remove-btn`),
+  childrenClickOptionStore: clickable(`${CHILDREN_DROPDOWN} > .ember-power-select-option:contains(${LLD.nameStoreUn})`),
+  childrenClickOptionCompany: clickable(`${CHILDREN_DROPDOWN} > .ember-power-select-option:contains(${LLD.nameCompany})`),
+  childrenClickOptionDepartment: clickable(`${CHILDREN_DROPDOWN} > .ember-power-select-option:contains(${LLD.nameDepartment})`),
+  childrenClickOptionRegion: clickable(`${CHILDREN_DROPDOWN} > .ember-power-select-option:contains(${LLD.nameRegion})`),
+  childrenClickOptionLossPreventionDistrict: clickable(`${CHILDREN_DROPDOWN} > .ember-power-select-option:contains(${LLD.lossPreventionDistrict})`),
+  childrenClickOptionLossPreventionRegion: clickable(`${CHILDREN_DROPDOWN} > .ember-power-select-option:contains(${LLD.nameLossPreventionRegion})`),
+  childrenClickOptionDistrict: clickable(`${CHILDREN_DROPDOWN} > .ember-power-select-option:contains(${LLD.nameDistrict})`),
+  childrenClickOptionFacilityManagement: clickable(`${CHILDREN_DROPDOWN} > .ember-power-select-option:contains(${LLD.nameFacilityManagement})`),
+  childrenOptionLength: count(`${CHILDREN_DROPDOWN} > li`),
+  categoriesSelected: count(CHILDREN_MULTIPLE),
+  childrenTwoClickOptionOne: clickable(`${CHILDREN_DROPDOWN} > .ember-power-select-option:contains(${LLD.nameTwo})`),
 });
