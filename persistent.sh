f9d47aaea0123f744e612a8063a8aebb4f93793d
aronysidoro <pyaaron@gmail.com>
2015-12-07 09:45:45 -0800

    [PERSISTENT] break-up persistent fixtures to mirror 'deploy' except load with only 1 person and no ticket fixtures.

--- persistent.sh
+++ persistent.sh
@@ -64,8 +64,14 @@ TEST=$?; if [ "$TEST" == 1 ]; then echo "migrate failed"; exit $TEST; fi
 
 echo "AFTER MIGRATIONS, LOAD LATEST FIXTURE DATA."
 wait
+../venv/bin/python manage.py loaddata fixtures/location.State.json
 ../venv/bin/python manage.py loaddata fixtures/translation.json
-../venv/bin/python manage.py loaddata fixtures/persistent/persistent.json
+../venv/bin/python manage.py loaddata fixtures/accounting.Currency.json
+../venv/bin/python manage.py loaddata fixtures/contact.PhoneNumberType.json
+../venv/bin/python manage.py loaddata fixtures/contact.AddressType.json
+../venv/bin/python manage.py loaddata fixtures/auth.json
+../venv/bin/python manage.py loaddata fixtures/location.json
+../venv/bin/python manage.py loaddata fixtures/persistent/person.json
 TEST=$?; if [ "$TEST" == 1 ]; then echo "load fixture failed"; exit $TEST; fi
 
 
