cc7ad373886cf0beccedcc296cb032cf777d4515
snewcomer <snewcomer24@gmail.com>
2015-12-11 11:50:27 -0800

    [TICKET]: dirty tracking for request

--- bsrs-ember/app/components/tickets/ticket-single/component.js
+++ bsrs-ember/app/components/tickets/ticket-single/component.js
@@ -9,6 +9,7 @@ import ParentValidationComponent from 'bsrs-ember/mixins/validation/parent';
 var TicketSingleComponent = ParentValidationComponent.extend(RelaxedMixin, TabMixin, EditMixin, {
     child_components: ['parent-ticket-category-select'],
     repository: inject('ticket'),
+    requestValidation: validate('model.request'),
     numberValidation: validate('model.number'),
     assigneeValidation: validate('model.assignee'),
     priorityValidation: validate('model.priority'),
