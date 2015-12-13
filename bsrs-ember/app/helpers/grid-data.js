8fbdee3ca17d33b04f34e7c316654c5c823e71c7
snewcomer <snewcomer24@gmail.com>
2015-12-12 16:13:40 -0800

    [POWER SELECT]: revert until figure out how to pass tests...believe related to search

--- bsrs-ember/app/helpers/grid-data.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-import { translationMacro as t } from 'ember-i18n';
-
-export function gridData(params/*, hash*/) {
-    return params[0].get(params[1]);
-}
-
-export default Ember.Helper.helper(gridData);
