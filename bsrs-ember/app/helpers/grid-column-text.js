7a65b804c4e6e1d22fb5a668539dad81d57aaf22
toran billups <toranb@gmail.com>
2015-12-15 16:45:58 -0600

    [REFACTOR]: extracted a regex core for array/non array based property lookups

--- bsrs-ember/app/helpers/grid-column-text.js
+++ bsrs-ember/app/helpers/grid-column-text.js
@@ -1,22 +1,26 @@
 import Ember from 'ember';
+import regex_core from 'bsrs-ember/utilities/array/regex-core';
+
+var array_func = function(object, property) {
+    let result;
+    const prop = property.match(/^[^\[]+/);
+    const all = object.get(prop[0]) || [];
+    all.forEach((model, index) => {
+        const value = model.get(this) || '';
+        result = index > 0 ? `${result} &#8226; ${value}` : value;
+    });
+    const legit = all.length > 0 && result.replace(/&#8226;/g, '').trim().length > 0;
+    return legit ? Ember.String.htmlSafe(result) : '';
+};
+
+var func = function(object, property) {
+    return object.get(property) || '';
+};
 
 var GridColumnText = Ember.Helper.helper((params) => {
-    let attr;
     const object = params[0];
     const property = params[1];
-    const array = /\[([^\]]*)\]/g;
-    while (attr = array.exec(property)) {
-        let result;
-        const prop = property.match(/^[^\[]+/);
-        object.get(prop[0]).forEach((model, index) => {
-            const value = model.get(attr[1]);
-            result = index > 0 ? `${result} &#8226; ${value}` : value;
-        });
-        return Ember.String.htmlSafe(result);
-    }
-    return object.get(property);
+    return regex_core(object, property, array_func, func);
 });
-//TODO: make this forgiving (ie- not blow up)
-//TODO: share code here w/ the regex_property
 
 export default GridColumnText;
--- bsrs-ember/tests/unit/helpers/grid-column-text-test.js
+++ bsrs-ember/tests/unit/helpers/grid-column-text-test.js
@@ -24,3 +24,32 @@ test('related array property will return correct value', function(assert) {
   const model = Ember.Object.create({categories: Ember.A([one, two, three])});
   assert.equal(GridColumnText.compute([model, 'categories[name]']), 'ab &#8226; cd &#8226; ef');
 });
+
+test('helper is forgiving when model property not found', function(assert) {
+  const model = Ember.Object.create({name: 'wat'});
+  const result = GridColumnText.compute([model, 'yoyo']);
+  assert.equal(result, '');
+});
+
+test('helper is forgiving when related property not found', function(assert) {
+  const related = Ember.Object.create({name: 'zap'});
+  const model = Ember.Object.create({foo: related});
+  const result = GridColumnText.compute([model, 'foo.blah']);
+  assert.equal(result, '');
+});
+
+test('helper is forgiving when related array property not found', function(assert) {
+  const one = Ember.Object.create({name: 'ab'});
+  const two = Ember.Object.create({name: 'cd'});
+  const three = Ember.Object.create({name: 'ef'});
+  const model = Ember.Object.create({categories: Ember.A([one, two, three])});
+  assert.equal(GridColumnText.compute([model, 'categories[huh]']), '');
+});
+
+test('helper is forgiving when related array property incorrect', function(assert) {
+  const one = Ember.Object.create({name: 'ab'});
+  const two = Ember.Object.create({name: 'cd'});
+  const three = Ember.Object.create({name: 'ef'});
+  const model = Ember.Object.create({categories: Ember.A([one, two, three])});
+  assert.equal(GridColumnText.compute([model, 'shoes[name]']), '');
+});
