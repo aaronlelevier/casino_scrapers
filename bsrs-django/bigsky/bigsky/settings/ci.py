7d1b92b61acfe16d339ba141de5ece1a254dbc7a
toran billups <toranb@gmail.com>
2015-12-21 13:15:26 -0600

    [BUILD]: django test build now using isolated psql database

--- bsrs-django/bigsky/bigsky/settings/ci.py
+++ bsrs-django/bigsky/bigsky/settings/ci.py
@@ -22,3 +22,14 @@ third_party,ticket,translation,utils,work_request',
 ### ATTACHMENTS
 DEFAULT_TEST_FILE = os.path.join(BASE_DIR, "source/test_in/aaron.jpeg")
 DEFAULT_TEST_IMAGE = os.path.join(BASE_DIR, "source/test_in/es.csv")
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'test',
+        'USER': 'bsdev',
+        'PASSWORD': 'tango',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
