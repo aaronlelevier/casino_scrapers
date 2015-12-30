89ed8f5c37e6e40d5e2a82e8579d37c196c719d4
toran billups <toranb@gmail.com>
2015-12-30 13:53:50 -0600

    [EMBER]: first pass at the simple-store v4 upgrade (category-repo-test)

--- bsrs-ember/tests/unit/repositories/category-test.js
+++ bsrs-ember/tests/unit/repositories/category-test.js
@@ -7,7 +7,7 @@ import CategoryRepository from 'bsrs-ember/repositories/category';
 import CategoryDeserializer from 'bsrs-ember/deserializers/category';
 import CATEGORY_DEFAULTS from 'bsrs-ember/vendor/defaults/category';
 
-var store, original_xhr;
+var store, original_xhr, run = Ember.run;
 
 module('unit: category repository test', {
     beforeEach() {
@@ -25,51 +25,63 @@ module('unit: category repository test', {
 });
 
 test('findCategoryChildren will format url correctly for search criteria and return correct categories that are already present in store', (assert) => {
-    store.push('category', {id: CATEGORY_DEFAULTS.idOne, name: 'abc'});
-    store.push('category', {id: CATEGORY_DEFAULTS.idTwo, name: 'abcd'});
-    store.push('category', {id: CATEGORY_DEFAULTS.unusedId, name: 'xyz'});
-    store.push('category', {id: CATEGORY_DEFAULTS.anotherId, name: 'mmm'});
+    run(function() {
+        store.push('category', {id: CATEGORY_DEFAULTS.idOne, name: 'abc'});
+        store.push('category', {id: CATEGORY_DEFAULTS.idTwo, name: 'abcd'});
+        store.push('category', {id: CATEGORY_DEFAULTS.unusedId, name: 'xyz'});
+        store.push('category', {id: CATEGORY_DEFAULTS.anotherId, name: 'mmm'});
+    });
     let subject = CategoryRepository.create({store: store});
     let category_array_proxy = subject.findCategoryChildren('abc');
     assert.equal(category_array_proxy.get('length'), 2);
 });
 
 test('findCategoryChildren will lower case search', (assert) => {
-    store.push('category', {id: CATEGORY_DEFAULTS.idOne, name: 'abc'});
-    store.push('category', {id: CATEGORY_DEFAULTS.idTwo, name: 'abcd'});
-    store.push('category', {id: CATEGORY_DEFAULTS.unusedId, name: 'xyz'});
-    store.push('category', {id: CATEGORY_DEFAULTS.anotherId, name: 'mmm'});
+    run(function() {
+        store.push('category', {id: CATEGORY_DEFAULTS.idOne, name: 'abc'});
+        store.push('category', {id: CATEGORY_DEFAULTS.idTwo, name: 'abcd'});
+        store.push('category', {id: CATEGORY_DEFAULTS.unusedId, name: 'xyz'});
+        store.push('category', {id: CATEGORY_DEFAULTS.anotherId, name: 'mmm'});
+    });
     let subject = CategoryRepository.create({store: store});
     let category_array_proxy = subject.findCategoryChildren('Abc');
     assert.equal(category_array_proxy.get('length'), 2);
 });
 
 test('findCategoryChildren will return categories without new flag', (assert) => {
-    store.push('category', {id: CATEGORY_DEFAULTS.idOne, name: 'abc', new: true});
-    store.push('category', {id: CATEGORY_DEFAULTS.idTwo, name: 'abcd'});
-    store.push('category', {id: CATEGORY_DEFAULTS.unusedId, name: 'xyz'});
-    store.push('category', {id: CATEGORY_DEFAULTS.anotherId, name: 'mmm'});
+    run(function() {
+        store.push('category', {id: CATEGORY_DEFAULTS.idOne, name: 'abc', new: true});
+        store.push('category', {id: CATEGORY_DEFAULTS.idTwo, name: 'abcd'});
+        store.push('category', {id: CATEGORY_DEFAULTS.unusedId, name: 'xyz'});
+        store.push('category', {id: CATEGORY_DEFAULTS.anotherId, name: 'mmm'});
+    });
     let subject = CategoryRepository.create({store: store});
     let category_array_proxy = subject.findCategoryChildren('Abc');
     assert.equal(category_array_proxy.get('length'), 1);
 });
 
 test('findTopLevelCategories will format url correctly for search criteria and return correct categories that are already present in store', (assert) => {
-    store.push('category', {id: CATEGORY_DEFAULTS.idOne, name: 'abc'});
-    store.push('category', {id: CATEGORY_DEFAULTS.idTwo, name: 'abcd'});
-    store.push('category', {id: CATEGORY_DEFAULTS.unusedId, name: 'xyz', parent_id: CATEGORY_DEFAULTS.idTwo});
-    store.push('category', {id: CATEGORY_DEFAULTS.anotherId, name: 'mmm', parent_id: CATEGORY_DEFAULTS.idOne});
+    run(function() {
+        store.push('category', {id: CATEGORY_DEFAULTS.idOne, name: 'abc'});
+        store.push('category', {id: CATEGORY_DEFAULTS.idTwo, name: 'abcd'});
+        store.push('category', {id: CATEGORY_DEFAULTS.unusedId, name: 'xyz', parent_id: CATEGORY_DEFAULTS.idTwo});
+        store.push('category', {id: CATEGORY_DEFAULTS.anotherId, name: 'mmm', parent_id: CATEGORY_DEFAULTS.idOne});
+    });
     let subject = CategoryRepository.create({store: store});
     let category_array_proxy = subject.findTopLevelCategories();
     assert.equal(category_array_proxy.get('length'), 2);
 });
 
 test('findTopLevelCategories will exclude children when parent category is not yet loaded', (assert) => {
-    store.push('category', {id: CATEGORY_DEFAULTS.anotherId, name: 'mmm', parent_id: CATEGORY_DEFAULTS.idOne});
+    run(function() {
+        store.push('category', {id: CATEGORY_DEFAULTS.anotherId, name: 'mmm', parent_id: CATEGORY_DEFAULTS.idOne});
+    });
     let subject = CategoryRepository.create({store: store});
     let category_array_proxy = subject.findTopLevelCategories();
     assert.equal(category_array_proxy.get('length'), 0);
-    store.push('category', {id: CATEGORY_DEFAULTS.idOne, name: 'abc'});
+    run(function() {
+        store.push('category', {id: CATEGORY_DEFAULTS.idOne, name: 'abc'});
+    });
     assert.equal(category_array_proxy.get('length'), 1);
     assert.equal(category_array_proxy.objectAt(0).get('id'), CATEGORY_DEFAULTS.idOne);
 });
