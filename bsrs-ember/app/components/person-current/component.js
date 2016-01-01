ec3b517ccee3b4f2e4b1a571cb2dc4078142a139
andykrier <andykrier@gmail.com>
2015-12-31 16:54:32 -0800

    [LOGOUT] Updated logout to use a standard $.get as the promise mixin was expecting a JSON response.

--- bsrs-ember/app/components/person-current/component.js
+++ bsrs-ember/app/components/person-current/component.js
@@ -5,19 +5,30 @@ import windowProxy from 'bsrs-ember/utilities/window-proxy';
 import config from 'bsrs-ember/config/environment';
 
 export default Ember.Component.extend({
-  store: inject('main'),
-  tagName: 'li',
-  classNames: ['current-user t-current-user'],
-  personCurrent: Ember.inject.service(),
-  instance: Ember.computed(function(){
-    var service = this.get('personCurrent');
-    return service.get('model');
-  }),
-  actions: {
-    logout() {
-      PromiseMixin.xhr('/api-auth/logout/', 'GET').then(() => {
-          windowProxy.changeLocation('/login');
-      });
+    store: inject('main'),
+    tagName: 'li',
+    classNames: ['current-user t-current-user'],
+    personCurrent: Ember.inject.service(),
+    instance: Ember.computed(function(){
+        var service = this.get('personCurrent');
+        return service.get('model');
+    }),
+    actions: {
+        logout() {
+            Ember.$.get('/api-auth/logout/', function(data){
+                windowProxy.changeLocation('/login');
+            });
+            /*
+            TODO:
+
+            The ember promise mixin expects JSON back
+            Django is currently returning html
+
+            PromiseMixin.xhr('/api-auth/logout/', 'GET').then(() => {
+                windowProxy.changeLocation('/login');
+            });
+            */
+
+        }
     }
-  }
 });
