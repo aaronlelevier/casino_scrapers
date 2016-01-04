cabf6a0b228ca9b0e14b4a7a48bc0880f4e69e63
aronysidoro <pyaaron@gmail.com>
2016-01-04 14:25:26 -0800

    [EMBER URL REDIRECTS] 'logout' or a  '403'

--- bsrs-ember/app/utilities/error-function.js
+++ bsrs-ember/app/utilities/error-function.js
@@ -2,7 +2,7 @@ import windowProxy from 'bsrs-ember/utilities/window-proxy';
 
 var errorFunction = (xhr, textStatus, errorThrown, message) => {
     if (xhr.status === 403) {
-        windowProxy.changeLocation('/login');
+        windowProxy.changeLocation('/login/');
     }
 };
 
