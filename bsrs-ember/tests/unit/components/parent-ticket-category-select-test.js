a974ec4cac9ee25243275e66d30fe3c28d9828b6
toran billups <toranb@gmail.com>
2016-01-06 16:09:29 -0600

    [EMBER]: unit test simplification effort v1

--- bsrs-ember/tests/unit/components/parent-ticket-category-select-test.js
+++ bsrs-ember/tests/unit/components/parent-ticket-category-select-test.js
@@ -17,31 +17,23 @@ module('unit: parent-ticket-category-select component test', {
 });
 
 test('component is not valid when the only category (on the ticket model) has children', (assert) => {
-    run(function() {
-        ticket = store.push('ticket', {id: TICKET_DEFAULTS.idOne});
-        category_one = store.push('category', {id: CATEGORY_DEFAULTS.idOne, name: CATEGORY_DEFAULTS.nameOne, parent_id: CATEGORY_DEFAULTS.idTwo, children_fks: []});
-        category_two = store.push('category', {id: CATEGORY_DEFAULTS.idTwo, name: CATEGORY_DEFAULTS.nameTwo, parent_id: CATEGORY_DEFAULTS.unusedId, children_fks: [CATEGORY_DEFAULTS.idOne]});
-        category_three = store.push('category', {id: CATEGORY_DEFAULTS.unusedId, name: CATEGORY_DEFAULTS.nameThree, parent_id: null, children_fks: [CATEGORY_DEFAULTS.idTwo]});
-    });
+    ticket = store.push('ticket', {id: TICKET_DEFAULTS.idOne});
+    category_one = store.push('category', {id: CATEGORY_DEFAULTS.idOne, name: CATEGORY_DEFAULTS.nameOne, parent_id: CATEGORY_DEFAULTS.idTwo, children_fks: []});
+    category_two = store.push('category', {id: CATEGORY_DEFAULTS.idTwo, name: CATEGORY_DEFAULTS.nameTwo, parent_id: CATEGORY_DEFAULTS.unusedId, children_fks: [CATEGORY_DEFAULTS.idOne]});
+    category_three = store.push('category', {id: CATEGORY_DEFAULTS.unusedId, name: CATEGORY_DEFAULTS.nameThree, parent_id: null, children_fks: [CATEGORY_DEFAULTS.idTwo]});
     let subject = ParentTicketCategorySelect.create({ticket: ticket, eventbus: eventbus});
     assert.equal(ticket.get('categories').get('length'), 0);
     let valid = subject.get('valid');
     assert.equal(valid, false);
-    Ember.run(function() {
-        ticket.change_category_tree(category_three.get('id'));
-    });
+    ticket.change_category_tree(category_three.get('id'));
     assert.equal(ticket.get('categories').get('length'), 1);
     valid = subject.get('valid');
     assert.equal(valid, false);
-    Ember.run(function() {
-        ticket.change_category_tree(category_two.get('id'));
-    });
+    ticket.change_category_tree(category_two.get('id'));
     assert.equal(ticket.get('categories').get('length'), 2);
     valid = subject.get('valid');
     assert.equal(valid, false);
-    Ember.run(function() {
-        ticket.change_category_tree(category_one.get('id'));
-    });
+    ticket.change_category_tree(category_one.get('id'));
     assert.equal(ticket.get('categories').get('length'), 3);
     assert.equal(category_three.get('has_many_children').get('length'), 1);
     assert.equal(category_two.get('has_many_children').get('length'), 1);
@@ -51,32 +43,24 @@ test('component is not valid when the only category (on the ticket model) has ch
 });
 
 test('sorted categories will start with the parent and end with the leaf child category', (assert) => {
-    run(function() {
-        ticket = store.push('ticket', {id: TICKET_DEFAULTS.idOne});
-        category_one = store.push('category', {id: CATEGORY_DEFAULTS.idOne, name: CATEGORY_DEFAULTS.nameOne, parent_id: CATEGORY_DEFAULTS.idTwo, children_fks: []});
-        category_two = store.push('category', {id: CATEGORY_DEFAULTS.idTwo, name: CATEGORY_DEFAULTS.nameTwo, parent_id: CATEGORY_DEFAULTS.unusedId, children_fks: [CATEGORY_DEFAULTS.idOne]});
-        category_three = store.push('category', {id: CATEGORY_DEFAULTS.unusedId, name: CATEGORY_DEFAULTS.nameThree, parent_id: null, children_fks: [CATEGORY_DEFAULTS.idTwo]});
-    });
+    ticket = store.push('ticket', {id: TICKET_DEFAULTS.idOne});
+    category_one = store.push('category', {id: CATEGORY_DEFAULTS.idOne, name: CATEGORY_DEFAULTS.nameOne, parent_id: CATEGORY_DEFAULTS.idTwo, children_fks: []});
+    category_two = store.push('category', {id: CATEGORY_DEFAULTS.idTwo, name: CATEGORY_DEFAULTS.nameTwo, parent_id: CATEGORY_DEFAULTS.unusedId, children_fks: [CATEGORY_DEFAULTS.idOne]});
+    category_three = store.push('category', {id: CATEGORY_DEFAULTS.unusedId, name: CATEGORY_DEFAULTS.nameThree, parent_id: null, children_fks: [CATEGORY_DEFAULTS.idTwo]});
     let subject = ParentTicketCategorySelect.create({ticket: ticket, eventbus: eventbus});
     assert.equal(ticket.get('categories').get('length'), 0);
     let valid = subject.get('valid');
     assert.equal(valid, false);
-    Ember.run(function() {
-        ticket.change_category_tree(category_three.get('id'));
-    });
+    ticket.change_category_tree(category_three.get('id'));
     assert.equal(ticket.get('categories').get('length'), 1);
     assert.equal(ticket.get('sorted_categories').get('length'), 1);
     assert.equal(ticket.get('sorted_categories').objectAt(0).get('id'), category_three.get('id'));
-    Ember.run(function() {
-        ticket.change_category_tree(category_two.get('id'));
-    });
+    ticket.change_category_tree(category_two.get('id'));
     assert.equal(ticket.get('categories').get('length'), 2);
     assert.equal(ticket.get('sorted_categories').get('length'), 2);
     assert.equal(ticket.get('sorted_categories').objectAt(0).get('id'), category_three.get('id'));
     assert.equal(ticket.get('sorted_categories').objectAt(1).get('id'), category_two.get('id'));
-    Ember.run(function() {
-        ticket.change_category_tree(category_one.get('id'));
-    });
+    ticket.change_category_tree(category_one.get('id'));
     assert.equal(ticket.get('categories').get('length'), 3);
     assert.equal(ticket.get('sorted_categories').get('length'), 3);
     assert.equal(ticket.get('sorted_categories').objectAt(0).get('id'), category_three.get('id'));
