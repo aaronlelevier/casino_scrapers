fdde9347597165044cd73241136b21c45f82cc80
toran billups <toranb@gmail.com>
2016-01-20 16:44:48 -0600

    [GRIDVIEW]: upgraded admin-translation to vNext

--- bsrs-ember/app/components/translations/translation-list/component.js
+++ bsrs-ember/app/components/translations/translation-list/component.js
@@ -2,6 +2,9 @@ import GridViewComponent from 'bsrs-ember/components/grid-view/component';
 
 export default GridViewComponent.extend({
     layoutName: 'components/grid-view',
-    searchable: ['key'],
-    nonsearchable: ['key']
+    version: 'vNext', //TODO: remove this after the grid-view upgrade is complete
+    columns: [
+        {field: 'key', headerLabel: 'Key', isSortable: true, isFilterable: true, isSearchable: true},
+        {field: 'key', headerLabel: 'Key', isTranslatable: true}
+    ]
 });
