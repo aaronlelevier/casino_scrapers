016de4d3c393924b45e7b7b01dd5b7dddb016c75
toran billups <toranb@gmail.com>
2016-01-21 15:36:21 -0600

    [GRIDVIEW]: upgraded third-party to vNext

--- bsrs-ember/app/components/third-parties/component.js
+++ bsrs-ember/app/components/third-parties/component.js
@@ -2,6 +2,10 @@ import GridViewComponent from 'bsrs-ember/components/grid-view/component';
 
 export default GridViewComponent.extend({
     layoutName: 'components/grid-view',
-    searchable: ['name', 'status.name'],
-    nonsearchable: ['number']
+    version: 'vNext', //TODO: remove this after the grid-view upgrade is complete
+    columns: [
+        {field: 'name', headerLabel: 'Name', isSortable: true, isFilterable: true, isSearchable: true},
+        {field: 'status.name', headerLabel: 'Status', isSortable: true, isFilterable: true, isSearchable: true},
+        {field: 'number', headerLabel: 'Number'}
+    ]
 });
