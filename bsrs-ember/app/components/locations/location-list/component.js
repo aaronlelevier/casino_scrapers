110dbef1ae9444824ab93d23f8d3aba8f1ca5eb6
toran billups <toranb@gmail.com>
2016-01-21 16:08:03 -0600

    [GRIDVIEW]: upgraded location to vNext

--- bsrs-ember/app/components/locations/location-list/component.js
+++ bsrs-ember/app/components/locations/location-list/component.js
@@ -3,6 +3,11 @@ import GridViewComponent from 'bsrs-ember/components/grid-view/component';
 
 export default GridViewComponent.extend({
     layoutName: 'components/grid-view',
-    searchable: ['name', 'number', 'status.translated_name'],
-    nonsearchable: ['location_level']
+    version: 'vNext', //TODO: remove this after the grid-view upgrade is complete
+    columns: [
+        {field: 'name', headerLabel: 'Name', isSortable: true, isFilterable: true, isSearchable: true},
+        {field: 'number', headerLabel: 'Number', isSortable: true, isFilterable: true, isSearchable: true},
+        {field: 'status.translated_name', headerLabel: 'Status', isSortable: true, isFilterable: true, isSearchable: true},
+        {field: 'location_level', headerLabel: 'Organization'}
+    ]
 });
