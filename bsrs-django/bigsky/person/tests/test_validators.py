905693ecf07ace021b8f85812ddf2f3e05752de9
aronysidoro <pyaaron@gmail.com>
2016-01-22 15:42:37 -0800

    [PERSON] refactor validation test file location

--- bsrs-django/bigsky/person/tests/test_validators.py
+++ bsrs-django/bigsky/person/tests/test_validators.py
@@ -1,10 +1,62 @@
+import json
 import uuid
 
+from model_mommy import mommy
 from rest_framework.test import APITestCase
 
 from category.tests.factory import create_single_category
-from person.serializers import RoleCreateSerializer
+from location.models import Location, LocationLevel
+from location.tests.factory import create_location
+from person.models import Person
+from person.serializers import RoleCreateSerializer, PersonUpdateSerializer
 from person.tests.factory import PASSWORD, create_single_person, create_role
+
+
+class RoleLocationValidatorTests(APITestCase):
+
+    def setUp(self):
+        self.role = create_role()
+        self.location = create_location(location_level=self.role.location_level)
+        self.person = create_single_person()
+        # data
+        serializer = PersonUpdateSerializer(self.person)
+        self.data = serializer.data
+        # login
+        self.client.login(username=self.person.username, password=PASSWORD)
+
+    def tearDown(self):
+        self.client.logout()
+
+    def test_location_level_not_equal_init_role(self):
+        location_level = mommy.make(LocationLevel)
+        location = mommy.make(Location, location_level=location_level)
+        self.assertNotEqual(self.person.role.location_level, location_level)
+        self.data['locations'].append(str(location.id))
+
+        response = self.client.put('/api/admin/people/{}/'.format(self.person.id),
+            self.data, format='json')
+
+        self.assertEqual(response.status_code, 400)
+        self.assertNotIn(
+            location.id,
+            Person.objects.get(id=self.data['id']).locations.values_list('id', flat=True)
+        )
+
+    def test_location_level_not_equal_new_role(self):
+        location = mommy.make(Location, location_level=self.person.role.location_level)
+        self.data['locations'].append(str(location.id))
+        location_level = mommy.make(LocationLevel)
+        new_role = create_role(name='new', location_level=location_level)
+        self.data['role'] = str(new_role.id)
+
+        response = self.client.put('/api/admin/people/{}/'.format(self.person.id),
+            self.data, format='json')
+
+        self.assertEqual(response.status_code, 400)
+        self.assertNotIn(
+            location.id,
+            Person.objects.get(id=self.data['id']).locations.values_list('id', flat=True)
+        )
 
 
 class RoleCategoryValidatorTests(APITestCase):
--- bsrs-django/bigsky/person/tests/test_views.py
+++ bsrs-django/bigsky/person/tests/test_views.py
@@ -548,21 +548,6 @@ class PersonPutTests(APITestCase):
         data = json.loads(response.content.decode('utf8'))
         self.assertIn(str(location.id), [l for l in data['locations']])
 
-    def test_location_level_not_equal_init_role(self):
-        # create separate LocationLevel
-        location_level = mommy.make(LocationLevel)
-        location = mommy.make(Location, location_level=location_level)
-        self.assertNotEqual(self.person.role.location_level, location_level)
-        # Adding a non authorized LocationLevel Location raises an error
-        self.data['locations'].append(str(location.id))
-        response = self.client.put('/api/admin/people/{}/'.format(self.person.id),
-            self.data, format='json')
-        self.assertEqual(response.status_code, 400)
-        self.assertNotIn(
-            location.id,
-            Person.objects.get(id=self.data['id']).locations.values_list('id', flat=True)
-            )
-
     def test_location_level_equal_new_role(self):
         # create separate LocationLevel
         location_level = mommy.make(LocationLevel)
@@ -577,22 +562,6 @@ class PersonPutTests(APITestCase):
         data = json.loads(response.content.decode('utf8'))
         self.assertTrue(data['locations'])
         self.assertIn(
-            location.id,
-            Person.objects.get(id=self.data['id']).locations.values_list('id', flat=True)
-            )
-
-    def test_location_level_not_equal_new_role(self):
-        location = mommy.make(Location, location_level=self.person.role.location_level)
-        self.data['locations'].append(str(location.id))
-        # Assign new Role w/ a different LocationLevel
-        location_level = mommy.make(LocationLevel)
-        new_role = mommy.make(Role, name='new', location_level=location_level)
-        self.data['role'] = str(new_role.id)
-        # Adding a non authorized LocationLevel Location raises an error
-        response = self.client.put('/api/admin/people/{}/'.format(self.person.id),
-            self.data, format='json')
-        self.assertEqual(response.status_code, 400)
-        self.assertNotIn(
             location.id,
             Person.objects.get(id=self.data['id']).locations.values_list('id', flat=True)
             )
