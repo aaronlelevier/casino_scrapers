9ac7424f192ae16dc0bde5462f8253dd3e07aac0
snewcomer <snewcomer24@gmail.com>
2016-01-24 11:52:01 -0800

    [TICKET]: number is searchable commented out.  Need to convert to a string before calling toLowerCase on it

--- bsrs-ember/app/components/tickets/ticket-list/component.js
+++ bsrs-ember/app/components/tickets/ticket-list/component.js
@@ -64,7 +64,7 @@ export default GridViewComponent.extend({
             headerLabel: 'ticket.label.numberSymbol',
             headerIsTranslatable: true,
             isSortable: true,
-            isSearchable: true
+            // isSearchable: true
         }
     ]
 });
