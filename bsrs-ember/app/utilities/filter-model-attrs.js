065ea4abae22bbc71c4f8541056842547c3f8a36
toran billups <toranb@gmail.com>
2016-01-28 14:50:01 -0600

    [TODO]: added a hint about where to fix the modal + escape queryParam bug

--- bsrs-ember/app/utilities/filter-model-attrs.js
+++ bsrs-ember/app/utilities/filter-model-attrs.js
@@ -1,6 +1,5 @@
 import Ember from 'ember';
 
-
 var set_filter_model_attrs = function(filterModel, query) {
     let columns = query ? query.split(',') : [];
     columns.forEach((pair) => {
@@ -9,6 +8,9 @@ var set_filter_model_attrs = function(filterModel, query) {
             const [object, field] = attrs[0].split('.');
             const obj = Ember.Object.create();
             obj[field] = attrs[1];
+            //TODO: Andy this is the spot to fix the modal bug
+            //if attrs[1] === "" AND filterModel ...
+            //doesn't have that field (or it's also "") don't set anything :)
             filterModel.set(object, obj);
         }else{
             filterModel.set(attrs[0], attrs[1]);
