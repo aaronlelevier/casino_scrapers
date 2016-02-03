0e72f7fe32fde15cc3bc961a45f4db1e847a5023
aronysidoro <pyaaron@gmail.com>
2016-02-03 14:12:52 -0800

    [PERSON CURRENT] remove locations-n-children n categories bootstrapped on person-current'

--- bsrs-django/bigsky/bigsky/tests/test_views.py
+++ bsrs-django/bigsky/bigsky/tests/test_views.py
@@ -298,14 +298,6 @@ class BootstrappedDataTests(TestCase):
         self.assertEqual(data['locale'], str(self.person.locale.id))
         self.assertEqual(data['role'], str(self.person.role.id))
         self.assertEqual(data['status'], str(self.person.status.id))
-        self.assertIn('all_locations_and_children', data)
-        self.assertEqual(len(data['all_locations_and_children']), 1)
-        self.assertEqual(data['all_locations_and_children'][0]['id'], str(self.person.locations.first().id))
-        self.assertEqual(data['all_locations_and_children'][0]['name'], self.person.locations.first().name)
-        self.assertEqual(data['all_locations_and_children'][0]['location_level'], str(self.person.locations.first().location_level.id))
-        self.assertEqual(data['all_locations_and_children'][0]['status'], str(self.person.locations.first().status.id))
-        self.assertEqual(data['categories'][0]['id'], str(self.person.role.categories.first().id))
-        self.assertEqual(data['categories'][0]['name'], self.person.role.categories.first().name)
 
     def test_default_model_ordering(self):
         # Note: this is a Dict Object generated off off URL's and the models,
