5858d59e68345ed0875137ded2cc4843bf1cadfc
andykrier <andykrier@gmail.com>
2016-02-07 07:46:20 -0800

    [LOCATIONS] Added sort and filter to the organization column.

--- bsrs-ember/app/components/locations/location-list/component.js
+++ bsrs-ember/app/components/locations/location-list/component.js
@@ -4,9 +4,33 @@ import GridViewComponent from 'bsrs-ember/components/grid-view/component';
 export default GridViewComponent.extend({
     layoutName: 'components/grid-view',
     columns: [
-        {field: 'name', headerLabel: 'Name', isSortable: true, isFilterable: true, isSearchable: true},
-        {field: 'number', headerLabel: 'Number', isSortable: true, isFilterable: true, isSearchable: true},
-        {field: 'status.translated_name', headerLabel: 'Status', isSortable: true, isFilterable: true, isSearchable: true},
-        {field: 'location_level', headerLabel: 'Organization'}
+        {
+            field: 'name',
+            headerLabel: 'Name',
+            isSortable: true,
+            isFilterable: true,
+            isSearchable: true
+        },
+        {
+            field: 'number',
+            headerLabel: 'Number',
+            isSortable: true,
+            isFilterable: true,
+            isSearchable: true
+        },
+        {
+            field: 'status.translated_name',
+            headerLabel: 'Status',
+            isSortable: true,
+            isFilterable: true,
+            isSearchable: true
+        },
+        {
+            field: 'location_level',
+            headerLabel: 'Organization',
+            isSortable: true,
+            isFilterable: true,
+            isSearchable: true
+        }
     ]
 });
