4f1cab0b06e1316135498eb2fd89c1c84b9b1d34
aronysidoro <pyaaron@gmail.com>
2016-02-11 10:46:09 -0800

    [PERSISTENT] checkout to v0.0.1 w/ contact type fixtures.

--- persistent.sh
+++ persistent.sh
@@ -30,8 +30,9 @@ if [  -d "${PROJECT_DIR}/persistent/bsrs" ];
         git pull
     else
         echo "BSRS REPO DOES NOT EXIST"
-        git clone --branch v0.1 --depth 1 git@github.com:bigskytech/bsrs.git
+        git clone git@github.com:bigskytech/bsrs.git
         cd bsrs
+        git checkout v0.1.1
 fi
 TEST=$?; if [ "$TEST" == 1 ]; then echo "git pull/clone failed"; exit $TEST; fi
 
