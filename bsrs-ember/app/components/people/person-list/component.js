19d982c0161e27da6165746324edf781ea063fd4
snewcomer <snewcomer24@gmail.com>
2016-02-17 17:12:27 -0800

    [PERSON]: grid status updated

--- bsrs-ember/app/components/people/person-list/component.js
+++ bsrs-ember/app/components/people/person-list/component.js
@@ -3,7 +3,7 @@ import GridViewComponent from 'bsrs-ember/components/grid-view/component';
 export default GridViewComponent.extend({
     layoutName: 'components/grid-view',
     columns: [
-        {field: 'status.translated_name', headerLabel: 'person.label.status-name', headerIsTranslatable: true, isSortable: true, isFilterable: true, isSearchable: true, templateName: 'tickets/ticket-status-tag', classNames: ['person-status'] },
+        {field: 'status.translated_name', headerLabel: 'person.label.status', headerIsTranslatable: true, isSortable: true, isFilterable: true, isSearchable: true, templateName: 'tickets/ticket-status-tag', classNames: ['person-status'] },
         {field: 'fullname', headerLabel: 'Name', isSortable: true, isFilterable: true, isSearchable: true},
         {field: 'username', headerLabel: 'Username', isSortable: true, isFilterable: true, isSearchable: true},
         {field: 'title', headerLabel: 'Title', isSortable: true, isFilterable: true, isSearchable: true},
