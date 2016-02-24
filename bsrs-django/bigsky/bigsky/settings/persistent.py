14648f60637bbbee55701f7b92d11e2c378f2858
aronysidoro <pyaaron@gmail.com>
2016-02-24 13:57:46 -0800

    [PERSISTENT] enable 'transforms' apps n db

--- bsrs-django/bigsky/bigsky/settings/persistent.py
+++ bsrs-django/bigsky/bigsky/settings/persistent.py
@@ -1,6 +1,14 @@
 from .prod import *
 
 
+PERSISTENT_LOCAL_APPS = [
+    'utils_transform.tlocation',
+    'utils_transform.tcategory',
+    ]
+
+INSTALLED_APPS = DEFAULT_APPS + THIRD_PARTY_APPS + LOCAL_APPS + PERSISTENT_LOCAL_APPS
+
+
 DATABASES['default'] = {
     'ENGINE': 'django.db.backends.postgresql_psycopg2',
     'NAME': 'persistent',
@@ -9,6 +17,18 @@ DATABASES['default'] = {
     'HOST': 'localhost',
     'PORT': '5432',
 }
+
+DATABASES['transforms'] = {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'transforms',
+    'USER': 'bsdev',
+    'PASSWORD': 'tango',
+    'HOST': 'localhost',
+    'PORT': '5432',
+}
+
+DATABASE_ROUTERS = ['bigsky.db_router.TransformRouter', 'bigsky.db_router.DefaultRouter']
+
 
 MEDIA_ROOT = "/var/www/media/persistent/"
 STATIC_ROOT = "/var/www/static/persistent/"
