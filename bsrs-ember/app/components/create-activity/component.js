f6ed3c4811fe57c623f35127452fbaeb0aa99943
snewcomer <snewcomer24@gmail.com>
2016-02-26 12:44:50 -0800

    [MOMENT]: interval in each activity template directly...doesnt work if on parent close #183

--- bsrs-ember/app/components/create-activity/component.js
+++ bsrs-ember/app/components/create-activity/component.js
@@ -5,9 +5,8 @@ var createActivity = Ember.Component.extend({
         const i18n_string = this.get('i18nString');
         if (i18n_string) {
             const str = i18n_string.string;
-            const [beg_string, timestamp] = str.split('$');
+            const [beg_string] = str.split('$');
             this.set('begString', beg_string.trim());
-            this.set('timestamp', timestamp.trim());
             return this.get('activity').get('person').get('fullname');
         }
     }),
