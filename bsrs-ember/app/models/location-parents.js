899a4eaf622cd6c2c2403526d6f45a9bfd480e0c
snewcomer <snewcomer24@gmail.com>
2016-02-29 15:17:22 -0800

    remove dead code

--- bsrs-ember/app/models/location-parents.js
+++ bsrs-ember/app/models/location-parents.js
@@ -2,12 +2,6 @@ import Ember from 'ember';
 import inject from 'bsrs-ember/utilities/store';
 
 export default Ember.Object.extend({
-    store: inject('main'),
-    removeRecord() {
-        Ember.run(() => {
-            this.get('store').remove('location-parents', this.get('id'));
-        });
-    },
 });
 
 
