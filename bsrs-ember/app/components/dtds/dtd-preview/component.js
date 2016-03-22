d12fb95a2891df6abe3d379dcf2ba76c4546a433
snewcomer <snewcomer24@gmail.com>
2016-03-21 17:15:37 -0700

    [DTD]: preview add bootstrap class name close #215

--- bsrs-ember/app/components/dtds/dtd-preview/component.js
+++ bsrs-ember/app/components/dtds/dtd-preview/component.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  i18n: Ember.inject.service(),
+  noteClass: Ember.computed(function() {
+    const note_type = this.get('model.note_type');
+    const translated = this.get('i18n').t(note_type);
+    const className = translated.string ? translated.string.toLowerCase() : 'info';
+    return `alert-${className}`;
+  }),
 });
--- bsrs-ember/tests/integration/components/dtds/dtd-preview/component-test.js
+++ bsrs-ember/tests/integration/components/dtds/dtd-preview/component-test.js
@@ -2,19 +2,26 @@ import Ember from 'ember';
 const { run } = Ember;
 import { moduleForComponent, test } from 'ember-qunit';
 import module_registry from 'bsrs-ember/tests/helpers/module_registry';
+import loadTranslations from 'bsrs-ember/tests/helpers/translations';
+import translation from 'bsrs-ember/instance-initializers/ember-i18n';
+import translations from "bsrs-ember/vendor/translation_fixtures";
 import hbs from 'htmlbars-inline-precompile';
 import DTD from 'bsrs-ember/vendor/defaults/dtd';
 import LINK from 'bsrs-ember/vendor/defaults/link';
 import DTDF from 'bsrs-ember/vendor/dtd_fixtures';
 import page from 'bsrs-ember/tests/pages/dtd';
 
-var dtd, store;
+var dtd, store, trans;
 
 moduleForComponent('dtds/dtd-preview', 'Integration | Component | dtds/dtd preview', {
   integration: true,
   beforeEach() {
     page.setContext(this);
     store = module_registry(this.container, this.registry, ['model:dtd']);
+    translation.initialize(this);
+    trans = this.container.lookup('service:i18n');
+    var json = translations.generate('en');
+    loadTranslations(trans, json);
     run(() => {
       dtd = store.push('dtd', DTDF.generate());
     });
@@ -25,6 +32,9 @@ moduleForComponent('dtds/dtd-preview', 'Integration | Component | dtds/dtd previ
 });
 
 test('preview renders with model data passed from dtd route - v1', function(assert) {
+  run(() => {
+    dtd = store.push('dtd', {id: dtd.get('id'), note_type: ''});
+  })
   this.model = dtd;
   this.render(hbs`{{dtds/dtd-preview model=model}}`);
   assert.equal(page.previewDescription, DTD.descriptionOne);
@@ -33,7 +43,19 @@ test('preview renders with model data passed from dtd route - v1', function(asse
   assert.ok(page.previewHasButtons);
   assert.notOk(page.previewHasList);
   assert.equal(page.previewButtonOne, LINK.textOne);
-
   // Add back when needed
   // assert.equal(this.$('.t-dtd-preview-note_type').text().trim(), DTD.noteTypeOne);
+  const $component = this.$('.t-dtd-note-preview');
+  assert.ok($component.hasClass('alert-info'));
+});
+
+test('note_type determines class on note (success, info, warning, danger)', function(assert) {
+  run(() => {
+    store.push('dtd', {note_type: dtd.note_types[0]});
+  });
+  this.model = dtd;
+  this.render(hbs`{{dtds/dtd-preview model=model}}`);
+  const $component = this.$('.t-dtd-note-preview');
+  const className = trans.t(dtd.get('note_type')).string.toLowerCase();
+  assert.ok($component.hasClass(`alert-${className}`));
 });
