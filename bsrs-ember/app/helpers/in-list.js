26cf512d5bd1ca583582c5dd94a7f033e6c1572c
snewcomer <snewcomer24@gmail.com>
2016-03-23 15:16:43 -0700

    remove helper

--- bsrs-ember/app/helpers/in-list.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Helper.helper((params) => {
-    let ids = params[1].map(function(model) {
-        return model.get('id');
-    });
-    return Ember.$.inArray(params[0], ids) > -1;
-});
