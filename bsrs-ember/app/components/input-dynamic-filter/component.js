47d335fdbbb8d18a5f2ce1ec76a65a47c3ff36b5
snewcomer <snewcomer24@gmail.com>
2016-03-31 14:16:07 -0700

    [DTD]: search filterField and placeholder added to grid

--- bsrs-ember/app/components/input-dynamic-filter/component.js
+++ bsrs-ember/app/components/input-dynamic-filter/component.js
@@ -4,20 +4,20 @@ import KeyCodes  from 'bsrs-ember/utilities/key-codes';
 import InputDynamic from 'bsrs-ember/components/input-dynamic/component';
 
 export default InputDynamic.extend({
-    eventbus: Ember.inject.service(),
-    observeValid: Ember.observer('value', function() {
-        Ember.run.debounce(this, this.valueUpdated, config.DEBOUNCE_TIMEOUT_INTERVAL, false);
-    }),
-    valueUpdated: function() {
-        let prop = this.get('prop');
-        let value = this.get('value');
-        if(value !== null) {
-            this.get('eventbus').publish('bsrs-ember@component:input-dynamic-filter:', this, 'onValueUpdated', prop, value);
-        }
-    },
-    keyDown: function (event) {
-        if (KeyCodes.keyPressed(event) === 'enter' || KeyCodes.keyPressed(event) === 'escape' ) {
-            this.sendAction('close');
-        }
+  eventbus: Ember.inject.service(),
+  observeValid: Ember.observer('value', function() {
+    Ember.run.debounce(this, this.valueUpdated, config.DEBOUNCE_TIMEOUT_INTERVAL, false);
+  }),
+  valueUpdated: function() {
+    let prop = this.get('prop');
+    let value = this.get('value');
+    if(value !== null) {
+      this.get('eventbus').publish('bsrs-ember@component:input-dynamic-filter:', this, 'onValueUpdated', prop, value);
     }
+  },
+  keyDown: function (event) {
+    if (KeyCodes.keyPressed(event) === 'enter' || KeyCodes.keyPressed(event) === 'escape' ) {
+      this.sendAction('close');
+    }
+  }
 });
--- bsrs-ember/app/components/input-dynamic/component.js
+++ bsrs-ember/app/components/input-dynamic/component.js
@@ -1,25 +1,25 @@
 import Ember from 'ember';
 
 var InputDynamic = Ember.TextField.extend({
-    classNames: ['t-new-entry form-control input-md'],
-    value: Ember.computed('obj', {
-        get(key){
-            var prop = this.get('prop');
-            return this.get('obj.' + prop);
-        },
-        set(key, value){
-            var prop = this.get('prop');
-            if (prop.indexOf('.') > -1) {
-                const [object, field] = prop.split('.');
-                const obj = Ember.Object.create();
-                obj[field] = value;
-                this.set('obj.' + object, obj);
-            }else{
-                this.set('obj.' + prop, value);
-            }
-            return this.get('obj.' + prop, value);
-        }
-    })
+  classNames: ['t-new-entry form-control input-md'],
+  value: Ember.computed('obj', {
+    get(key){
+      var prop = this.get('prop');
+      return this.get('obj.' + prop);
+    },
+    set(key, value){
+      var prop = this.get('prop');
+      if (prop.indexOf('.') > -1) {
+        const [object, field] = prop.split('.');
+        const obj = Ember.Object.create();
+        obj[field] = value;
+        this.set('obj.' + object, obj);
+      }else{
+        this.set('obj.' + prop, value);
+      }
+      return this.get('obj.' + prop, value);
+    }
+  })
 });
 
 export default InputDynamic;
