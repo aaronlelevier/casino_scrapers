53db971790f885020bb798b1ee26fbccef61a2c4
snewcomer <snewcomer24@gmail.com>
2016-04-07 23:32:05 -0700

    [dtd]: category tests to be worked on

--- bsrs-ember/tests/acceptance/general/dtd/detail-test.js
+++ bsrs-ember/tests/acceptance/general/dtd/detail-test.js
@@ -396,22 +396,17 @@ test('deep linking with an xhr with a 404 status code will show up in the error 
   assert.equal(find('.t-grid-data').length, PAGE_SIZE);
   assert.equal(find('.t-error-message').text(), 'WAT');
 });
-/* jshint ignore:end */
 
-test('click add-link, and fill in', (assert) => {
+test('click add-link, and fill in', async assert => {
   random.uuid = function() { return UUID.value; };
-  page.visitDetail();
-  andThen(() => {
-    assert.ok(find('.t-dtd-link-action_button').prop('checked'));
-    assert.equal(page.textCount, 1);
-  });
-  page.clickAddLinkBtn();
-  andThen(() => {
-    assert.equal(page.textCount, 2);
-  });
+  await page.visitDetail();
+  assert.ok(find('.t-dtd-link-action_button').prop('checked'));
+  assert.equal(page.textCount, 1);
+  await page.clickAddLinkBtn();
+  assert.equal(page.textCount, 2);
   const json = { 'results': [{id: DTD.idOne, key: DTD.keyOne}, {id: DTD.idTwo, key: DTD.keyTwo}] };
   ajax(`/api/dtds/?key__icontains=1`, 'GET', null, {}, 200, json);
-  page
+  await page
   .requestFillIn_two(LINK.requestTwo)
   .textFillIn_two(LINK.textTwo)
   .action_buttonClick_two()
@@ -419,33 +414,329 @@ test('click add-link, and fill in', (assert) => {
   .destinationSearch('1')
   .destinationClickOptionOne();
   xhr(DT_PUT_URL, 'PUT', JSON.stringify(dtd_payload_link_two_put), {}, 200, {});
-  generalPage.save();
-  andThen(() => {
-    assert.equal(currentURL(), DETAIL_URL);
-  });
+  await generalPage.save();
+  assert.equal(currentURL(), DETAIL_URL);
 });
 
-test('categories selector is wired up and working', assert => {
-  page.visitDetail();
-  ticketPage
-    .categoryOneClickDropdown()
-    .categoryOneClickOptionOne();
+/* CATEGORIES */
+test('categories selector is wired up and working', async assert => {
+  await page.visitDetail();
   const top_level_categories_endpoint = PREFIX + '/admin/categories/parents/';
   xhr(top_level_categories_endpoint, 'GET', null, {}, 200, CF.top_level());
-  andThen(() => {
-    assert.equal(ticketPage.categoryOneInput, CD.nameOne);
-  });
-  ticketPage
-    .categoryTwoClickDropdown()
-    .categoryTwoClickOptionOne();
+  await ticketPage
+    .categoryOneClickDropdown()
+    .categoryOneClickOptionOne();
+  assert.equal(ticketPage.categoryOneInput, CD.nameOne);
   const second_level_categories_endpoint = PREFIX + `/admin/categories/?parent=${CD.idOne}`;
   xhr(second_level_categories_endpoint, 'GET', null, {}, 200, CF.get_list(CD.idTwo, CD.nameTwo, [], CD.idOne, 1));
-  andThen(() => {
-    assert.equal(ticketPage.categoryTwoInput, CD.nameTwo);
-  });
-  generalPage.save();
+  await ticketPage
+    .categoryTwoClickDropdown()
+    .categoryTwoClickOptionOne();
+  assert.equal(ticketPage.categoryTwoInput, CD.nameTwo);
   xhr(DT_PUT_URL, 'PUT', JSON.stringify(dtd_payload_with_categories), {}, 200, {});
-  andThen(() => {
-    assert.equal(currentURL(), DETAIL_URL);
-  });
+  await generalPage.save();
+  assert.equal(currentURL(), DETAIL_URL);
 });
+
+// test('categories are in order based on text', async assert => {
+//   clearxhr(list_xhr);
+//   await page.visitDetail();
+//   assert.equal(page.categoryOneInput, CD.nameOne);
+//   assert.equal(page.categoryTwoInput, CD.nameRepairChild);
+//   assert.equal(page.categoryThreeInput, CD.namePlumbingChild);
+// });
+
+//test('power select options are rendered immediately when enter detail route and can save different top level category', (assert) => {
+//  let top_level_data = CF.top_level();
+//  top_level_data.results[1] = {id: CD.idThree, name: CD.nameThree, parent_id: null, children: [{id: CD.idLossPreventionChild, name: CD.nameLossPreventionChild}], level: 0};
+//  page.visitDetail();
+//  andThen(() => {
+//    let components = page.powerSelectComponents;
+//    assert.equal(components, 3);
+//    let dtd = store.find('dtd', DTD.idOne);
+//    assert.equal(dtd.get('top_level_category').get('id'), CD.idOne);
+//    assert.equal(dtd.get('categories').get('length'), 3);
+//  });
+//  let top_level_categories_endpoint = PREFIX + '/admin/categories/parents/';
+//  xhr(top_level_categories_endpoint, 'GET', null, {}, 200, top_level_data);
+//  page.categoryOneClickDropdown();
+//  andThen(() => {
+//    assert.equal(page.categoryOneInput, CD.nameOne);
+//    assert.equal(page.categoryOneOptionLength, 2);
+//  });
+//  page.categoryOneClickDropdown();
+//  ajax(`${PREFIX}/admin/categories/?parent=${CD.idOne}`, 'GET', null, {}, 200, CF.get(CD.idTwo, CD.nameTwo));
+//  page.categoryTwoClickDropdown();
+//  andThen(() => {
+//    assert.equal(page.categoryTwoInput, CD.nameRepairChild);
+//  });
+//  page.categoryTwoClickDropdown();
+//  ajax(`${PREFIX}/admin/categories/?parent=${CD.idPlumbing}`, 'GET', null, {}, 200, CF.get_list(CD.idPlumbing, CD.nameRepairChild));
+//  page.categoryThreeClickDropdown();
+//  andThen(() => {
+//    assert.equal(page.categoryThreeInput, CD.namePlumbingChild);
+//    assert.equal(page.categoryThreeOptionLength, 1);
+//  });
+//  page.categoryThreeClickDropdown();
+//  //click loss prevention
+//  page.categoryOneClickDropdown();
+//  page.categoryOneClickOptionTwo();
+//  andThen(() => {
+//    let components = page.powerSelectComponents;
+//    assert.equal(components, 2);
+//    let dtd = store.find('dtd', DTD.idOne);
+//    assert.equal(dtd.get('top_level_category').get('id'), CD.idThree);
+//    assert.equal(dtd.get('model_categories_fks').length, 3);
+//    assert.ok(dtd.get('isDirtyOrRelatedDirty'));
+//  });
+//  page.categoryOneClickDropdown();
+//  andThen(() => {
+//    assert.equal(page.categoryOneInput, CD.nameThree);
+//    assert.equal(page.categoryOneOptionLength, 2);
+//  });
+//  page.categoryOneClickDropdown();
+//  const security = CF.get_list(CD.idLossPreventionChild, CD.nameLossPreventionChild, [], CD.idThree, 1);
+//  ajax(`${PREFIX}/admin/categories/?parent=${CD.idThree}`, 'GET', null, {}, 200, security);
+//  page.categoryTwoClickDropdown();
+//  andThen(() => {
+//    assert.equal(page.categoryTwoOptionLength, 1);
+//  });
+//  page.categoryTwoClickOptionSecurity();
+//  andThen(() => {
+//    assert.equal(page.categoryTwoInput, CD.nameLossPreventionChild);
+//  });
+//  const payload = DTDF.put({id: DTD.idOne, categories: [CD.idThree, CD.idLossPreventionChild]});
+//  xhr(DDTD_PUT_URL, 'PUT', JSON.stringify(payload), {}, 200);
+//  generalPage.save();
+//  andThen(() => {
+//    assert.equal(currentURL(), DDTD_URL);
+//  });
+//});
+
+//test('selecting a top level category will alter the url and can cancel/discard changes and return to index', (assert) => {
+//  page.visitDetail();
+//  andThen(() => {
+//    //override electrical to have children
+//    store.push('category-children', {id: CCD.idOne, category_pk: CD.idTwo, child_pk: CD.idChild});
+//    let cat = store.push('category', {id: CD.idTwo, name: CD.nameTwo, category_children_fks: [CCD.idOne], parent_id: CD.idOne, level: 1});
+//    cat.save();
+//    let components = page.powerSelectComponents;
+//    assert.equal(store.find('category').get('length'), 4);
+//    let dtd = store.find('dtd', DTD.idOne);
+//    assert.equal(dtd.get('categories').get('length'), 3);
+//    // assert.ok(dtd.get('isNotDirtyOrRelatedNotDirty'));
+//    // assert.ok(dtd.get('categoriesIsNotDirty'));
+//    assert.equal(components, 3);
+//  });
+//  // select same
+//  let top_level_categories_endpoint = PREFIX + '/admin/categories/parents/';
+//  top_level_xhr = xhr(top_level_categories_endpoint, 'GET', null, {}, 200, CF.top_level());
+//  page.categoryOneClickDropdown();
+//  page.categoryOneClickOptionOne();
+//  andThen(() => {
+//    let components = page.powerSelectComponents;
+//    assert.equal(store.find('dtd').get('length'), 1);
+//    let dtd = store.find('dtd', DTD.idOne);
+//    assert.equal(dtd.get('categories').get('length'), 3);
+//    assert.equal(dtd.get('sorted_categories').get('length'), 3);
+//    assert.equal(dtd.get('sorted_categories').objectAt(0).get('children').get('length'), 2);
+//    assert.equal(dtd.get('sorted_categories').objectAt(1).get('children').get('length'), 1);
+//    assert.equal(dtd.get('sorted_categories').objectAt(2).get('children').get('length'), 0);
+//    // assert.ok(dtd.get('isNotDirtyOrRelatedNotDirty'));
+//    // assert.ok(dtd.get('categoriesIsNotDirty'));
+//    assert.equal(components, 3);
+//  });
+//  //select electrical from second level
+//  ajax(`${PREFIX}/admin/categories/?parent=${CD.idOne}`, 'GET', null, {}, 200, CF.get_list(CD.idTwo, CD.nameTwo, [{id:CD.idChild}], CD.idOne, 1));
+//  page.categoryTwoClickDropdown();
+//  page.categoryTwoClickOptionElectrical();
+//  andThen(() => {
+//    let components = page.powerSelectComponents;
+//    let dtd = store.find('dtd', DTD.idOne);
+//    assert.equal(store.find('category').get('length'), 5);
+//    assert.equal(dtd.get('categories').get('length'), 2);
+//    assert.equal(dtd.get('sorted_categories').get('length'), 2);
+//    assert.equal(dtd.get('sorted_categories').objectAt(0).get('children').get('length'), 2);
+//    // assert.equal(dtd.get('sorted_categories').objectAt(1).get('children').get('length'), 1);
+//    // assert.ok(dtd.get('isDirtyOrRelatedDirty'));
+//    // assert.ok(dtd.get('categoriesIsDirty'));
+//    assert.equal(components, 3);
+//  });
+//  const payload = CF.get_list(CD.idChild, CD.nameElectricalChild, [], CD.idTwo, 2);
+//  ajax(`${PREFIX}/admin/categories/?parent=${CD.idTwo}`, 'GET', null, {}, 200, payload);
+//  page.categoryThreeClickDropdown();
+//  page.categoryThreeClickOptionOne();
+//  generalPage.cancel();
+//  andThen(() => {
+//    waitFor(() => {
+//      assert.equal(currentURL(), DETAIL_URL);
+//      assert.ok(generalPage.modalIsVisible);
+//      assert.equal(find('.t-modal-body').text().trim(), t('crud.discard_changes_confirm'));
+//    });
+//  });
+//  generalPage.clickModalCancel();
+//  andThen(() => {
+//    waitFor(() => {
+//      assert.equal(currentURL(), DETAIL_URL);
+//      assert.ok(generalPage.modalIsHidden);
+//      let components = page.powerSelectComponents;
+//      let dtds = store.find('dtd');
+//      assert.equal(store.find('category').get('length'), 5);
+//      assert.equal(dtds.get('length'), 1);
+//      let dtd = store.find('dtd', DTD.idOne);
+//      assert.equal(dtd.get('categories').get('length'), 3);
+//      assert.equal(dtd.get('sorted_categories').objectAt(0).get('children').get('length'), 2);
+//      assert.equal(dtd.get('sorted_categories').objectAt(1).get('children').get('length'), 1);
+//      assert.equal(dtd.get('sorted_categories').objectAt(2).get('children').get('length'), 0);
+//      // assert.ok(dtd.get('isDirtyOrRelatedDirty'));
+//      // assert.ok(dtd.get('categoriesIsDirty'));
+//      assert.equal(components, 3);
+//    });
+//  });
+//  generalPage.cancel();
+//  andThen(() => {
+//    waitFor(() => {
+//      assert.equal(currentURL(), DETAIL_URL);
+//      assert.ok(generalPage.modalIsVisible);
+//      assert.equal(find('.t-modal-body').text().trim(), t('crud.discard_changes_confirm'));
+//    });
+//  });
+//  generalPage.clickModalRollback();
+//  andThen(() => {
+//    waitFor(() => {
+//      assert.equal(currentURL(), DDTD_URL);
+//    });
+//  });
+//});
+
+//test('changing tree and reverting tree should not show as dirty', (assert) => {
+//  let top_level_categories_endpoint = PREFIX + '/admin/categories/parents/';
+//  top_level_xhr = xhr(top_level_categories_endpoint, 'GET', null, {}, 200, CF.top_level());
+//  clearxhr(list_xhr);
+//  page.visitDetail();
+//  andThen(() => {
+//    //override electrical to have children
+//    store.push('category-children', {id: CCD.idOne, category_pk: CD.idTwo, child_pk: CD.idChild});
+//    let cat = store.push('category', {id: CD.idTwo, name: CD.nameTwo, category_children_fks: [CCD.idOne], parent_id: CD.idOne, level: 1});
+//    cat.save();
+//    let dtd = store.find('dtd', DTD.idOne);
+//    assert.ok(dtd.get('isNotDirtyOrRelatedNotDirty'));
+//    assert.ok(dtd.get('categoriesIsNotDirty'));
+//  });
+//  //select same
+//  page.categoryOneClickDropdown();
+//  page.categoryOneClickOptionOne();
+//  andThen(() => {
+//    let components = page.powerSelectComponents;
+//    let dtd = store.find('dtd', DTD.idOne);
+//    assert.ok(dtd.get('isNotDirtyOrRelatedNotDirty'));
+//    assert.ok(dtd.get('categoriesIsNotDirty'));
+//  });
+//  //select electrical from second level
+//  ajax(`${PREFIX}/admin/categories/?parent=${CD.idOne}`, 'GET', null, {}, 200, CF.get_list(CD.idTwo, CD.nameTwo, [{id: CD.idChild}], CD.idOne, 1));
+//  page.categoryTwoClickDropdown();
+//  page.categoryTwoClickOptionElectrical();
+//  andThen(() => {
+//    let dtd = store.find('dtd', DTD.idOne);
+//    assert.ok(dtd.get('isDirtyOrRelatedDirty'));
+//    assert.ok(dtd.get('categoriesIsDirty'));
+//  });
+//  ajax(`${PREFIX}/admin/categories/?parent=${CD.idTwo}`, 'GET', null, {}, 200, CF.get_list(CD.idChild, CD.nameElectricalChild, [], CD.idTwo, 2));
+//  page.categoryThreeClickDropdown();
+//  page.categoryThreeClickOptionOne();
+//  andThen(() => {
+//    let dtd = store.find('dtd', DTD.idOne);
+//    assert.ok(dtd.get('isDirtyOrRelatedDirty'));
+//    assert.ok(dtd.get('categoriesIsDirty'));
+//  });
+//  ajax(`${PREFIX}/admin/categories/?parent=${CD.idOne}`, 'GET', null, {}, 200, CF.get_list(CD.idPlumbing, CD.nameRepairChild, [{id:CD.idPlumbingChild}], CD.idOne, 1));
+//  page.categoryTwoClickDropdown();
+//  page.categoryTwoClickOptionPlumbing();
+//  andThen(() => {
+//    let dtd = store.find('dtd', DTD.idOne);
+//    assert.ok(dtd.get('isDirtyOrRelatedDirty'));
+//    assert.ok(dtd.get('categoriesIsDirty'));
+//  });
+//  ajax(`${PREFIX}/admin/categories/?parent=${CD.idPlumbing}`, 'GET', null, {}, 200, CF.get_list(CD.idPlumbingChild, CD.namePlumbingChild, [], CD.idPlumbing, 2));
+//  page.categoryThreeClickDropdown();
+//  //reset tree back to original
+//  page.categoryThreeClickOptionToilet();
+//  andThen(() => {
+//    let dtd = store.find('dtd', DTD.idOne);
+//    assert.ok(dtd.get('isNotDirtyOrRelatedNotDirty'));
+//    assert.ok(dtd.get('categoriesIsNotDirty'));
+//  });
+//});
+
+//test('selecting and removing a top level category will remove children categories already selected', (assert) => {
+//  let top_level_categories_endpoint = PREFIX + '/admin/categories/parents/';
+//  top_level_xhr = xhr(top_level_categories_endpoint, 'GET', null, {}, 200, CF.top_level());
+//  clearxhr(list_xhr);
+//  page.visitDetail();
+//  andThen(() => {
+//    let components = page.powerSelectComponents;
+//    assert.equal(store.find('category').get('length'), 4);
+//    let dtds = store.find('dtd');
+//  });
+//  //change top level
+//  page.categoryOneClickDropdown();
+//  page.categoryOneClickOptionTwo();
+//  andThen(() => {
+//    let components = page.powerSelectComponents;
+//    let dtds = store.find('dtd');
+//    assert.equal(dtds.get('length'), 1);
+//    assert.equal(dtds.objectAt(0).get('categories').get('length'), 1);
+//    assert.equal(dtds.objectAt(0).get('categories').objectAt(0).get('children').get('length'), 0);
+//    assert.equal(components, 1);
+//  });
+//});
+
+//test('when selecting a new parent category it should remove previously selected child category but if select same, it wont clear tree', (assert) => {
+//  page.visitDetail();
+//  ajax(`${PREFIX}/admin/categories/?parent=${CD.idOne}`, 'GET', null, {}, 200, CF.get_list(CD.idPlumbing, CD.nameRepairChild, [{id: CD.idChild}], CD.idOne, 1));
+//  page.categoryTwoClickDropdown();
+//  page.categoryTwoClickOptionPlumbing();
+//  andThen(() => {
+//    let dtd = store.findOne('dtd');
+//    assert.ok(dtd.get('isNotDirtyOrRelatedNotDirty'));
+//    assert.equal(dtd.get('categories').get('length'), 3);
+//    let components = page.powerSelectComponents;
+//    assert.equal(components, 3);
+//  });
+//  ajax(`${PREFIX}/admin/categories/?parent=${CD.idOne}`, 'GET', null, {}, 200, CF.get_list(CD.idTwo, CD.nameTwo, [{id: CD.idChild}], CD.idOne, 1));
+//  page.categoryTwoClickDropdown();
+//  page.categoryTwoClickOptionElectrical();
+//  andThen(() => {
+//    let dtd = store.findOne('dtd');
+//    assert.ok(dtd.get('isDirtyOrRelatedDirty'));
+//    assert.equal(dtd.get('categories').get('length'), 2);
+//    let components = page.powerSelectComponents;
+//    assert.equal(components, 3);
+//  });
+//  let top_level_categories_endpoint = PREFIX + '/admin/categories/parents/';
+//  top_level_xhr = xhr(top_level_categories_endpoint, 'GET', null, {}, 200, CF.top_level());
+//  page.categoryOneClickDropdown();
+//  page.categoryOneClickOptionTwo();
+//  andThen(() => {
+//    let dtd = store.findOne('dtd');
+//    assert.ok(dtd.get('isDirtyOrRelatedDirty'));
+//    assert.equal(dtd.get('categories').get('length'), 1);
+//    let components = page.powerSelectComponents;
+//    assert.equal(components, 1);
+//  });
+//  page.categoryOneClickDropdown();
+//  page.categoryOneClickOptionOne();
+//  ajax(`${PREFIX}/admin/categories/?parent=${CD.idTwo}`, 'GET', null, {}, 200, CF.get_list(CD.idChild, CD.nameElectricalChild, [], CD.idTwo, 2));
+//  page.categoryTwoClickDropdown();
+//  page.categoryTwoClickOptionOne();
+//  page.categoryThreeClickDropdown();
+//  page.categoryThreeClickOptionOne();
+//  let payload = dtd_payload_detail;
+//  payload.categories = [CD.idOne, CD.idTwo , CD.idChild];
+//  xhr(DDTD_PUT_URL, 'PUT', JSON.stringify(payload), {}, 200, {});
+//  generalPage.save();
+//  andThen(() => {
+//    assert.equal(currentURL(), DDTD_URL);
+//  });
+//});
+/* jshint ignore:end */
