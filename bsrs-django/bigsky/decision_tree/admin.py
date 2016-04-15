7ca6463624a18fe8fe7601de1e82adf318bb465b
aronysidoro <pyaaron@gmail.com>
2016-04-15 12:07:56 -0700

    [DTD] missing test coverage, default django-admin

--- bsrs-django/bigsky/decision_tree/admin.py
+++ bsrs-django/bigsky/decision_tree/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from decision_tree import models
+
+
+@admin.register(models.TreeData)
+class TreeDataAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.TreeField)
+class TreeFieldAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.TreeOption)
+class TreeOptionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.TreeLink)
+class TreeLinkAdmin(admin.ModelAdmin):
+    pass
---
+++ bsrs-django/bigsky/decision_tree/tests/test_models.py
@@ -0,0 +1,33 @@
+from django.db.models import Q
+from django.test import TestCase
+
+from model_mommy import mommy
+
+from decision_tree.models import TreeData, TreeDataManager
+from decision_tree.tests.factory import create_tree_link, create_tree_data
+
+
+class TreeDataManagerTests(TestCase):
+
+    def test_search_multi(self):
+        keyword = 'a'
+        mommy.make(TreeData, key=keyword)
+        mommy.make(TreeData, key='b')
+        mommy.make(TreeData, description=keyword)
+        raw_ret = TreeData.objects.filter(
+            Q(key__icontains=keyword) | \
+            Q(description__icontains=keyword)
+        )
+
+        ret = TreeData.objects.search_multi(keyword)
+
+        self.assertEqual(len(ret), len(raw_ret))
+
+
+class TreeDataTests(TestCase):
+
+    def test_manager(self):
+        self.assertIsInstance(TreeData.objects, TreeDataManager)
+
+    def test_meta_ordering(self):
+        self.assertEqual(TreeData._meta.ordering, ('key',))
