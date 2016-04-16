4c4f02235cb6fa84c00e636f954b098f9233c13d
snewcomer <snewcomer24@gmail.com>
2016-04-16 10:47:38 -0700

    [COMPONENT]: add back tag name...dangerous setting tag name to nothing...

--- bsrs-ember/app/components/parent-model-category-select/component.js
+++ bsrs-ember/app/components/parent-model-category-select/component.js
@@ -3,18 +3,17 @@ import inject from 'bsrs-ember/utilities/inject';
 import ChildValidationComponent from 'bsrs-ember/mixins/validation/child';
 
 var ParentTicketCategorySelect = ChildValidationComponent.extend({
-    repository: inject('category'),
-    tagName: '',
-    valid: Ember.computed('ticket.categories.[]', function() {
-        let ticket = this.get('ticket');
-        let categories = ticket.get('categories') || [];
-        if(categories.get('length') < 1) {
-            return false;
-        }
-        let parent = ticket.get('top_level_category');
-        let tree = ticket.construct_category_tree(parent);
-        return tree ? tree.pop().get('children').get('length') === 0 : true;
-    })
+  repository: inject('category'),
+  valid: Ember.computed('ticket.categories.[]', function() {
+    let ticket = this.get('ticket');
+    let categories = ticket.get('categories') || [];
+    if(categories.get('length') < 1) {
+      return false;
+    }
+    let parent = ticket.get('top_level_category');
+    let tree = ticket.construct_category_tree(parent);
+    return tree ? tree.pop().get('children').get('length') === 0 : true;
+  })
 });
 
 export default ParentTicketCategorySelect;
