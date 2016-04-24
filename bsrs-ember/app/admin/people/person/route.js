e163c60a06ffa2d6c72657e805cec1831427a11b
snewcomer <snewcomer24@gmail.com>
2016-04-23 18:01:05 -0700

    [PERSON]: use setProperties in setupController

--- bsrs-ember/app/admin/people/person/route.js
+++ bsrs-ember/app/admin/people/person/route.js
@@ -43,19 +43,7 @@ var PersonRoute = TabRoute.extend(FindById, ContactRouteMixin, {
                                  locales:locales, roles:roles, role_change:role_change}, override);
   },
   setupController(controller, hash) {
-    controller.set('model', hash.model);
-    controller.set('email_types', hash.email_types);
-    controller.set('default_email_type', hash.default_email_type);
-    controller.set('phone_number_types', hash.phone_number_types);
-    controller.set('default_phone_number_type', hash.default_phone_number_type);
-    controller.set('address_types', hash.address_types);
-    controller.set('default_address_type', hash.default_address_type);
-    controller.set('state_list', hash.state_list);
-    controller.set('countries', hash.countries);
-    controller.set('statuses', hash.statuses);
-    controller.set('roles', hash.roles);
-    controller.set('locales', hash.locales);
-    controller.set('role_change', hash.role_change);
+    controller.setProperties(hash);
   }
 });
 
