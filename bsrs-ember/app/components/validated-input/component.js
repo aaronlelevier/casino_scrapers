094ef46c73ed1c6e95d870d469d819efc577ae59
snewcomer <snewcomer24@gmail.com>
2016-04-28 20:28:06 -0700

    remove some imports

--- bsrs-ember/app/components/validated-input/component.js
+++ bsrs-ember/app/components/validated-input/component.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
-const { computed, observer, defineProperty, run } = Ember;
+const { computed, defineProperty } = Ember;
 
 export default Ember.Component.extend({
   type: 'text',
