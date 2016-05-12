e8fdc9468eb758c8be1a488f0c3ec06221c02d37
snewcomer <snewcomer24@gmail.com>
2016-05-12 11:40:37 -0700

    django settings note: ask aaron

--- bsrs-django/bigsky/bigsky/wsgi.py
+++ bsrs-django/bigsky/bigsky/wsgi.py
@@ -2,6 +2,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+# bigsky.settings.base?
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bigsky.settings")
 
 application = get_wsgi_application()
