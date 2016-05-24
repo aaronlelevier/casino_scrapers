746303584667952cbb4f7437386ea583a4840509
snewcomer <snewcomer24@gmail.com>
2016-05-24 13:28:40 -0700

    [NAV]: get rid of unused showing flag.  Probably from Andys user menu work

--- bsrs-ember/app/components/user-menu/component.js
+++ bsrs-ember/app/components/user-menu/component.js
@@ -3,14 +3,4 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     tagName: 'ul',
     classNames: ['nav navbar-nav navbar-right'],
-    actions: {
-        show_menu(){
-            let isShowing = this.get('showing');
-            if(isShowing){
-                this.set('showing', false);
-            }else{
-                this.set('showing', true);
-            }
-        }
-    }
 });
