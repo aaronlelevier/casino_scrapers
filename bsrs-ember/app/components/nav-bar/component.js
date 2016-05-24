e95ec54e21b4dc26e17542bf0860af5bbc78d9ea
snewcomer <snewcomer24@gmail.com>
2016-05-23 18:09:51 -0700

    remove dead code

--- bsrs-ember/app/components/nav-bar/component.js
+++ bsrs-ember/app/components/nav-bar/component.js
@@ -1,14 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    actions: {
-        show_menu(){
-            let isShowing = this.get('showing');
-            if(isShowing){
-                this.set('showing', false);
-            }else{
-                this.set('showing', true);
-            }
-        }
-    }
 });
