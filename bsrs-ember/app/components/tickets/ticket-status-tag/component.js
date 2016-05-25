5c6e09dbd5dc3f99d8d6a8ebe7b0085201bc7afc
snewcomer <snewcomer24@gmail.com>
2016-05-25 11:59:43 -0700

    move tags to globals.  Input address to own style.scss file

--- bsrs-ember/app/components/tickets/ticket-status-tag/component.js
+++ bsrs-ember/app/components/tickets/ticket-status-tag/component.js
@@ -1,12 +1,12 @@
 import Ember from 'ember';
 
 var TicketStatusTag = Ember.Component.extend({
-    tagName: 'span',
-    className: Ember.computed('item.status', function(){
-        let tagClass = this.get('item.status_class');
-        return `tag ${tagClass}`;
-    }),
-    classNameBindings: ['className']
+  tagName: 'span',
+  className: Ember.computed('item.status', function(){
+    let tagClass = this.get('item.status_class');
+    return `tag ${tagClass}`;
+  }),
+  classNameBindings: ['className']
 });
 
 export default TicketStatusTag;
