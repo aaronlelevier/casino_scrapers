a0ddd92ee3e15a965bef0e9ee9246b511c5fab6d
snewcomer <snewcomer24@gmail.com>
2016-05-25 12:24:06 -0700

    address styles moved to component.  small refactors

--- bsrs-ember/app/components/input-multi-address/component.js
+++ bsrs-ember/app/components/input-multi-address/component.js
@@ -9,37 +9,37 @@ import postalCodeValidation from 'bsrs-ember/validation/postal_code';
 const { run } = Ember;
 
 var MultiAddressComponent = ChildValidationComponent.extend(ValidationMixin, CustomValidMixin, {
-    uuid: inject('uuid'),
-    tagName: 'div',
-    classNames: ['input-multi-address t-input-multi-address'],
-    addressFormat: validateEach('address', addressNameValidation),
-    postal_codeFormat: validateEach('postal_code', postalCodeValidation),
-    actions: {
-        changed(address, val) {
-            address.set('type', val);
-        },
-        append() {
-            const id = this.get('uuid').v4();
-            const type = this.get('default_type').get('id');
-            const related_pk = this.get('related_pk');
-            var model = {id: id, type: type};
-            model['model_fk'] = related_pk;
-            run(() => {
-                this.get('model').push(model);
-            });
-        },
-        delete(entry) {
-            run(() => {
-                this.get('model').push({id: entry.get('id'), removed: true});
-            });
-        },
-        changeState(address, val) {
-            address.set('state', val);
-        },
-        changeCountry(country, val) {
-            country.set('country', val);
-        }
+  uuid: inject('uuid'),
+  tagName: 'div',
+  classNames: ['input-multi-address t-input-multi-address'],
+  addressFormat: validateEach('address', addressNameValidation),
+  postal_codeFormat: validateEach('postal_code', postalCodeValidation),
+  actions: {
+    changed(address, val) {
+      address.set('type', val);
+    },
+    append() {
+      const id = this.get('uuid').v4();
+      const type = this.get('default_type').get('id');
+      const related_pk = this.get('related_pk');
+      var model = {id: id, type: type};
+      model['model_fk'] = related_pk;
+      run(() => {
+        this.get('model').push(model);
+      });
+    },
+    delete(entry) {
+      run(() => {
+        this.get('model').push({id: entry.get('id'), removed: true});
+      });
+    },
+    changeState(address, val) {
+      address.set('state', val);
+    },
+    changeCountry(country, val) {
+      country.set('country', val);
     }
+  }
 });
 
 export default MultiAddressComponent;
