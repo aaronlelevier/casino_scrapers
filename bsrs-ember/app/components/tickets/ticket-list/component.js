db74242f92f12a6de52c4a228f91f95a324b89da
snewcomer <snewcomer24@gmail.com>
2016-05-31 19:12:28 -0700

    small changes to footer styles

--- bsrs-ember/app/components/tickets/ticket-list/component.js
+++ bsrs-ember/app/components/tickets/ticket-list/component.js
@@ -1,74 +1,74 @@
 import GridViewComponent from 'bsrs-ember/components/grid-view/component';
 
 export default GridViewComponent.extend({
-    layoutName: 'components/grid-view',
-    columns: [
-        {
-            field: 'priority.translated_name',
-            headerLabel: 'ticket.label.priority-name',
-            headerIsTranslatable: true,
-            isFilterable: true,
-            isSearchable: true,
-            templateName: 'tickets/ticket-priority-tag',
-            classNames: ['ticket-priority']
-        },
-        {
-            field: 'status.translated_name',
-            headerLabel: 'ticket.label.status-name',
-            headerIsTranslatable: true,
-            isFilterable: true,
-            isSearchable: true,
-            templateName: 'tickets/ticket-status-tag',
-            classNames: ['ticket-status']
-        },
-        {
-            field: 'number',
-            headerLabel: 'ticket.label.numberSymbol',
-            headerIsTranslatable: true,
-            isSortable: true,
-            isSearchable: true,
-            classNames: ['ticket-number']
-        },
-        {
-            field: 'created',
-            headerLabel: 'ticket.label.created',
-            headerIsTranslatable: true,
-            isSortable: true,
-            templateName: 'grid/helpers/grid-date',
-            classNames: ['ticket-created']
-        },
-        {
-            field: 'location.name',
-            headerLabel: 'ticket.label.location-name',
-            headerIsTranslatable: true,
-            isSortable: true,
-            isFilterable: true,
-            isSearchable: true,
-            classNames: ['ticket-location']
-        },
-        {
-            field: 'assignee.fullname',
-            headerLabel: 'ticket.label.assignee-fullname',
-            headerIsTranslatable: true,
-            isSortable: true,
-            isFilterable: true,
-            isSearchable: true,
-            classNames: ['ticket-assignee']
-        },
-        {
-            field: 'categories',
-            headerLabel: 'ticket.label.category-name',
-            headerIsTranslatable: true,
-            classNames: ['model-category']
-        },
-        {
-            field: 'request',
-            headerLabel: 'ticket.label.request',
-            headerIsTranslatable: true,
-            isSortable: true,
-            isFilterable: true,
-            isSearchable: true,
-            classNames: ['ticket-request']
-        }
-    ]
+  layoutName: 'components/grid-view',
+  columns: [
+    {
+      field: 'priority.translated_name',
+      headerLabel: 'ticket.label.priority-name',
+      headerIsTranslatable: true,
+      isFilterable: true,
+      isSearchable: true,
+      templateName: 'tickets/ticket-priority-tag',
+      classNames: ['ticket-priority']
+    },
+    {
+      field: 'status.translated_name',
+      headerLabel: 'ticket.label.status-name',
+      headerIsTranslatable: true,
+      isFilterable: true,
+      isSearchable: true,
+      templateName: 'tickets/ticket-status-tag',
+      classNames: ['ticket-status']
+    },
+    {
+      field: 'number',
+      headerLabel: 'ticket.label.numberSymbol',
+      headerIsTranslatable: true,
+      isSortable: true,
+      isSearchable: true,
+      classNames: ['ticket-number']
+    },
+    {
+      field: 'created',
+      headerLabel: 'ticket.label.created',
+      headerIsTranslatable: true,
+      isSortable: true,
+      templateName: 'grid/helpers/grid-date',
+      classNames: ['ticket-created']
+    },
+    {
+      field: 'location.name',
+      headerLabel: 'ticket.label.location-name',
+      headerIsTranslatable: true,
+      isSortable: true,
+      isFilterable: true,
+      isSearchable: true,
+      classNames: ['ticket-location']
+    },
+    {
+      field: 'assignee.fullname',
+      headerLabel: 'ticket.label.assignee-fullname',
+      headerIsTranslatable: true,
+      isSortable: true,
+      isFilterable: true,
+      isSearchable: true,
+      classNames: ['ticket-assignee']
+    },
+    {
+      field: 'categories',
+      headerLabel: 'ticket.label.category-name',
+      headerIsTranslatable: true,
+      classNames: ['model-category']
+    },
+    {
+      field: 'request',
+      headerLabel: 'ticket.label.request',
+      headerIsTranslatable: true,
+      isSortable: true,
+      isFilterable: true,
+      isSearchable: true,
+      classNames: ['ticket-request']
+    }
+  ]
 });
