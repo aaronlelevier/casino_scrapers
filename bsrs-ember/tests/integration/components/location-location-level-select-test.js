db89491d3f088e31f2a96ba2faeb5e6c908f7a34
snewcomer <snewcomer24@gmail.com>
2016-06-05 14:21:21 -0700

    [POWER SELECT]: remove need for location-level-select by using existing power-select-foreign key
    close #239

--- bsrs-ember/tests/integration/components/location-location-level-select-test.js
+++ bsrs-ember/tests/integration/components/location-location-level-select-test.js
@@ -11,11 +11,11 @@ import loadTranslations from 'bsrs-ember/tests/helpers/translations';
 import { clickTrigger, nativeMouseUp } from '../../helpers/ember-power-select';
 
 let store, locationz, location_level, all_location_levels, location_levels, trans, run = Ember.run;
-const PowerSelect = '.ember-power-select-trigger';
 const COMPONENT = '.t-location-level-select';
 const DROPDOWN = '.ember-power-select-dropdown';
+const SELECTED = '.ember-power-select-selected-item';
 
-moduleForComponent('location-location-level-select', 'integration: location-location-level-select test', {
+moduleForComponent('location location-level-select', 'integration: location location-level-select', {
   integration: true,
   setup() {
 
@@ -38,9 +38,8 @@ test('should render a selectbox when location type options are empty', function(
   let all_location_levels = Ember.A([]);
   this.set('model', locationz);
   this.set('all_location_levels', all_location_levels);
-  this.render(hbs`{{location-level-select model=model options=all_location_levels}}`);
+  this.render(hbs`{{power-select-foreign-key mainModel=model selected=model.location_level relatedModels=all_location_levels change_method='change_location_level' relatedModelName='location-level' notTranslated=true}}`);
   let $component = this.$(COMPONENT);
-  //assert.equal($component.find(PowerSelect).text().trim(), 'Select One');
   clickTrigger();
   assert.equal($(DROPDOWN).length, 1);
   assert.equal($('.ember-basic-dropdown-content').length, 1);
@@ -53,9 +52,8 @@ test('should render a selectbox when location type options are empty', function(
 test('should render a selectbox with bound options', function(assert) {
   this.set('model', locationz);
   this.set('all_location_levels', all_location_levels);
-  this.render(hbs`{{location-level-select model=model options=all_location_levels}}`);
+  this.render(hbs`{{power-select-foreign-key mainModel=model selected=model.location_level relatedModels=all_location_levels change_method='change_location_level' relatedModelName='location-level' notTranslated=true}}`);
   let $component = this.$(COMPONENT);
-  //assert.equal($component.find(PowerSelect).text().trim(), 'Select One');
   clickTrigger();
   assert.equal($(DROPDOWN).length, 1);
   assert.equal($('.ember-basic-dropdown-content').length, 1);
@@ -65,9 +63,8 @@ test('should render a selectbox with bound options', function(assert) {
 test('should be able to select new location level when one doesnt exist', function(assert) {
   this.set('model', locationz);
   this.set('all_location_levels', all_location_levels);
-  this.render(hbs`{{location-level-select model=model options=all_location_levels}}`);
+  this.render(hbs`{{power-select-foreign-key mainModel=model selected=model.location_level relatedModels=all_location_levels change_method='change_location_level' relatedModelName='location-level' notTranslated=true}}`);
   let $component = this.$(COMPONENT);
-  //assert.equal($component.find(PowerSelect).text().trim(), 'Select One');
   clickTrigger();
   assert.equal($(DROPDOWN).length, 1);
   assert.equal($('.ember-basic-dropdown-content').length, 1);
@@ -76,7 +73,7 @@ test('should be able to select new location level when one doesnt exist', functi
   assert.equal($(DROPDOWN).length, 0);
   assert.equal($('.ember-basic-dropdown-content').length, 0);
   assert.equal($('.ember-power-select-options > li').length, 0);
-  assert.equal($component.find(PowerSelect).text().trim().split(' +')[0].split(' ')[0].trim(), LLD.nameCompany);
+  assert.equal($component.find(SELECTED).text().trim(), LLD.nameCompany);
   assert.equal(locationz.get('location_level'), LLD.nameCompany);
 });
 
@@ -87,9 +84,9 @@ test('should be able to select same location level when location already has a l
   });
   this.set('model', locationz);
   this.set('all_location_levels', all_location_levels);
-  this.render(hbs`{{location-level-select model=model options=all_location_levels}}`);
+  this.render(hbs`{{power-select-foreign-key mainModel=model selected=model.location_level relatedModels=all_location_levels change_method='change_location_level' relatedModelName='location-level' notTranslated=true}}`);
   let $component = this.$(COMPONENT);
-  assert.equal($component.find(PowerSelect).text().trim().split(' +')[0].split(' ')[0].trim(), LLD.nameCompany);
+  assert.equal($component.find(SELECTED).text().trim(), LLD.nameCompany);
   clickTrigger();
   assert.equal($(DROPDOWN).length, 1);
   assert.equal($('.ember-basic-dropdown-content').length, 1);
@@ -98,7 +95,7 @@ test('should be able to select same location level when location already has a l
   assert.equal($(DROPDOWN).length, 0);
   assert.equal($('.ember-basic-dropdown-content').length, 0);
   assert.equal($('.ember-power-select-options > li').length, 0);
-  assert.equal($component.find(PowerSelect).text().trim().split(' +')[0].split(' ')[0].trim(), LLD.nameCompany);
+  assert.equal($component.find(SELECTED).text().trim(), LLD.nameCompany);
   assert.equal(locationz.get('location_level').get('name'), LLD.nameCompany);
 });
 
@@ -109,9 +106,9 @@ test('should be able to select new location level when location already has a lo
   });
   this.set('model', locationz);
   this.set('all_location_levels', all_location_levels);
-  this.render(hbs`{{location-level-select model=model options=all_location_levels}}`);
+  this.render(hbs`{{power-select-foreign-key mainModel=model selected=model.location_level relatedModels=all_location_levels change_method='change_location_level' relatedModelName='location-level' notTranslated=true}}`);
   let $component = this.$(COMPONENT);
-  assert.equal($component.find(PowerSelect).text().trim().split(' +')[0].split(' ')[0].trim(), LLD.nameCompany);
+  assert.equal($component.find(SELECTED).text().trim(), LLD.nameCompany);
   clickTrigger();
   assert.equal($(DROPDOWN).length, 1);
   assert.equal($('.ember-basic-dropdown-content').length, 1);
@@ -120,6 +117,6 @@ test('should be able to select new location level when location already has a lo
   assert.equal($(DROPDOWN).length, 0);
   assert.equal($('.ember-basic-dropdown-content').length, 0);
   assert.equal($('.ember-power-select-options > li').length, 0);
-  assert.equal($component.find(PowerSelect).text().trim().split(' +')[0].split(' ')[0].trim(), LLD.nameDistrict);
+  assert.equal($component.find(SELECTED).text().trim(), LLD.nameDistrict);
   assert.equal(locationz.get('location_level').get('name'), LLD.nameDistrict);
 });
