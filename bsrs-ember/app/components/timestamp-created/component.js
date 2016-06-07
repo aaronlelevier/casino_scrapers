e927827f2f54bfc11d154ef3ce9dd9ee2509c0a5
snewcomer <snewcomer24@gmail.com>
2016-06-07 12:01:23 -0700

    [CSS]: && Andy finish moving css into ITCSS structure

--- bsrs-ember/app/components/timestamp-created/component.js
+++ bsrs-ember/app/components/timestamp-created/component.js
@@ -2,10 +2,9 @@ import Ember from 'ember';
 import moment from 'moment';
 
 export default Ember.Component.extend({
-    tagName: 'span',
-    classNames: ['create-date', 't-created-comment'],
-    title: Ember.computed(function(){
-        return moment(this.get('model').get('created')).format('dddd, MMMM Do YYYY, h:mm:ss a z');
-    }),
+  tagName: 'span',
+  classNames: ['create-date', 't-created-comment'],
+  title: Ember.computed(function(){
+    return moment(this.get('model').get('created')).format('dddd, MMMM Do YYYY, h:mm:ss a z');
+  }),
 });
-
