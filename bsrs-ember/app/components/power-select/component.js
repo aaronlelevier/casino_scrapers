1486f365bc4ea2fb9804ae668f36d53669213b7d
snewcomer <snewcomer24@gmail.com>
2016-06-14 16:10:57 -0700

    bring back power select component default configs

--- bsrs-ember/app/components/power-select/component.js
+++ bsrs-ember/app/components/power-select/component.js
@@ -5,17 +5,17 @@ export default EmberPowerSelect.extend({
   // You can even use computed properties to do other stuff, like apply i18n, that wouldn't be
   // possible with static configuration.
   renderInPlace: true,
-  // i18n: Ember.inject.service(),
-  // loadingMessage: Ember.computed('i18n.locale', function() {
-  //   return this.get('i18n').t('selects.loading');
-  // }),
-  // searchMessage: Ember.computed('i18n.locale', function() {
-  //   return this.get('i18n').t('selects.searchMessage');
-  // }),
-  // searchPlaceholder: Ember.computed('i18n.locale', function() {
-  //   return this.get('i18n').t('selects.searchPlaceholder');
-  // }),
-  // noMatchesMessage: Ember.computed('i18n.locale', function() {
-  //   return this.get('i18n').t('selects.noMatchesMessage');
-  // }),
+  i18n: Ember.inject.service(),
+  loadingMessage: Ember.computed('i18n.locale', function() {
+    return this.get('i18n').t('selects.loading');
+  }),
+  searchMessage: Ember.computed('i18n.locale', function() {
+    return this.get('i18n').t('selects.searchMessage');
+  }),
+  searchPlaceholder: Ember.computed('i18n.locale', function() {
+    return this.get('i18n').t('selects.searchPlaceholder');
+  }),
+  noMatchesMessage: Ember.computed('i18n.locale', function() {
+    return this.get('i18n').t('selects.noMatchesMessage');
+  }),
 });
