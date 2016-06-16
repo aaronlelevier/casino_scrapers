6c324926ab5dcfbc7b9857eb1c56f0d12849a3be
snewcomer <snewcomer24@gmail.com>
2016-06-15 18:45:05 -0700

    small note on return value for update-find function

--- bsrs-ember/app/mixins/update-find.js
+++ bsrs-ember/app/mixins/update-find.js
@@ -5,28 +5,29 @@ import Ember from 'ember';
 * @param {string} value - 'wat'
 * @param {string} find - 'location.name:wat,status.translated_name:foo'
 * pure function that does not mutate state
+* @return {string} newFind - `find` string 'location.name:newwat,status.translated_name:foo' for that one column (desktop - saving-filter component, mobile - grid-head)
 */
 var UpdateFind = Ember.Mixin.create({
-    update_find_query: function(column, value, find) {
-        find = find || '';
-        let newFind = '';
-        let hash = column + ':' + value;
-        /* array of existing filter keys */
-        let keysOnlyArray = find.split(',').map((criteria) => {
-            return criteria.split(':')[0];
-        });
-        /* if not in array, append column and new column:value */
-        if(Ember.$.inArray(column, keysOnlyArray) === -1) {
-            newFind = find && find.length > 0 ? find + ',' + hash : hash;
-        } else {
-            /* otherwise build RegExp object (/location.name:\w*,?/) and clear it out or replace the existing with new hash */
-            let regex = new RegExp(column +':\\w*,?');
-            let replacedFind = value === '' ? find.replace(regex, '') : find.replace(regex, hash + ',');
-            /* trailing comma needs to be removed */
-            newFind = replacedFind.replace(/,+$/, '');
-        }
-        return newFind;
+  update_find_query: function(column, value, find) {
+    find = find || '';
+    let newFind = '';
+    let hash = column + ':' + value;
+    /* array of existing filter keys */
+    let keysOnlyArray = find.split(',').map((criteria) => {
+      return criteria.split(':')[0];
+    });
+    /* if not in array, append column and new column:value */
+    if(Ember.$.inArray(column, keysOnlyArray) === -1) {
+      newFind = find && find.length > 0 ? find + ',' + hash : hash;
+    } else {
+      /* otherwise build RegExp object (/location.name:\w*,?/) and clear it out or replace the existing with new hash */
+      let regex = new RegExp(column +':\\w*,?');
+      let replacedFind = value === '' ? find.replace(regex, '') : find.replace(regex, hash + ',');
+      /* trailing comma needs to be removed */
+      newFind = replacedFind.replace(/,+$/, '');
     }
+    return newFind;
+  }
 });
 
 export default UpdateFind;
