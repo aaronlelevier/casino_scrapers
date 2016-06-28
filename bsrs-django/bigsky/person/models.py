f54e3225919724d0fc8c4b406d6c2d2ac2f30836
aronysidoro <pyaaron@gmail.com>
2016-06-28 10:53:16 -0700

    [FIXTURES] regen fixtures in order to allow NULL for 'dashboard_text' field on Role b/c this field can be inherited from their Tenant.

--- bsrs-django/bigsky/person/models.py
+++ bsrs-django/bigsky/person/models.py
@@ -43,7 +43,7 @@ class Role(SettingMixin, BaseModel):
     # Required
     name = models.CharField(max_length=75, unique=True, help_text="Will be set to the Group Name")
     categories = models.ManyToManyField(Category, blank=True) 
-    dashboard_text = models.CharField(max_length=255, blank=True)
+    dashboard_text = models.CharField(max_length=255, null=True)
     create_all = models.BooleanField(blank=True, default=False,
         help_text='Allow document creation for all locations')
     modules = models.TextField(blank=True)
