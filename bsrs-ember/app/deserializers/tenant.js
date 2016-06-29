b4c048ae60bb476cfddef62b9a26c022db250de3
aronysidoro <pyaaron@gmail.com>
2016-06-29 16:34:19 -0700

    [TENANT REPO W/ MIXIN] working w/ findById mixin

--- bsrs-ember/app/deserializers/tenant.js
+++ bsrs-ember/app/deserializers/tenant.js
@@ -2,12 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Object.extend({
   deserialize(response, id) {
-    let store = this.get('simpleStore');
-    let existing_tenant = store.find('tenant', id);
-    if (!existing_tenant.get('id') || existing_tenant.get('isNotDirtyOrRelatedNotDirty')) {
-      let tenant = store.push('tenant', response);
-      tenant.save();
-      return tenant;
-    }
+    return this.get('simpleStore').push('tenant', response);
   }
 });
