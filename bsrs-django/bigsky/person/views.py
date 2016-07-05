331bf58f5182fbae6bebbdec6f5b7c368b5c84aa
snewcomer <snewcomer24@gmail.com>
2016-07-05 13:49:09 -0700

    [PERSON]: remove uneeded regex in power select search endpoint

--- bsrs-django/bigsky/person/views.py
+++ bsrs-django/bigsky/person/views.py
@@ -105,7 +105,7 @@ class PersonViewSet(EagerLoadQuerySetMixin, SearchMultiMixin, BaseModelViewSet):
         serializer = ps.PersonCurrentSerializer(instance)
         return Response(serializer.data)
 
-    @list_route(methods=['GET'], url_path=r"person__icontains=(?P<search_key>[\w\-]+)")
+    @list_route(methods=['GET'], url_path=r"person__icontains=(?P<search_key>[\w]+)")
     def search_power_select(self, request, search_key=None):
         queryset = Person.objects.search_power_select(search_key)
         serializer = ps.PersonSearchSerializer(queryset, many=True)
