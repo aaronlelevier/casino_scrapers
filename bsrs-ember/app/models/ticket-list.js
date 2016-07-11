5e8b14a069deb2031cc8777849833e13f4ce8bbe
snewcomer <snewcomer24@gmail.com>
2016-07-11 15:06:03 -0700

    import computed instead of Ember.computed

--- bsrs-ember/app/models/ticket-list.js
+++ bsrs-ember/app/models/ticket-list.js
@@ -1,56 +1,42 @@
 import Ember from 'ember';
+const { computed } = Ember;
 import { Model } from 'ember-cli-simple-store/model';
 import inject from 'bsrs-ember/utilities/store';
 
 //TODO: does this need to be a model
 export default Model.extend({
   simpleStore: Ember.inject.service(),
-  ticket: Ember.computed(function() {
+  ticket: computed(function() {
     const store = this.get('simpleStore');
     return store.find('ticket', this.get('id'));
   }),
-  isDirtyOrRelatedDirty: Ember.computed('ticket.isDirtyOrRelatedDirty', function() {
+  isDirtyOrRelatedDirty: computed('ticket.isDirtyOrRelatedDirty', function() {
     return this.get('ticket').get('isDirtyOrRelatedDirty');
   }),
-  isNotDirtyOrRelatedNotDirty: Ember.computed.not('isDirtyOrRelatedDirty'),
-  status: Ember.computed(function() {
+  isNotDirtyOrRelatedNotDirty: computed.not('isDirtyOrRelatedDirty'),
+  status: computed(function() {
     const store = this.get('simpleStore');
     const ticket_status_list = store.find('general-status-list');
     return ticket_status_list.filter((ts) => {
       return Ember.$.inArray(this.get('id'), ts.get('tickets')) > -1;
     }).objectAt(0);
   }),
-  priority: Ember.computed(function() {
+  priority: computed(function() {
     const store = this.get('simpleStore');
     const ticket_priority_list = store.find('ticket-priority-list');
     return ticket_priority_list.filter((tp) => {
       return Ember.$.inArray(this.get('id'), tp.get('tickets')) > -1;
     }).objectAt(0);
   }),
-  assignee: Ember.computed(function() {
-    const store = this.get('simpleStore');
-    const assignees = store.find('person-list');
-    return assignees.filter((assignee) => {
-      return Ember.$.inArray(this.get('id'), assignee.get('tickets')) > -1;
-    }).objectAt(0);
-  }),
-  status_class: Ember.computed('status', function(){
+  status_class: computed('status', function(){
     const name = this.get('status.name');
     return name ? name.replace(/\./g, '-') : '';
   }),
-  priority_class: Ember.computed('priority', function(){
+  priority_class: computed('priority', function(){
     const name = this.get('priority.name');
     return name ? name.replace(/\./g, '-') : '';
   }),
-  location: Ember.computed(function() {
-    const store = this.get('simpleStore');
-    const locations = store.find('location-list');
-    const location_pk = this.get('location_fk');
-    return locations.filter((location) => {
-      return location.get('id') === location_pk;
-    }).objectAt(0);
-  }),
-  categories: Ember.computed(function() {
+  categories: computed(function() {
     const store = this.get('simpleStore');
     const categories = store.find('category-list');
     const category_ids = this.get('category_ids');
