e418c4ba6a2f3694b74a03adb95ac6fbd6b74494
aronysidoro <pyaaron@gmail.com>
2016-07-12 15:18:44 -0700

    [js format] update formating form inline to top-to-botton nested

--- bsrs-ember/app/components/categories/category-list/component.js
+++ bsrs-ember/app/components/categories/category-list/component.js
@@ -3,11 +3,34 @@ import GridViewComponent from 'bsrs-ember/components/grid-view/component';
 
 export default GridViewComponent.extend({
   layoutName: 'components/grid-view',
-  columns: [
-    {field: 'name', headerLabel: 'admin.category.label.name', headerIsTranslatable: true, isSortable: true, isFilterable: true, isSearchable: true},
-    {field: 'description', headerLabel: 'admin.category.label.description', headerIsTranslatable: true, isSortable: true, isFilterable: true, isSearchable: true},
-    {field: 'label', headerLabel: 'admin.category.label.label', headerIsTranslatable: true, isSortable: true, isFilterable: true, isSearchable: true},
-    {field: 'cost_amount', headerLabel: 'admin.category.label.cost_amount', headerIsTranslatable: true},
-    {field: 'cost_code', headerLabel: 'admin.category.label.cost_code', headerIsTranslatable: true}
-  ]
+  columns: [{
+    field: 'name',
+    headerLabel: 'admin.category.label.name',
+    headerIsTranslatable: true,
+    isSortable: true,
+    isFilterable: true,
+    isSearchable: true
+  }, {
+    field: 'description',
+    headerLabel: 'admin.category.label.description',
+    headerIsTranslatable: true,
+    isSortable: true,
+    isFilterable: true,
+    isSearchable: true
+  }, {
+    field: 'label',
+    headerLabel: 'admin.category.label.label',
+    headerIsTranslatable: true,
+    isSortable: true,
+    isFilterable: true,
+    isSearchable: true
+  }, {
+    field: 'cost_amount',
+    headerLabel: 'admin.category.label.cost_amount',
+    headerIsTranslatable: true
+  }, {
+    field: 'cost_code',
+    headerLabel: 'admin.category.label.cost_code',
+    headerIsTranslatable: true
+  }]
 });
