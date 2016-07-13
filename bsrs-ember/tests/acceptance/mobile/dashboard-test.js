15567171f277c0470f3813289b4abaf369702d62
snewcomer <snewcomer24@gmail.com>
2016-07-13 16:17:55 -0700

    [MOBILE]: application layou test for drawer

--- bsrs-ember/tests/acceptance/mobile/dashboard-test.js
+++ bsrs-ember/tests/acceptance/mobile/dashboard-test.js
@@ -3,22 +3,22 @@ const { run } = Ember;
 import module from 'bsrs-ember/tests/helpers/module';
 import { test } from 'qunit';
 import startApp from 'bsrs-ember/tests/helpers/start-app';
-import {xhr, clearxhr} from 'bsrs-ember/tests/helpers/xhr';
-import config from 'bsrs-ember/config/environment';
-import BASEURLS from 'bsrs-ember/tests/helpers/urls';
+import { xhr, clearxhr } from 'bsrs-ember/tests/helpers/xhr';
 import TENANT_DEFAULTS from 'bsrs-ember/vendor/defaults/tenant';
+import generalPage from 'bsrs-ember/tests/pages/general';
+import pageDrawer from 'bsrs-ember/tests/pages/nav-drawer';
+import BASEURLS, { DASHBOARD_URL } from 'bsrs-ember/utilities/urls';
 
 var application, store, dashboard_xhr, flexi, bp;
 
-const PREFIX = config.APP.NAMESPACE;
-const DASHBOARD_URL = BASEURLS.dashboard_url;
+const NAVBAR = '.t-navbar-items';
 
 module('Acceptance | mobile dashboard test', {
   beforeEach() {
     /* SETUP */
     application = startApp();
     store = application.__container__.lookup('service:simpleStore');
-    dashboard_xhr = xhr(`${PREFIX}${DASHBOARD_URL}/`, 'GET', null, {}, 200, {settings: {dashboard_text: TENANT_DEFAULTS.dashboard_text}});
+    dashboard_xhr = xhr(`${DASHBOARD_URL}`, 'GET', null, {}, 200, {settings: {dashboard_text: TENANT_DEFAULTS.dashboard_text}});
     /* MOBILE RENDER */
     flexi = application.__container__.lookup('service:device/layout');
     const breakpoints = flexi.get('breakpoints');
@@ -37,10 +37,35 @@ module('Acceptance | mobile dashboard test', {
 });
 
 test('dashboard renders with welcome text and no sidebar', assert => {
-  visit(DASHBOARD_URL);
+  generalPage.visitDashboard();
   andThen(() => {
     assert.equal(find('.t-dashboard-text').text().trim(), 'Welcome');
     assert.equal(find('.t-dashboard-text h1').prop('tagName'), 'H1');
     assert.equal(find('.t-side-menu').length, 0);
+  });
+});
+
+test('visiting dashboard can click on tray and show base modules', function(assert) {
+  generalPage.visitDashboard();
+  andThen(() => {
+    assert.equal(currentURL(), BASEURLS.DASHBOARD_URL);
+    assert.equal(find('label > i').attr('class').split(/-/)[2], 'reorder');
+    assert.equal(find('li.nav-item:eq(0)').text(), t('modules.tickets.newTickets'));
+    assert.equal(find('li.nav-item:eq(1)').text(), t('modules.tickets.openTickets'));
+    assert.equal(find('li.nav-item:eq(2)').text(), t('modules.workOrders.overdue'));
+    assert.equal(find('li.nav-item:eq(3)').text(), t('admin.approval.other'));
+    assert.equal(find('li.nav-item:eq(5)').text(), t('modules.tickets.titleShort'));
+    assert.equal(find('li.nav-item:eq(6)').text(), t('modules.workOrders.titleShort'));
+    assert.equal(find('li.nav-item:eq(7)').text(), t('modules.invoices.titleShort'));
+    assert.equal(find('li.nav-item:eq(8)').text(), t('admin.title'));
+    assert.notOk(find('.t-nav-trigger').prop('checked'));
+  });
+  pageDrawer.clickDrawer();
+  andThen(() => {
+    assert.ok(find('.t-nav-trigger').prop('checked'));
+  });
+  pageDrawer.clickDrawer();
+  andThen(() => {
+    assert.notOk(find('.t-nav-trigger').prop('checked'));
   });
 });
