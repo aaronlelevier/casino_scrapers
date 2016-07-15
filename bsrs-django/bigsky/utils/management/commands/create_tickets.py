ad2b7c1760ec93093acda67026876c20fdd263b3
aronysidoro <pyaaron@gmail.com>
2016-07-14 17:00:17 -0700

    [AP] add re-gen logic for Assignment Profiles to 'create_tickets' custom mgmg command, and re-gen fixtures to include them b/c table structure changed.

--- bsrs-django/bigsky/utils/management/commands/create_tickets.py
+++ bsrs-django/bigsky/utils/management/commands/create_tickets.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 
+from routing.tests.factory import create_assignments
 from ticket.tests.factory_related import (create_ticket_statuses, create_ticket_priorities)
 from ticket.tests.factory import (create_ticket_activity_types, create_tickets)
 
@@ -12,3 +13,6 @@ class Command(BaseCommand):
         create_ticket_priorities()
         create_ticket_activity_types()
         create_tickets(100)
+
+        # assignment profiles
+        create_assignments()
