a6d3aa245aa682908bfcfdbf02749cec2b62e44f
snewcomer <snewcomer24@gmail.com>
2016-07-19 15:15:23 -0700

    console log uuid gen

--- bsrs-ember/app/models/uuid.js
+++ bsrs-ember/app/models/uuid.js
@@ -3,6 +3,8 @@ import random from 'bsrs-ember/models/random';
 
 export default Ember.Object.extend({
   v4: function() {
-    return random.uuid();
+    var x = random.uuid();
+    console.log(x);
+    return x;
   }
 });
