ec112ffd7ef8cac5d33f60d6c4a4c885e9ebcdba
snewcomer <snewcomer24@gmail.com>
2016-07-21 10:33:36 -0700

    cleanup profile single test

--- bsrs-ember/tests/integration/components/profile-single-test.js
+++ bsrs-ember/tests/integration/components/profile-single-test.js
@@ -5,8 +5,6 @@ import { moduleForComponent, test } from 'ember-qunit';
 import { getLabelText } from 'bsrs-ember/tests/helpers/translations';
 import module_registry from 'bsrs-ember/tests/helpers/module_registry';
 import PD from 'bsrs-ember/vendor/defaults/profile';
-import page from 'bsrs-ember/tests/pages/profile';
-import generalPage from 'bsrs-ember/tests/pages/general';
 
 var store, model, trans;
 
@@ -15,22 +13,15 @@ const ERR_TEXT = '.validated-input-error-dialog';
 moduleForComponent('profile-single', 'integration: profile-single test', {
   integration: true,
   setup() {
-    page.setContext(this);
-    generalPage.setContext(this);
     store = module_registry(this.container, this.registry, ['model:profile']);
     trans = this.container.lookup('service:i18n');
-    run(function() {
+    run(() => {
       model = store.push('profile', {
         id: PD.idOne,
         description: PD.descOne,
       });
     });
-    this.set('model', model);
   },
-  afterEach() {
-    page.removeContext(this);
-    generalPage.removeContext(this);
-  }
 });
 
 test('description is required validation, cannot save w/o description', function(assert) {
