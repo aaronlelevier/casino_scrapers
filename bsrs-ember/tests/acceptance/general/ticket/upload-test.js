e6968a7d777ba97776f7345c77ee85815b4a28ee
snewcomer <snewcomer24@gmail.com>
2016-07-20 22:55:30 -0700

    [TEST]: uuidReset jshint notify + variable remove from test

--- bsrs-ember/tests/acceptance/general/ticket/upload-test.js
+++ bsrs-ember/tests/acceptance/general/ticket/upload-test.js
@@ -31,7 +31,6 @@ moduleForAcceptance('Acceptance | ticket file upload test', {
   beforeEach() {
     store = this.application.__container__.lookup('service:simpleStore');
     xhr(`${TICKETS_URL}${TD.idOne}/activity/`, 'GET', null, {}, 200, TA_FIXTURES.empty());
-    originaldd_uuid = random.uuid;
     random.uuid = function() { return UUID.value; };
   },
 });
