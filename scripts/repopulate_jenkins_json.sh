52e010cd622ef5f1c68a7146f1f3da457a1fd27a
aronysidoro <pyaaron@gmail.com>
2016-07-22 14:20:38 -0700

    [FIXTURES] regen fixtures so 'repopulate' and 'build' scripts work again, reorder fixtures so then work based on their dependencies, now that Category and LocationLevel have a Tenant.

--- scripts/repopulate_jenkins_json.sh
+++ scripts/repopulate_jenkins_json.sh
@@ -13,24 +13,28 @@ createdb ci
 wait
 ./manage.py migrate
 wait
+# leaf node
 ./manage.py loaddata fixtures/contact.Country.json
 ./manage.py loaddata fixtures/contact.State.json
-./manage.py loaddata fixtures/translation.json
-./manage.py loaddata fixtures/accounting.Currency.json
 ./manage.py loaddata fixtures/contact.EmailType.json
 ./manage.py loaddata fixtures/contact.PhoneNumberType.json
 ./manage.py loaddata fixtures/contact.AddressType.json
+./manage.py loaddata fixtures/translation.json
+# tenant requires currency and dtd-start
+./manage.py loaddata fixtures/accounting.Currency.json
+./manage.py loaddata fixtures/dtd.json
+./manage.py loaddata fixtures/tenant.json
+# category and location need a tenant
 ./manage.py loaddata fixtures/category.json
+./manage.py loaddata fixtures/location.json
+# other
 ./manage.py loaddata fixtures/third_party.json
 ./manage.py loaddata fixtures/auth.json
-./manage.py loaddata fixtures/location.json
-./manage.py loaddata fixtures/dtd.json
 
 wait
 ./manage.py create_all_people
 wait
 ./manage.py dumpdata person --indent=2 > fixtures/person.json
-./manage.py dumpdata tenant --indent=2 > fixtures/tenant.json
 
 wait
 ./manage.py create_tickets
