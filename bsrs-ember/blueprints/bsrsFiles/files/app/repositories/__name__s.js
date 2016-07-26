7aed8a20e6ab909a3c30b4d908e60120cdcf62d5
snewcomer <snewcomer24@gmail.com>
2016-07-25 18:16:11 -0700

    [BLUEPRINT]: update blueprint files

--- bsrs-ember/blueprints/bsrsFiles/files/app/repositories/__name__s.js
+++ bsrs-ember/blueprints/bsrsFiles/files/app/repositories/__name__s.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+const { run } = Ember;
 import config from 'bsrs-ember/config/environment';
 import PromiseMixin from 'ember-promise/mixins/promise';
 import injectDeserializer from 'bsrs-ember/utilities/deserializer';
@@ -7,9 +8,6 @@ import FindByIdMixin from 'bsrs-ember/mixins/repositories/findById';
 import CRUDMixin from 'bsrs-ember/mixins/repositories/crud';
 import GridRepositoryMixin from 'bsrs-ember/mixins/repositories/grid';
 import BASEURLS, { <%= CapitalizeModule %>_URL, <%= secondModelPluralCaps %>_URL } from 'bsrs-ember/utilities/urls';
-
-const { run } = Ember;
-var PREFIX = config.APP.NAMESPACE;
 
 export default Ember.Object.extend(GridRepositoryMixin, FindByIdMixin, CRUDMixin, {
   type: '<%= dasherizedModuleName %>',
@@ -21,11 +19,4 @@ export default Ember.Object.extend(GridRepositoryMixin, FindByIdMixin, CRUDMixin
   <%= camelizedModuleName %>Deserializer: injectDeserializer('<%= dasherizedModuleName %>'),
   url: <%= CapitalizeModule %>_URL,
   deserializer: Ember.computed.alias('<%= camelizedModuleName %>Deserializer'),
-  update(model) {
-    let id = model.get('id');
-    return PromiseMixin.xhr(`${<%= CapitalizeModule %>_URL}${id}/`, 'PUT', { data: JSON.stringify(model.serialize()) }).then(() => {
-      model.save();
-      model.saveRelated();
-    });
-  },
 });
