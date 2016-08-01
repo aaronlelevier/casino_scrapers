7895667cc5d0be397b5bdf780586ef87f6b94a41
snewcomer <snewcomer24@gmail.com>
2016-08-01 15:47:31 -0700

    [DJANGO]: context_instance is deprecated

--- bsrs-django/bigsky/bigsky/views.py
+++ bsrs-django/bigsky/bigsky/views.py
@@ -86,13 +86,13 @@ def logout(request):
 
 def handler404(request):
     response = render(request, 'error/404.html', {},
-        context_instance=RequestContext(request))
+        context=RequestContext(request))
     response.status_code = 404
     return response
 
 
 def handler500(request):
     response = render(request, 'error/500.html', {},
-        context_instance=RequestContext(request))
+        context=RequestContext(request))
     response.status_code = 500
     return response
