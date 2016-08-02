f0f6bc029ccbc869ba10a266e5e618e4876fd2c8
snewcomer <snewcomer24@gmail.com>
2016-08-02 13:07:01 -0700

    mocks for assignment 4200 land

---
+++ bsrs-ember/server/mocks/admin/assignments.js
@@ -0,0 +1,44 @@
+var ASSIGMENT_FIXTURES = require('../../../vendor/assignment_fixtures.js');
+
+module.exports = function(app) {
+  var express = require('express');
+  var adminAssignmentsRouter = express.Router();
+
+  adminAssignmentsRouter.get('/', function(req, res) {
+    var page = req.query.page || 1;
+    var sort = req.query.ordering ? req.query.ordering.trim() : '';
+    var search = req.query.search ? req.query.search.trim() : '';
+    var page_size = req.query.page_size ? req.query.page_size.trim() : '';
+    if(search && search.length > 0) {
+        var term = decodeURIComponent(search);
+        res.send(ASSIGMENT_FIXTURES.searched(term, sort, page));
+    } else if(sort && sort.length > 0) {
+        res.send(ASSIGMENT_FIXTURES.sorted(sort, page));
+    } else if(page_size && page_size.length > 0) {
+        res.send(ASSIGMENT_FIXTURES.paginated(page_size));
+    } else if(parseInt(page) > 1) {
+        res.send(ASSIGMENT_FIXTURES.list_two());
+    } else {
+        res.send(ASSIGMENT_FIXTURES.list());
+    }
+  });
+
+  adminAssignmentsRouter.post('/', function(req, res) {
+    res.status(201);
+    res.send(req.body);
+  });
+
+  adminAssignmentsRouter.get('/:id', function(req, res) {
+    res.send(ASSIGMENT_FIXTURES.detail());
+  });
+
+  adminAssignmentsRouter.put('/:id', function(req, res) {
+    res.send(ASSIGMENT_FIXTURES.put(req.params.id));
+  });
+
+  adminAssignmentsRouter.delete('/:id', function(req, res) {
+    res.status(204).end();
+  });
+
+  app.use('/api/admin/assignments', adminAssignmentsRouter);
+};
