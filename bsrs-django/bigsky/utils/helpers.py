757d704f973bc64e01b8770852f84b8f6ab8c1e9
aronysidoro <pyaaron@gmail.com>
2016-08-02 15:00:45 -0700

    [django1.10 test fails] fix fails for Ticket.

--- bsrs-django/bigsky/utils/helpers.py
+++ bsrs-django/bigsky/utils/helpers.py
@@ -3,6 +3,8 @@ import uuid
 
 from django.conf import settings
 from django.contrib.auth.models import ContentType
+from django.db import models
+from django.forms.models import model_to_dict as django_model_to_dict
 
 
 BASE_UUID = "530c4-ce6c-4724-9cfd-37a16e787"
@@ -18,6 +20,18 @@ def model_to_json_select_related(model, *args):
 
 def model_to_json_prefetch_related(model, *args):
     return json.dumps([m.to_dict() for m in model.objects.all().prefetch_related(*args)])
+
+
+def model_to_dict(instance):
+    """
+    Because as of Django 1.10 `model_to_dict` returns a QuerySet for
+    m2m related models.
+    """
+    d = django_model_to_dict(instance)
+    for k,v in d.items():
+        if isinstance(v, models.query.QuerySet):
+            d[k] = [x for x in v.values_list('id', flat=True)]
+    return d
 
 
 def get_content_type_number(model):
