1dcfd5bb7367cb0a86ef6f10b16b7ff24569e875
snewcomer <snewcomer24@gmail.com>
2016-08-10 09:45:44 -0700

    [LLEVEL]: power select tests commented out for now as action in controller.  If routable components come back, then bring back!

--- bsrs-ember/tests/integration/components/location-level-children-select-test.js
+++ bsrs-ember/tests/integration/components/location-level-children-select-test.js
@@ -14,14 +14,12 @@ const PowerSelect = '.ember-power-select-trigger > .ember-power-select-multiple-
 const COMPONENT = '.t-location-level-children-select';
 const DROPDOWN = '.ember-power-select-dropdown';
 
-moduleForComponent('location-level-children-select', 'integration: location-level-children-select test', {
+moduleForComponent('location-level-children-select', 'scott integration: location-level-children-select test', {
   integration: true,
   setup() {
-
     trans = this.container.lookup('service:i18n');
     loadTranslations(trans, translations.generate('en'));
     translation.initialize(this);
-
     store = module_registry(this.container, this.registry, ['model:location-level']);
     run(function() {
       location_level = store.push('location-level', {id: LLD.idOne, name: LLD.nameCompany});
@@ -62,79 +60,80 @@ test('should render a selectbox with bound options', function(assert) {
   assert.equal($('.ember-power-select-options > li').length, 5);
 });
 
-test('should not be able to select same location level when one doesnt exist', function(assert) {
-  this.set('model', location_level);
-  this.set('options', location_levels);
-  this.render(hbs`{{location-level-children-power-select model=model options=options}}`);
-  let $component = this.$(COMPONENT);
-  //assert.equal($component.find(`${PowerSelect} > input`).attr('placeholder').trim(), 'Select One');
-  clickTrigger();
-  assert.equal($(DROPDOWN).length, 1);
-  assert.equal($('.ember-basic-dropdown-content').length, 1);
-  assert.equal($('.ember-power-select-options > li').length, 5);
-  nativeMouseUp(`.ember-power-select-option:contains(${LLD.nameCompany})`);
-  assert.equal($(DROPDOWN).length, 0);
-  assert.equal($('.ember-basic-dropdown-content').length, 0);
-  assert.equal($('.ember-power-select-options > li').length, 0);
-  assert.equal($component.find(PowerSelect).text().trim().split('+ ')[0].split(' ').pop(), '');
-  // assert.equal(location_level.get('children.id'), undefined);
-});
+// setChild action is in controller.  Need to migrate these tests over to acceptane
+//test('should not be able to select same location level when one doesnt exist', function(assert) {
+//  this.set('model', location_level);
+//  this.set('options', location_levels);
+//  this.render(hbs`{{location-level-children-power-select model=model options=options}}`);
+//  let $component = this.$(COMPONENT);
+//  //assert.equal($component.find(`${PowerSelect} > input`).attr('placeholder').trim(), 'Select One');
+//  clickTrigger();
+//  assert.equal($(DROPDOWN).length, 1);
+//  assert.equal($('.ember-basic-dropdown-content').length, 1);
+//  assert.equal($('.ember-power-select-options > li').length, 5);
+//  nativeMouseUp(`.ember-power-select-option:contains(${LLD.nameCompany})`);
+//  assert.equal($(DROPDOWN).length, 0);
+//  assert.equal($('.ember-basic-dropdown-content').length, 0);
+//  assert.equal($('.ember-power-select-options > li').length, 0);
+//  assert.equal($component.find(PowerSelect).text().trim().split('+ ')[0].split(' ').pop(), '');
+//  // assert.equal(location_level.get('children.id'), undefined);
+//});
 
-test('should be able to select new location level when one doesnt exist', function(assert) {
-  this.set('model', location_level);
-  this.set('options', location_levels);
-  this.render(hbs`{{location-level-children-power-select model=model options=options}}`);
-  let $component = this.$(COMPONENT);
-  //assert.equal($component.find(`${PowerSelect} > input`).attr('placeholder').trim(), 'Select One');
-  clickTrigger();
-  assert.equal($(DROPDOWN).length, 1);
-  assert.equal($('.ember-basic-dropdown-content').length, 1);
-  assert.equal($('.ember-power-select-options > li').length, 5);
-  nativeMouseUp(`.ember-power-select-option:contains(${LLD.nameRegion})`);
-  assert.equal($(DROPDOWN).length, 0);
-  assert.equal($('.ember-basic-dropdown-content').length, 0);
-  assert.equal($('.ember-power-select-options > li').length, 0);
-  assert.equal($component.find(PowerSelect).text().trim().split('+ ')[0].split(' ').pop(), LLD.nameRegion);
-  // assert.equal(location_level.get('children.id'), LLD.idRegion);
-});
+//test('should be able to select new location level when one doesnt exist', function(assert) {
+//  this.set('model', location_level);
+//  this.set('options', location_levels);
+//  this.render(hbs`{{location-level-children-power-select model=model options=options}}`);
+//  let $component = this.$(COMPONENT);
+//  //assert.equal($component.find(`${PowerSelect} > input`).attr('placeholder').trim(), 'Select One');
+//  clickTrigger();
+//  assert.equal($(DROPDOWN).length, 1);
+//  assert.equal($('.ember-basic-dropdown-content').length, 1);
+//  assert.equal($('.ember-power-select-options > li').length, 5);
+//  nativeMouseUp(`.ember-power-select-option:contains(${LLD.nameRegion})`);
+//  assert.equal($(DROPDOWN).length, 0);
+//  assert.equal($('.ember-basic-dropdown-content').length, 0);
+//  assert.equal($('.ember-power-select-options > li').length, 0);
+//  assert.equal($component.find(PowerSelect).text().trim().split('+ ')[0].split(' ').pop(), LLD.nameRegion);
+//  // assert.equal(location_level.get('children.id'), LLD.idRegion);
+//});
 
-test('selecting same location level will remove that location level', function(assert) {
-  run(function() {
-    store.push('location-level', {id: LLD.idOne, name: LLD.nameCompany, children_fks: [LLD.idTwo, LLD.idThree]});
-  });
-  this.set('model', location_level);
-  this.set('options', location_levels);
-  this.render(hbs`{{location-level-children-power-select model=model options=options}}`);
-  let $component = this.$(COMPONENT);
-  assert.equal($component.find(PowerSelect).text().trim().split('+ ')[0].split(' ').pop(), LLD.nameRegion);
-  clickTrigger();
-  assert.equal($(DROPDOWN).length, 1);
-  assert.equal($('.ember-basic-dropdown-content').length, 1);
-  assert.equal($('.ember-power-select-options > li').length, 5);
-  nativeMouseUp(`.ember-power-select-option:contains(${LLD.nameRegion})`);
-  assert.equal($(DROPDOWN).length, 0);
-  assert.equal($('.ember-basic-dropdown-content').length, 0);
-  assert.equal($('.ember-power-select-options > li').length, 0);
-  assert.equal($component.find(PowerSelect).text().trim().split('+ ')[0].split(' ').pop(), LLD.nameDepartment);
-  assert.deepEqual(location_level.get('children_fks'), [LLD.idTwo]);
-});
+//test('selecting same location level will remove that location level', function(assert) {
+//  run(function() {
+//    store.push('location-level', {id: LLD.idOne, name: LLD.nameCompany, children_fks: [LLD.idTwo, LLD.idThree]});
+//  });
+//  this.set('model', location_level);
+//  this.set('options', location_levels);
+//  this.render(hbs`{{location-level-children-power-select model=model options=options}}`);
+//  let $component = this.$(COMPONENT);
+//  assert.equal($component.find(PowerSelect).text().trim().split('+ ')[0].split(' ').pop(), LLD.nameRegion);
+//  clickTrigger();
+//  assert.equal($(DROPDOWN).length, 1);
+//  assert.equal($('.ember-basic-dropdown-content').length, 1);
+//  assert.equal($('.ember-power-select-options > li').length, 5);
+//  nativeMouseUp(`.ember-power-select-option:contains(${LLD.nameRegion})`);
+//  assert.equal($(DROPDOWN).length, 0);
+//  assert.equal($('.ember-basic-dropdown-content').length, 0);
+//  assert.equal($('.ember-power-select-options > li').length, 0);
+//  assert.equal($component.find(PowerSelect).text().trim().split('+ ')[0].split(' ').pop(), LLD.nameDepartment);
+//  assert.deepEqual(location_level.get('children_fks'), [LLD.idTwo]);
+//});
 
-test('should be able to select new location level when location already has a location level', function(assert) {
-  run(function() {
-    store.push('location-level', {id: LLD.idOne, children_fks: [LLD.idTwo, LLD.idThree]});
-  });
-  this.set('model', location_level);
-  this.set('options', location_levels);
-  this.render(hbs`{{location-level-children-power-select model=model options=options}}`);
-  let $component = this.$(COMPONENT);
-  assert.equal($component.find(PowerSelect).text().trim().split('+ ')[0].split(' ').pop(), LLD.nameRegion);
-  clickTrigger();
-  assert.equal($(DROPDOWN).length, 1);
-  assert.equal($('.ember-basic-dropdown-content').length, 1);
-  assert.equal($('.ember-power-select-options > li').length, 5);
-  nativeMouseUp(`.ember-power-select-option:contains(${LLD.nameDistrict})`);
-  assert.equal($(DROPDOWN).length, 0);
-  assert.equal($('.ember-basic-dropdown-content').length, 0);
-  assert.equal($('.ember-power-select-options > li').length, 0);
-  assert.deepEqual(location_level.get('children_fks'), [LLD.idTwo, LLD.idThree, LLD.idDistrict]);
-});
+//test('should be able to select new location level when location already has a location level', function(assert) {
+//  run(function() {
+//    store.push('location-level', {id: LLD.idOne, children_fks: [LLD.idTwo, LLD.idThree]});
+//  });
+//  this.set('model', location_level);
+//  this.set('options', location_levels);
+//  this.render(hbs`{{location-level-children-power-select model=model options=options}}`);
+//  let $component = this.$(COMPONENT);
+//  assert.equal($component.find(PowerSelect).text().trim().split('+ ')[0].split(' ').pop(), LLD.nameRegion);
+//  clickTrigger();
+//  assert.equal($(DROPDOWN).length, 1);
+//  assert.equal($('.ember-basic-dropdown-content').length, 1);
+//  assert.equal($('.ember-power-select-options > li').length, 5);
+//  nativeMouseUp(`.ember-power-select-option:contains(${LLD.nameDistrict})`);
+//  assert.equal($(DROPDOWN).length, 0);
+//  assert.equal($('.ember-basic-dropdown-content').length, 0);
+//  assert.equal($('.ember-power-select-options > li').length, 0);
+//  assert.deepEqual(location_level.get('children_fks'), [LLD.idTwo, LLD.idThree, LLD.idDistrict]);
+//});
