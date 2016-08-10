40dae528df210e3cf33049a863fb7031607e128f
snewcomer <snewcomer24@gmail.com>
2016-08-09 17:00:28 -0700

    [CATEGORY]: DDAU set state on string primitives

--- bsrs-ember/app/admin/location-levels/location-level/controller.js
+++ bsrs-ember/app/admin/location-levels/location-level/controller.js
@@ -6,12 +6,9 @@ var LocationLevelController = Ember.Controller.extend({
       this.send('closeTabMaster', tab, action);
     },
     setName(name) {
-      // mutable
       const model = this.get('model');
       model.set('name', name);
     }
   }
 });
 export default LocationLevelController;
-
-
