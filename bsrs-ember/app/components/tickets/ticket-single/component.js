c8f56b996fdf40654c513431b3ea4acd02d58044
snewcomer <snewcomer24@gmail.com>
2016-08-18 14:27:48 -0700

    [TICKET]: save task

--- bsrs-ember/app/components/tickets/ticket-single/component.js
+++ bsrs-ember/app/components/tickets/ticket-single/component.js
@@ -4,6 +4,7 @@ import TabMixin from 'bsrs-ember/mixins/components/tab/base';
 import RelaxedMixin from 'bsrs-ember/mixins/validation/relaxed';
 import { validate } from 'ember-cli-simple-validation/mixins/validate';
 import ParentValidationComponent from 'bsrs-ember/mixins/validation/parent';
+import { task } from 'ember-concurrency';
 
 var TicketSingleComponent = ParentValidationComponent.extend(RelaxedMixin, TabMixin, {
   didValidate: false,
@@ -18,25 +19,26 @@ var TicketSingleComponent = ParentValidationComponent.extend(RelaxedMixin, TabMi
     const last_dt = ticket.get('dt_path').slice(-1);
     return last_dt[0]['dtd']['id'];
   }),
+  saveTask: task(function * (update, updateActivities) {
+    this.set('submitted', true);
+    if (this.all_components_valid()) {
+      if (this.get('model.validations.isValid')) {
+        const tab = this.tab();
+        const activities = yield this.get('save')(tab, this.get('activityRepository'), update, updateActivities);
+        if (activities && updateActivities) {
+          this.set('activities', activities);
+        }
+      }
+      this.set('didValidate', true);
+    }
+  }),
   actions: {
     /* @method save
-    * @param {bool} update - general boolean to no close tab
-    * @param {bool} updateActivities - specifically to fetch activities for detail view to return and set in respective single component
-    */
+     * @param {bool} update - general boolean to no close tab
+     * @param {bool} updateActivities - specifically to fetch activities for detail view to return and set in respective single component
+     */
     save(update, updateActivities) {
-      this.set('submitted', true);
-      if (this.all_components_valid()) {
-        if (this.get('model.validations.isValid')) {
-          const tab = this.tab();
-          const promise = this.get('save')(tab, this.get('activityRepository'), update, updateActivities);
-          if (promise && promise.then && updateActivities) {
-            promise.then((activities) => {
-              this.set('activities', activities);
-            });
-          }
-        }
-        this.set('didValidate', true);
-      }
+      this.get('saveTask').perform(update, updateActivities);
     },
     deleteAttachment(tab, callback) {
       this.sendAction('deleteAttachment', tab, callback);
