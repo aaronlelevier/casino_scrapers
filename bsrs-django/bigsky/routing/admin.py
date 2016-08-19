cd4170f57323bed341b3adba3636e15b37874119
aronysidoro <pyaaron@gmail.com>
2016-08-19 14:59:06 -0700

    [ROUTING] add list display fields for help debugging.

--- bsrs-django/bigsky/routing/admin.py
+++ bsrs-django/bigsky/routing/admin.py
@@ -5,13 +5,15 @@ from routing import models
 
 @admin.register(models.Assignment)
 class AssignmentAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id', 'description')
 
 
 @admin.register(models.ProfileFilter)
 class ProfileFilterAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id', 'source_key')
 
+    def source_key(self, obj):
+        return obj.source.key or obj.source
 
 @admin.register(models.AvailableFilter)
 class AvailableFilterAdmin(admin.ModelAdmin):
