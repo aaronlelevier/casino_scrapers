2fcd40a94ebdb275e72a544390aa0e372ff1edc8
snewcomer <snewcomer24@gmail.com>
2016-08-19 10:15:42 -0700

    [SAVE]: btn disabled across all modules w/ tests

--- bsrs-ember/app/components/roles/role-single/component.js
+++ bsrs-ember/app/components/roles/role-single/component.js
@@ -3,19 +3,23 @@ import inject from 'bsrs-ember/utilities/inject';
 import {ValidationMixin, validate} from 'ember-cli-simple-validation/mixins/validate';
 import TabMixin from 'bsrs-ember/mixins/components/tab/base';
 import ChangeBoolMixin from 'bsrs-ember/mixins/components/change-bool';
+import { task } from 'ember-concurrency';
 
 var RoleSingle = Ember.Component.extend(TabMixin, ValidationMixin, ChangeBoolMixin, {
   repository: inject('role'),
   simpleStore: Ember.inject.service(),
   nameValidation: validate('model.name'),
   locationLevelValidation: validate('model.location_level'),
+  saveTask: task(function * () {
+    this.set('submitted', true);
+    if (this.get('valid')) {
+      const tab = this.tab();
+      yield this.get('save')(tab);
+    }
+  }),
   actions: {
     save() {
-      this.set('submitted', true);
-      if (this.get('valid')) {
-        const tab = this.tab();
-        return this.get('save')(tab);
-      }
+      this.get('saveTask').perform();
     },
     changedLocLevel(model, val) {
       model.set('location_level', val);
