303d4a786393031b9b879927315fe0e6491d9dfb
snewcomer <snewcomer24@gmail.com>
2016-08-24 07:42:07 -0700

    update initializers

--- bsrs-ember/app/initializers/deserializers.js
+++ bsrs-ember/app/initializers/deserializers.js
@@ -1,13 +1,12 @@
 import registerWithContainer from 'ember-cli-auto-register/register';
 
-export function initialize() {
-    var application = arguments[1] || arguments[0];
-    registerWithContainer('deserializers', application);
-    application.inject('deserializers', 'simpleStore', 'service:simpleStore');
+export function initialize(application) {
+  registerWithContainer('deserializers', application);
+  application.inject('deserializers', 'simpleStore', 'service:simpleStore');
 }
 
 export default {
-    name: 'deserializers',
-    after: 'simple-store',
-    initialize: initialize
+  name: 'deserializers',
+  after: 'simple-store',
+  initialize: initialize
 };
--- bsrs-ember/app/initializers/services.js
+++ bsrs-ember/app/initializers/services.js
@@ -1,11 +1,10 @@
-export function initialize() {
-    var application = arguments[1] || arguments[0];
-    application.inject('route', 'trx', 'service:transition');
-    application.inject('component', 'trx', 'service:transition');
-    application.inject('controller', 'trx', 'service:transition');
+export function initialize(application) {
+  application.inject('route', 'trx', 'service:transition');
+  application.inject('component', 'trx', 'service:transition');
+  application.inject('controller', 'trx', 'service:transition');
 }
 
 export default {
-    name: 'services',
-    initialize: initialize
+  name: 'services',
+  initialize: initialize
 };
