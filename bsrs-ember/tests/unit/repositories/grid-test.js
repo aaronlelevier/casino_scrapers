1dfadc547af538581fad93cce0a1036d615ac6ba
snewcomer <snewcomer24@gmail.com>
2016-08-26 10:55:45 -0700

    [UNIT]: Fix unit tests

--- bsrs-ember/tests/unit/repositories/grid-test.js
+++ bsrs-ember/tests/unit/repositories/grid-test.js
@@ -48,16 +48,18 @@ test('findWithQuery will format find url string correctly', (assert) => {
 // });
 
 test('it accepts id_in paramter single and correctly modifies url', function(assert) {
-  let subject = FakeRepo.create({url: '/api/tickets/'});
+  let subject = FakeRepo.create();
   const priority_id = 'dfe28a24-307f-4da0-85e7-cdac016808c0';
-  let endpoint = subject.modifyEndpoint(1, undefined, undefined, `priority:${priority_id}`);
+  const url = '/api/tickets/';
+  let endpoint = subject.modifyEndpoint(url, 1, undefined, undefined, `priority:${priority_id}`);
   assert.equal(endpoint, `/api/tickets/?page=1&priority__id__in=${priority_id}`);
 });
 
 test('it accepts id_in paramter multiple and correctly modifies url', function(assert) {
-  let subject = FakeRepo.create({url: '/api/tickets/'});
+  let subject = FakeRepo.create();
   const priority_id = 'dfe28a24-307f-4da0-85e7-cdac016808c0';
   const priority_two_id = '2a4c8c9c-7acb-44ca-af95-62a84e410e09';
-  let endpoint = subject.modifyEndpoint(1, undefined, undefined, `priority:${priority_id},${priority_two_id}`);
+  const url = '/api/tickets/';
+  let endpoint = subject.modifyEndpoint(url, 1, undefined, undefined, `priority:${priority_id},${priority_two_id}`);
   assert.equal(endpoint, `/api/tickets/?page=1&priority__id__in=${priority_id},${priority_two_id}`);
 });
