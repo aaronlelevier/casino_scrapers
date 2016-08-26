57c7d9f20d115ac42dc292620af61122d12347bc
snewcomer <snewcomer24@gmail.com>
2016-08-26 10:07:27 -0700

    [Logging]: missing translation

--- bsrs-ember/app/utils/i18n/missing-message.js
+++ bsrs-ember/app/utils/i18n/missing-message.js
@@ -1,6 +1,7 @@
+import Ember from 'ember';
 // @public
 //
-// Generate a "missing template" message that will be used
+// Generate a 'missing template' message that will be used
 // as a translation.
 //
 // To override this, define `util:i18n/missing-message` with
@@ -8,5 +9,6 @@
 //
 // `Function(String, String, Object) -> String`.
 export default function missingMessage(locale, key /*, data */) {
+  Ember.Logger.warn('Missing translation: ' + key);
   return key;
 }
