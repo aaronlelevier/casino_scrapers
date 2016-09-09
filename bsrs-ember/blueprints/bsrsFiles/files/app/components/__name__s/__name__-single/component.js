3371f1c6b1dc6c41c2122c0384bc14083ee36b45
snewcomer <snewcomer24@gmail.com>
2016-09-09 07:53:51 -0700

    [BLUEPRINT]: update repos in single component

--- bsrs-ember/blueprints/bsrsFiles/files/app/components/__name__s/__name__-single/component.js
+++ bsrs-ember/blueprints/bsrsFiles/files/app/components/__name__s/__name__-single/component.js
@@ -4,9 +4,9 @@ import TabMixin from 'bsrs-ember/mixins/components/tab/base';
 import { task } from 'ember-concurrency';
 
 export default Ember.Component.extend(TabMixin, {
-  repository: injectRepo('<%= dasherizedModuleName %>'),
   // For db-fetch if applicable
   <%= secondModelCamel %>Repo: injectRepo('<%= secondModel %>'),
+  <%= thirdAssociatedModelCamel %>Repo: injectRepo('<%= secondModel %>'),
   saveTask: task(function * () {
     if (this.get('model.validations.isValid')) {
       const tab = this.tab();
