4e6b1fd07d27136e48dc209d22d4c250a3f933e2
aronysidoro <pyaaron@gmail.com>
2016-09-15 17:49:49 -0700

    format js file the same as other grid header files are formatted

--- bsrs-ember/app/components/people/person-list/component.js
+++ bsrs-ember/app/components/people/person-list/component.js
@@ -2,11 +2,42 @@ import GridViewComponent from 'bsrs-ember/components/grid-view/component';
 
 export default GridViewComponent.extend({
   layoutName: 'components/grid-view',
-  columns: [
-    {field: 'status.translated_name', headerLabel: 'admin.person.label.status', headerIsTranslatable: true, isSortable: true, isFilterable: true, isSearchable: true, templateName: 'tickets/ticket-status-tag', classNames: ['person-status'] },
-    {field: 'fullname', headerLabel: 'admin.person.label.fullname', headerIsTranslatable: true, isSortable: true, isFilterable: true, isSearchable: true},
-    {field: 'username', headerLabel: 'admin.person.label.username', headerIsTranslatable: true, isSortable: true, isFilterable: true, isSearchable: true},
-    {field: 'title', headerLabel: 'admin.person.label.title', headerIsTranslatable: true, isSortable: true, isFilterable: true, isSearchable: true},
-    {field: 'role.name', headerLabel: 'admin.person.label.role-name', headerIsTranslatable: true, isSortable: true, isFilterable: true, isSearchable: true},
-  ]
+  columns: [{
+    field: 'status.translated_name',
+    headerLabel: 'admin.person.label.status',
+    headerIsTranslatable: true,
+    isSortable: true,
+    isFilterable: true,
+    isSearchable: true,
+    templateName: 'tickets/ticket-status-tag',
+    classNames: ['person-status']
+  }, {
+    field: 'fullname',
+    headerLabel: 'admin.person.label.fullname',
+    headerIsTranslatable: true,
+    isSortable: true,
+    isFilterable: true,
+    isSearchable: true
+  }, {
+    field: 'username',
+    headerLabel: 'admin.person.label.username',
+    headerIsTranslatable: true,
+    isSortable: true,
+    isFilterable: true,
+    isSearchable: true
+  }, {
+    field: 'title',
+    headerLabel: 'admin.person.label.title',
+    headerIsTranslatable: true,
+    isSortable: true,
+    isFilterable: true,
+    isSearchable: true
+  }, {
+    field: 'role.name',
+    headerLabel: 'admin.person.label.role-name',
+    headerIsTranslatable: true,
+    isSortable: true,
+    isFilterable: true,
+    isSearchable: true
+  }, ]
 });
