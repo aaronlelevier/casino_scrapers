3b9e566676034adffced3f649caada3796bd24ea
snewcomer <snewcomer24@gmail.com>
2016-09-20 15:24:31 -0700

    [TAB]: update tab navigation

--- bsrs-ember/app/components/tab-navigation/component.js
+++ bsrs-ember/app/components/tab-navigation/component.js
@@ -20,26 +20,33 @@ export default Ember.Component.extend({
     },
     /*
      * rollback_model
-     * Rollback for multiple tabs should always close tab
-     * Rollback for single tabs (dtd) should not close tab
+     * Rollback for multiple tabs should always close tab - all modules except dtd.
+     * Rollback for single tabs (dtd) should not close tab - tabs that only can have one open at a time
      */
     rollback_model() {
       const tab = this.trx.attemptedTabModel;
       const model = this.trx.attemptedTransitionModel;
       const action = this.trx.attemptedAction; 
       const closeTabAction = this.trx.closeTabAction;
+
+      // ROLLBACK
       if(tab.get('tabType') === 'multiple') {
         model.rollback();
       } else {
         this.get('tabList').rollbackAll(tab);
       }
+     
+      // CLOSE MODAL
       tab.toggleProperty('modalIsShowing');
+      
+      // REDIRECT BACK TO APPLICATION ROUTE
       /* When closing single tab should send 'closeTab' action */
       if (tab.get('tabType') === 'multiple' || closeTabAction === 'closeTab') {
         this.attrs.closeTabMaster(tab, {action:'closeTab'});
+      } else {
+        /* Otherwise should not close the tab for single tabTypes, thus send rollback action that will prevent closing tab in closeTab method of tab service */
+        this.attrs.closeTabMaster(tab, {action:'rollback'});
       }
-      /* Otherwise should not close the tab for single tabTypes, thus send rollback action that will prevent closing tab in closeTab method of tab service */
-      this.attrs.closeTabMaster(tab, {action:'rollback'});
     },
     cancel_modal() {
       const tab = this.trx.attemptedTabModel;
