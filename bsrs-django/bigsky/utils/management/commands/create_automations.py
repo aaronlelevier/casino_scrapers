d202516c5f7bb3e831959dd34d7601076ae33dd6
aronysidoro <pyaaron@gmail.com>
2016-09-28 11:47:56 -0700

    [Automations] update fixtures to include all AutomationActionTypes

--- bsrs-django/bigsky/utils/management/commands/create_automations.py
+++ bsrs-django/bigsky/utils/management/commands/create_automations.py
@@ -1,7 +1,8 @@
 from django.core.management.base import BaseCommand
 
 from automation.tests.factory import (
-    create_automations, create_automation_filters, create_automation_events)
+    create_automations, create_automation_filters, create_automation_events,
+    create_automation_action_types)
 
 
 class Command(BaseCommand):
@@ -9,5 +10,6 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         create_automation_events()
+        create_automation_action_types()
         create_automation_filters()
         create_automations()
