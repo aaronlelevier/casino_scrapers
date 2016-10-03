e3c34ec3fd78597eaf8ca84a6853d165b2a58793
snewcomer <snewcomer24@gmail.com>
2016-10-02 22:18:18 -0700

    [CONTACT]: componentize single contact inputs and fix recursive naming multiple underscores in name

--- bsrs-ember/app/components/input-single-email/component.js
+++ bsrs-ember/app/components/input-single-email/component.js
@@ -1,7 +1,11 @@
 import Ember from 'ember';
 const { run } = Ember;
 
+/*
+ * override email valuePath with something like implementation_email
+ */
 export default Ember.Component.extend({
+  valuePath: 'email',
   actions: {
     delete(entry) {
       run(() => {
