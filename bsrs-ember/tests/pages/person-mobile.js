ad3189b0afab5d27ea34d7a55d495994d01d9108
snewcomer <snewcomer24@gmail.com>
2016-10-17 10:26:49 -0700

    [MOBILE]: people grid WIP

--- bsrs-ember/tests/pages/person-mobile.js
+++ bsrs-ember/tests/pages/person-mobile.js
@@ -1,4 +1,4 @@
-import { create, visitable } from 'ember-cli-page-object';
+import { create, visitable, clickable } from 'ember-cli-page-object';
 import PD from 'bsrs-ember/vendor/defaults/person';
 import BASEURLS from 'bsrs-ember/utilities/urls';
 
@@ -9,4 +9,5 @@ const DETAIL_URL = `${BASE_PEOPLE_URL}/${PD.idOne}`;
 export default create({
   visit: visitable(PEOPLE_INDEX_URL),
   visitDetail: visitable(DETAIL_URL),
+  clickFilterUsername: clickable('.t-filter-username'),
 });
