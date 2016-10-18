c76896bb86441c0031db244c81301d310b65cf15
snewcomer <snewcomer24@gmail.com>
2016-10-18 13:32:50 -0700

    [ADDON]: return out belongs to from ammend

--- bsrs-ember/addons/bsrs-components/addon/repository/belongs-to.js
+++ bsrs-ember/addons/bsrs-components/addon/repository/belongs-to.js
@@ -5,35 +5,37 @@ const { run } = Ember;
  * TODO: NEED TO REFACTOR
  */
 var belongs_to_extract = function(type, store, parent_model, type_string, parent_string, multiple) {
-    // DETAIL: belongs_to_extract(response.priority_fk, store, ticket, 'priority', 'ticket', 'tickets');
-    // LIST: belongs_to_extract(status_json, store, ticket, 'status', 'ticket', 'tickets');
-    if (parent_model.get('detail') && parent_model.get(`${type_string}.id`) !== type) {
-        parent_model[`change_${type_string}`](type);
-    } else if (typeof type === 'object') {
-        const type_list = store.find(`${parent_string}-${type_string}-list`, type.id);
-        const type_arr = type_list.get(`${multiple}`) || [];
-        const updated_type_arr = type_arr.concat(parent_model.get('id')).uniq();
-        const join_model = {id: type.id, name: type.name};
-        join_model[`${multiple}`] = updated_type_arr;
-        run(() =>{
-            store.push(`${parent_string}-${type_string}-list`, join_model);
-        });
-    }
+  // DETAIL: belongs_to_extract(response.priority_fk, store, ticket, 'priority', 'ticket', 'tickets');
+  // LIST: belongs_to_extract(status_json, store, ticket, 'status', 'ticket', 'tickets');
+  let model;
+  if (parent_model.get('detail') && parent_model.get(`${type_string}.id`) !== type) {
+    model = parent_model[`change_${type_string}`](type);
+  } else if (typeof type === 'object') {
+    const type_list = store.find(`${parent_string}-${type_string}-list`, type.id);
+    const type_arr = type_list.get(`${multiple}`) || [];
+    const updated_type_arr = type_arr.concat(parent_model.get('id')).uniq();
+    const join_model = {id: type.id, name: type.name};
+    join_model[`${multiple}`] = updated_type_arr;
+    run(() =>{
+      model = store.push(`${parent_string}-${type_string}-list`, join_model);
+    });
+  }
+  return model;
 };
 
 var belongs_to_extract_contacts = function(model, store, contact_model_str, contact_type) {
-    //model, store, email, emails
-    let contact_fks = [];
-    let contacts = model[`${contact_type}`] || [];
-    contacts.forEach((contact) => {
-        contact_fks.push(contact.id);
-        contact.model_fk = model.id;
-        run(() => {
-            store.push(`${contact_model_str}`, contact);
-        });
+  //model, store, email, emails
+  let contact_fks = [];
+  let contacts = model[`${contact_type}`] || [];
+  contacts.forEach((contact) => {
+    contact_fks.push(contact.id);
+    contact.model_fk = model.id;
+    run(() => {
+      store.push(`${contact_model_str}`, contact);
     });
-    delete model[`${contact_type}`];
-    return contact_fks;
+  });
+  delete model[`${contact_type}`];
+  return contact_fks;
 };
 
 var belongs_to = function(_ownerName, modelName, relatedModelName) {
@@ -48,7 +50,7 @@ var belongs_to = function(_ownerName, modelName, relatedModelName) {
 var belongs_to_json = function(modelName, _ownerName, relatedModelName) {
   return function(json, model) {
     const store = this.get('simpleStore');
-    belongs_to_extract(json, store, model, _ownerName, relatedModelName, this.OPT_CONF[_ownerName]['collection']);
+    return belongs_to_extract(json, store, model, _ownerName, relatedModelName, this.OPT_CONF[_ownerName]['collection']);
   };
 };
 
