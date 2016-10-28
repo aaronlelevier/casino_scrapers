60cc2fff8f2100228de35e6432c5424e63c5c337
aronysidoro <pyaaron@gmail.com>
2016-10-28 15:50:05 -0700

    [Automations] remove logging b/c it is loggin w/i project on jenkins, and doesn't have write perms, so needs to log outside of project

--- bsrs-django/bigsky/bigsky/settings/base.py
+++ bsrs-django/bigsky/bigsky/settings/base.py
@@ -254,48 +254,50 @@ EMAIL_HOST_USER = 'bigsky.sc.test@gmail.com'
 EMAIL_HOST_PASSWORD = os.environ['DEV_SC_USER_PASSWORD']
 EMAIL_PORT = 587
 
-
+# TODO: Logging needs to be moved to a directory with the right permissions
+# that we can write to. If it's logging w/i the project, the directory
+# wont have the write permissions
 ### LOGGING ###
-LOGGING_DIR = os.path.join(os.path.dirname(BASE_DIR), "log") # ../bsrs-django/log/
-LOGGING_INFO_FILE = os.path.join(LOGGING_DIR, 'info.log')
-LOGGING_REQUEST_FILE = os.path.join(LOGGING_DIR, 'request.log')
+# LOGGING_DIR = os.path.join(os.path.dirname(BASE_DIR), "log") # ../bsrs-django/log/
+# LOGGING_INFO_FILE = os.path.join(LOGGING_DIR, 'info.log')
+# LOGGING_REQUEST_FILE = os.path.join(LOGGING_DIR, 'request.log')
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'standard': {
-            'format': '%(asctime)s %(levelname)s %(name)s %(message)s'
-        },
-    },
-    'handlers': {
-        'default': {
-            'level':'DEBUG',
-            'class':'logging.handlers.RotatingFileHandler',
-            'filename': LOGGING_INFO_FILE,
-            'maxBytes': 1024*1024*5, # 5 MB
-            'backupCount': 5,
-            'formatter':'standard',
-        },
-        'request_handler': {
-                'level':'DEBUG',
-                'class':'logging.handlers.RotatingFileHandler',
-                'filename': LOGGING_REQUEST_FILE,
-                'maxBytes': 1024*1024*5, # 5 MB
-                'backupCount': 5,
-                'formatter':'standard',
-        },
-    },
-    'loggers': {
-        '': {
-            'handlers': ['default'],
-            'level': 'DEBUG',
-            'propagate': True
-        },
-        'django.request': { # Stop SQL debug from logging to main logger
-            'handlers': ['request_handler'],
-            'level': 'DEBUG',
-            'propagate': False
-        },
-    }
-}
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'formatters': {
+#         'standard': {
+#             'format': '%(asctime)s %(levelname)s %(name)s %(message)s'
+#         },
+#     },
+#     'handlers': {
+#         'default': {
+#             'level':'DEBUG',
+#             'class':'logging.handlers.RotatingFileHandler',
+#             'filename': LOGGING_INFO_FILE,
+#             'maxBytes': 1024*1024*5, # 5 MB
+#             'backupCount': 5,
+#             'formatter':'standard',
+#         },
+#         'request_handler': {
+#                 'level':'DEBUG',
+#                 'class':'logging.handlers.RotatingFileHandler',
+#                 'filename': LOGGING_REQUEST_FILE,
+#                 'maxBytes': 1024*1024*5, # 5 MB
+#                 'backupCount': 5,
+#                 'formatter':'standard',
+#         },
+#     },
+#     'loggers': {
+#         '': {
+#             'handlers': ['default'],
+#             'level': 'DEBUG',
+#             'propagate': True
+#         },
+#         'django.request': { # Stop SQL debug from logging to main logger
+#             'handlers': ['request_handler'],
+#             'level': 'DEBUG',
+#             'propagate': False
+#         },
+#     }
+# }
