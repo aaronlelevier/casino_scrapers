90c176f679cbc99202fc024dd9eeb4fe9c3b500c
aronysidoro <pyaaron@gmail.com>
2016-10-28 15:00:17 -0700

    [Automations] remove ticket activity sub templates. not needed

--- bsrs-django/bigsky/contact/_dj_views.py
+++ bsrs-django/bigsky/contact/_dj_views.py
@@ -6,4 +6,4 @@ from ticket.models import Ticket, TicketActivityType
 def ticket_activities(request, pk):
     ticket = get_object_or_404(Ticket, pk=pk)
     context = {'ticket': ticket}
-    return render(request, 'email/ticket-activities/email.txt', context)
+    return render(request, 'email/test/base.html', context)
