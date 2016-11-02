5e1349bc2adf57c4789d150d1c5cea34fec16d35
snewcomer <snewcomer24@gmail.com>
2016-11-02 12:48:11 -0700

    [ACCESSIBILITY]: add role to search component for easy find

--- bsrs-ember/app/components/grid/helpers/grid-search/component.js
+++ bsrs-ember/app/components/grid/helpers/grid-search/component.js
@@ -5,6 +5,7 @@ import Ember from 'ember';
  */
 export default Ember.Component.extend({
   classNames: ['search-wrap pull-left'],
+  ariaRole: 'search',
   actions: {
     keyup(search) {
       Ember.run.scheduleOnce('actions', this, function() {
