e55e3e38d20723231a7d5cb2d70e6557e66cfa5d
snewcomer <snewcomer24@gmail.com>
2016-11-04 14:08:27 -0700

    [AUTOMATION]: remove done callbacks in test. Did not fix test errors

--- bsrs-ember/tests/acceptance/general/automations/new-test.js
+++ bsrs-ember/tests/acceptance/general/automations/new-test.js
@@ -34,7 +34,6 @@ moduleForAcceptance('Acceptance | automation new test', {
 });
 
 test('visit new URL and create a new record', assert => {
-  var done = assert.async();
   visit(NEW_URL);
   andThen(() => {
     assert.equal(currentURL(), NEW_URL);
@@ -98,12 +97,10 @@ test('visit new URL and create a new record', assert => {
   generalPage.save();
   andThen(() => {
     assert.equal(currentURL(), AUTOMATION_LIST_URL);
-    done();
   });
 });
 
 test('when user creates an automation they should see an empty action', assert => {
-  var done = assert.async();
   clearxhr(listXhr);
   visit(NEW_URL);
   andThen(() => {
@@ -115,7 +112,6 @@ test('when user creates an automation they should see an empty action', assert =
   andThen(() => {
     assert.equal(page.actionTypeSelectedOne, AATD.keyOne);
     assert.equal(Ember.$('.t-automation-action-assignee-select').length, 1);
-    done();
   });
 });
 
