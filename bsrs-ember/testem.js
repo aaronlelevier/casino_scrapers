8eec935dbf78e82804d0932dbabdd1cc5bf354c4
snewcomer <snewcomer24@gmail.com>
2016-11-14 21:26:37 -0800

    TESTEM: ability to run in parallel w/ commented out code

--- bsrs-ember/testem.js
+++ bsrs-ember/testem.js
@@ -11,4 +11,24 @@ module.exports = {
     "Firefox",
     "Chrome"
   ]
-};
+}
+
+// /*jshint node:true*/
+// module.exports = {
+//   "framework": "qunit",
+//   "test_page": [
+//     "tests/index.html?hidepassed&filter=/^Unit/i",
+//     "tests/index.html?hidepassed&filter=/^Integration/i",
+//     "tests/index.html?hidepassed&filter=/General/i",
+//     "tests/index.html?hidepassed&filter=/Tab/i",
+//     "tests/index.html?hidepassed&filter=/Grid/i"
+//   ],
+//   "disable_watching": true,
+//   "parallel": 6,
+//   "launch_in_ci": [
+//     "Firefox"
+//   ],
+//   "launch_in_dev": [
+//     "Chrome"
+//   ]
+// };;
