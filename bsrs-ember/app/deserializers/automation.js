279e1a92a3cc8ea366327103a4b54a0fcce084ab
snewcomer <snewcomer24@gmail.com>
2016-11-21 14:37:13 -0800

    [AUTOMATION]: wrap list in run loop

--- bsrs-ember/app/deserializers/automation.js
+++ bsrs-ember/app/deserializers/automation.js
@@ -45,117 +45,118 @@ export default Ember.Object.extend(OptConf, {
     delete response.events;
     delete response.actions;
     response.detail = true;
-    let automation = store.push('automation', response);
-    const [,eventData,] = this.setup_event(events, automation);
+    let automation;
+    run(() => {
+      automation = store.push('automation', response);
+      const [,eventData,] = this.setup_event(events, automation);
 
-    let actionTypes = {};
-    let assignees = {};
-    let ticketccs = {};
-    let priorities = {};
-    let statuses = {};
-    let sendemails = {};
-    let sendsmss = {};
-    let recipient;
-    let sendsms_recipient;
-    actions.forEach((a) => {
-      // type
-      const type = a.type;
-      delete a.type;
-      actionTypes[a.id] = type;
-      a.type_fk = type.id;
+      let actionTypes = {};
+      let assignees = {};
+      let ticketccs = {};
+      let priorities = {};
+      let statuses = {};
+      let sendemails = {};
+      let sendsmss = {};
+      let recipient;
+      let sendsms_recipient;
+      actions.forEach((a) => {
+        // type
+        const type = a.type;
+        delete a.type;
+        actionTypes[a.id] = type;
+        a.type_fk = type.id;
 
-      /* START ACTION TYPE PROCESSING */
-      // assignee
-      if (a.assignee) {
-        const assignee = a.assignee;
-        delete a.assignee;
-        assignees[a.id] = assignee;
-        a.assignee_fk = assignee.id;
-      }
-      // ticketcc
-      if (a.ccs) {
-        const ticketcc = a.ccs;
-        delete a.ccs;
-        ticketccs[a.id] = ticketcc;
-      }
-      // priority
-      if (a.priority) {
-        const priority = a.priority;
-        delete a.priority;
-        priorities[a.id] = priority;
-        a.priority_fk = priority.id;
-      }
-      //status
-      if(a.status) {
-        const status = a.status;
-        delete a.status;
-        statuses[a.id] = status;
-        a.status_fk = status.id;
-      }
+        /* START ACTION TYPE PROCESSING */
+        // assignee
+        if (a.assignee) {
+          const assignee = a.assignee;
+          delete a.assignee;
+          assignees[a.id] = assignee;
+          a.assignee_fk = assignee.id;
+        }
+        // ticketcc
+        if (a.ccs) {
+          const ticketcc = a.ccs;
+          delete a.ccs;
+          ticketccs[a.id] = ticketcc;
+        }
+        // priority
+        if (a.priority) {
+          const priority = a.priority;
+          delete a.priority;
+          priorities[a.id] = priority;
+          a.priority_fk = priority.id;
+        }
+        //status
+        if(a.status) {
+          const status = a.status;
+          delete a.status;
+          statuses[a.id] = status;
+          a.status_fk = status.id;
+        }
 
-      //sendemail - delete recipients, type, body, subject (re-assign) and gen up uuid for send_email model
-      if (type.key === 'automation.actions.send_email') {
-        recipient = a.recipients; // {type: 'role', id: '1xxx'}
-        delete a.recipients;
-        let { body, subject } = a;
-        delete a.body;
-        delete a.subject;
-        const sendemail = { id: this.get('uuid').v4(), body: body, subject: subject };
-        sendemails[a.id] = sendemail;
-        a.sendemail_fk = sendemail.id;
-      }
+        //sendemail - delete recipients, type, body, subject (re-assign) and gen up uuid for send_email model
+        if (type.key === 'automation.actions.send_email') {
+          recipient = a.recipients; // {type: 'role', id: '1xxx'}
+          delete a.recipients;
+          let { body, subject } = a;
+          delete a.body;
+          delete a.subject;
+          const sendemail = { id: this.get('uuid').v4(), body: body, subject: subject };
+          sendemails[a.id] = sendemail;
+          a.sendemail_fk = sendemail.id;
+        }
 
-      // sendsms
-      if (type.key === 'automation.actions.send_sms') {
-        sendsms_recipient = a.recipients;
-        delete a.recipients;
-        let { body } = a;
-        delete a.body;
-        const sendsms = { id: this.get('uuid').v4(), body: body };
-        sendsmss[a.id] = sendsms;
-        a.sendsms_fk = sendsms.id;
-      }
-      /* END ACTION TYPE PROCESSING */
+        // sendsms
+        if (type.key === 'automation.actions.send_sms') {
+          sendsms_recipient = a.recipients;
+          delete a.recipients;
+          let { body } = a;
+          delete a.body;
+          const sendsms = { id: this.get('uuid').v4(), body: body };
+          sendsmss[a.id] = sendsms;
+          a.sendsms_fk = sendsms.id;
+        }
+        /* END ACTION TYPE PROCESSING */
 
-      // must set as "detail" b/c this is a detail payload
-      a.detail = true;
-    });
-
-    // push in the actions (pojo's) in the store and those are returned
-    const [,actionData,] = this.setup_action(actions, automation);
-    actionData.forEach((ad) => {
-      const action = store.find('automation-action', ad.id);
-      // type
-      let type = actionTypes[ad.id];
-      this.setup_type(type, action);
-
-      // assignee
-      let assignee = assignees[ad.id];
-      this.setup_assignee(assignee, action);
-      // ticketcc
-      // action may have different ccs, so need to wipe out join models and make new ones based on server payload
-      action.get('automation_action_ticketcc').forEach((m2m) => {
-        store.push('action-join-person', {id: m2m.get('id'), removed: true});
+        // must set as "detail" b/c this is a detail payload
+        a.detail = true;
       });
-      let ticketcc = ticketccs[ad.id];
-      if (ticketcc) {
-        this.setup_ticketcc(ticketcc, action);
-      }
-      // priority
-      let priority = priorities[ad.id];
-      this.setup_priority(priority, action);
-      // status - adds the action id to the array of 'actions' in the status model
-      let status = statuses[ad.id];
-      this.setup_status(status, action);
 
-      //sendemail
-      let sendemail = sendemails[ad.id];
-      this.setup_sendemail(sendemail, action);
-      // setup recipients - req'd by detail payload
-      if (sendemail) {
-        const sendemail_hydrated = store.find('sendemail', sendemail.id);
-        let [m2m_recipients, recipients, server_sum] = many_to_many_extract(recipient, store, sendemail_hydrated, 'generic_recipient', 'generic_pk', 'related-person', 'recipient_pk');
-        run(() => {
+      // push in the actions (pojo's) in the store and those are returned
+      const [,actionData,] = this.setup_action(actions, automation);
+      actionData.forEach((ad) => {
+        const action = store.find('automation-action', ad.id);
+        // type
+        let type = actionTypes[ad.id];
+        this.setup_type(type, action);
+
+        // assignee
+        let assignee = assignees[ad.id];
+        this.setup_assignee(assignee, action);
+        // ticketcc
+        // action may have different ccs, so need to wipe out join models and make new ones based on server payload
+        action.get('automation_action_ticketcc').forEach((m2m) => {
+          store.push('action-join-person', {id: m2m.get('id'), removed: true});
+        });
+        let ticketcc = ticketccs[ad.id];
+        if (ticketcc) {
+          this.setup_ticketcc(ticketcc, action);
+        }
+        // priority
+        let priority = priorities[ad.id];
+        this.setup_priority(priority, action);
+        // status - adds the action id to the array of 'actions' in the status model
+        let status = statuses[ad.id];
+        this.setup_status(status, action);
+
+        //sendemail
+        let sendemail = sendemails[ad.id];
+        this.setup_sendemail(sendemail, action);
+        // setup recipients - req'd by detail payload
+        if (sendemail) {
+          const sendemail_hydrated = store.find('sendemail', sendemail.id);
+          let [m2m_recipients, recipients, server_sum] = many_to_many_extract(recipient, store, sendemail_hydrated, 'generic_recipient', 'generic_pk', 'related-person', 'recipient_pk');
           recipients.forEach((rec) => {
             store.push('related-person', rec);
           });
@@ -163,18 +164,16 @@ export default Ember.Object.extend(OptConf, {
             store.push('generic-join-recipients', m2m);
           });
           store.push('sendemail', {id: sendemail_hydrated.get('id'), generic_recipient_fks: server_sum});
-        });
-        // this.setup_recipient(recipient, sendemail_hydrated);
-      }
+          // this.setup_recipient(recipient, sendemail_hydrated);
+        }
 
-      // sendsms
-      let sendsms = sendsmss[ad.id];
-      this.setup_sendsms(sendsms, action);
-      // setup recipients - req'd by detail payload
-      if(sendsms) {
-        const sendsms_hydrated = store.find('sendsms', sendsms.id);
-        let [m2m_recipients, recipients, server_sum] = many_to_many_extract(sendsms_recipient, store, sendsms_hydrated, 'generic_recipient', 'generic_pk', 'related-person', 'recipient_pk');
-        run(() => {
+        // sendsms
+        let sendsms = sendsmss[ad.id];
+        this.setup_sendsms(sendsms, action);
+        // setup recipients - req'd by detail payload
+        if(sendsms) {
+          const sendsms_hydrated = store.find('sendsms', sendsms.id);
+          let [m2m_recipients, recipients, server_sum] = many_to_many_extract(sendsms_recipient, store, sendsms_hydrated, 'generic_recipient', 'generic_pk', 'related-person', 'recipient_pk');
           recipients.forEach((rec) => {
             store.push('related-person', rec);
           });
@@ -182,9 +181,9 @@ export default Ember.Object.extend(OptConf, {
             store.push('generic-join-recipients', m2m);
           });
           store.push('sendsms', {id: sendsms_hydrated.get('id'), generic_recipient_fks: server_sum});
-        });
-        // this.setup_recipient(sendsms_recipient, sendsms_hydrated);
-      }
+          // this.setup_recipient(sendsms_recipient, sendsms_hydrated);
+        }
+      });
     });
 
     const [,pfs,] = this.setup_pf(filters, automation);
@@ -199,7 +198,9 @@ export default Ember.Object.extend(OptConf, {
   _deserializeList(response) {
     const store = this.get('simpleStore');
     response.results.forEach((model) => {
-      store.push('automation-list', model);
+      run(() => {
+        store.push('automation-list', model);
+      });
     });
   }
 });
