7f4f385ee678339f677d2d9446426d7754ed7919
snewcomer <snewcomer24@gmail.com>
2016-12-12 10:06:39 -0800

    cleanup equal func

--- bsrs-ember/app/utilities/equal.js
+++ bsrs-ember/app/utilities/equal.js
@@ -1,9 +1,8 @@
 var equal = function(first, second) {
-    if (first instanceof Array && second instanceof Array) {
-        return first.length === second.length && first.every((v, i) => v === second[i]);
-        // return Ember.$(first).not(second).get().length === 0 && Ember.$(second).not(first).get().length === 0;
-    }
-    return first === second;
+  if (first instanceof Array && second instanceof Array) {
+    return first.length === second.length && first.every((v, i) => v === second[i]);
+  }
+  return first === second;
 };
 
 export default equal;
