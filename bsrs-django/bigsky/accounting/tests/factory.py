9ed323afb1a1b50dec16fe99ebfcd910726a34f5
aronysidoro <pyaaron@gmail.com>
2017-01-05 10:58:36 -0800

    [Currency] django factory method for generating an individual Currency

--- bsrs-django/bigsky/accounting/tests/factory.py
+++ bsrs-django/bigsky/accounting/tests/factory.py
@@ -11,17 +11,31 @@ CURRENCIES = [
     ['US Dollar', 'US Dollars', 'USD', '$', '$', '2']
 ]
 
+CurrencyData = namedtuple('CurrencyData',
+                          ['name', 'name_plural', 'code',
+                           'symbol', 'symbol_native', 'decimal_digits'])
+
+def create_currency(code='USD'):
+    try:
+        c = [c for c in CURRENCIES if c[2] == code][0]
+    except IndexError as e:
+        # 'Not a Currency valid code'
+        raise e
+
+    data = CurrencyData._make(c)._asdict()
+    return get_or_create_currency(**data)
+
 
 def create_currencies():
-    CurrencyData = namedtuple('CurrencyData',
-                              ['name', 'name_plural', 'code',
-                               'symbol', 'symbol_native', 'decimal_digits'])
-
     for c in CURRENCIES:
         data = CurrencyData._make(c)._asdict()
+        get_or_create_currency(**data)
 
-        try:
-            Currency.objects.get(code=data['code'])
-        except Currency.DoesNotExist:
-            data['id'] = generate_uuid(Currency)
-            Currency.objects.create(**data)
+
+def get_or_create_currency(**kwargs):
+    try:
+        currency = Currency.objects.get(code=kwargs['code'])
+    except Currency.DoesNotExist:
+        kwargs['id'] = generate_uuid(Currency)
+        currency = Currency.objects.create(**kwargs)
+    return currency
--- bsrs-django/bigsky/accounting/tests/test_factory.py
+++ bsrs-django/bigsky/accounting/tests/test_factory.py
@@ -11,14 +11,39 @@ class FactoryTests(TestCase):
     def setUp(self):
         factory.create_currencies()
 
+    def test_create_currency(self):
+        ret = factory.create_currency()
+
+        self.assertIsInstance(ret, Currency)
+        self.assertEqual(ret.code, 'USD')
+
+    def test_create_currency__code(self):
+        code = 'CNY'
+
+        ret = factory.create_currency(code=code)
+
+        self.assertIsInstance(ret, Currency)
+        self.assertEqual(ret.code, code)
+
+    def test_create_currency__invalid_code(self):
+        code = 'foo'
+        self.assertNotIn(code, [x[2] for x in factory.CURRENCIES])
+
+        with self.assertRaises(IndexError):
+            ret = factory.create_currency(code=code)
+
     def test_create_currencies(self):
         for c in factory.CURRENCIES:
-            CurrencyData = namedtuple('CurrencyData',
-                                      ['name', 'name_plural', 'code',
-                                       'symbol', 'symbol_native', 'decimal_digits'])
-            data = CurrencyData._make(c)._asdict()
+            data = factory.CurrencyData._make(c)._asdict()
             self.assertTrue(Currency.objects.filter(code=data['code']).exists())
 
     def test_generate_uuid(self):
         for index, currency in enumerate(Currency.objects.order_by('id')):
             self.assertEqual(str(currency.id)[-3:], "{:03d}".format(index+1))
+
+    def test_get_or_create_currency(self):
+        data = factory.CurrencyData._make(factory.CURRENCIES[0])._asdict()
+
+        ret = factory.get_or_create_currency(**data)
+
+        self.assertIsInstance(ret, Currency)
