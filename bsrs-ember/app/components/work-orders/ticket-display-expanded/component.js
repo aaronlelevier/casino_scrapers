41eed5c2b13d04d7677351e827ecbc974d39f15b
andykrier <andykrier@gmail.com>
2017-02-08 09:42:53 -0800

    [RESCHEDULE] Allow user to reschedule and cancel reschedule.

--- bsrs-ember/app/components/work-orders/ticket-display-expanded/component.js
+++ bsrs-ember/app/components/work-orders/ticket-display-expanded/component.js
@@ -7,9 +7,13 @@ const { get, set } = Ember;
 export default Ember.Component.extend({
   currencyRepo: injectRepo('currency'),
   today: new Date(),
+  rescheduling: false,
   actions: {
     toggleIsExpanded() {
       this.get('toggleIsExpanded')();
+    },
+    toggleIsRescheduling() {
+      this.toggleProperty('rescheduling');
     },
     setDate(date) {
       const model = get(this, 'model');
